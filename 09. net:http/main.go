05062c751f039b0a38a150f6e3482a1283e8815c
parkjinhong03 <jinhong0719@naver.com>
2019-11-26 12:06:03 +0900

    Context.Params으로 HTTP 요청에 포함

--- 09. net:http/main.go
+++ 09. net:http/main.go
@@ -7,30 +7,30 @@ import (
 )
 
 func main() {
-	r := net.Router{Handlers: make(map[string]map[string]http.HandlerFunc)}
+	r := net.Router{Handlers: make(map[string]map[string]net.HandlerFunc)}
 
-	r.HandleFunc("GET", "/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "welcome!")
+	r.HandleFunc("GET", "/", func(c *net.Context) {
+		fmt.Fprintln(c.ResponseWriter, "welcome!")
 	})
 
-	r.HandleFunc("GET", "/about", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "about")
+	r.HandleFunc("GET", "/about", func(c *net.Context) {
+		fmt.Fprintln(c.ResponseWriter, "about")
 	})
 
-	r.HandleFunc("GET", "/users/:user_id", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "retrieve user")
+	r.HandleFunc("GET", "/users/:user_id", func(c *net.Context) {
+		fmt.Fprintf(c.ResponseWriter, "retrieve user %v\n", c.Params["user_id"])
 	})
 
-	r.HandleFunc("GET", "/users/:user_id/addresses/:address_id", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "retrieve user's address")
+	r.HandleFunc("GET", "/users/:user_id/addresses/:address_id", func(c *net.Context) {
+		fmt.Fprintf(c.ResponseWriter, "retrieve user %v's address %v\n", c.Params["user_id"], c.Params["address_id"])
 	})
 
-	r.HandleFunc("POST", "/users", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "create user")
+	r.HandleFunc("POST", "/users", func(c *net.Context) {
+		fmt.Fprintln(c.ResponseWriter, "create user")
 	})
 
-	r.HandleFunc("POST", "/users/:user_id/addresses", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "create user's address")
+	r.HandleFunc("POST", "/users/:user_id/addresses", func(c *net.Context) {
+		fmt.Fprintf(c.ResponseWriter, "create user %v's address", c.Params["user_id"])
 	})
 
 	http.ListenAndServe(":8080", r)
