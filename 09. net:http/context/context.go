843537339da668cf9d70eef116f4af5f18f3f6cd
parkjinhong03 <jinhong0719@naver.com>
2019-11-26 11:18:07 +0900

    URL의 매개변수를 Handler에 전달하기 위한 context 생성

---
+++ 09. net:http/context/context.go
@@ -0,0 +1,12 @@
+package context
+
+import "net/http"
+
+
+// Params 필드에 라우터에서 해석한 URL 매개변수로 담고, 핸들러 내부에는 Context 값이 전달되게 한다.
+type Context struct {
+	Params map[string]interface{}
+
+	ResponseWriter http.ResponseWriter
+	Request *http.Request
+}
