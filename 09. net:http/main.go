e94dbb9ac19ef991b66954ea2348e31e50c1ecc6
parkjinhong03 <jinhong0719@naver.com>
2019-11-26 11:10:55 +0900

    동적 URL의 핸들러 매핑 디버깅

--- 09. net:http/main.go
+++ 09. net:http/main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"./router"
 	"fmt"
 	"net/http"
 )
 
 func main() {
-	// "/" 경로로 접속했을 때 처리할 핸들러 함수 지
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// "hello world!" 문자열을 화면에 출력
-		fmt.Fprintf(w, "hello world!")
+	r := router.Router{Handlers: make(map[string]map[string]http.HandlerFunc)}
+
+	r.HandleFunc("GET", "/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "welcome!")
 	})
-	
-	// 8080 포트로 웹 서버 구동
-	http.ListenAndServe(":8080", nil)
+
+	r.HandleFunc("GET", "/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "about")
+	})
+
+	r.HandleFunc("GET", "/users/:user_id", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "retrieve user")
+	})
+
+	r.HandleFunc("GET", "/users/:user_id/addresses/:address_id", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "retrieve user's address")
+	})
+
+	r.HandleFunc("POST", "/users", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "create user")
+	})
+
+	r.HandleFunc("POST", "/users/:user_id/addresses", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "create user's address")
+	})
+
+	http.ListenAndServe(":8080", r)
 }
--- 09. net:http/router/router.go
+++ 09. net:http/router/router.go
@@ -1,24 +1,24 @@
-package main
+package router
 
 import (
 	"net/http"
 	"strings"
 )
 
-type router struct {
+type Router struct {
 	// 키: http 메서드
 	// 값: URL 패턴별로 실행할 HandleFunc
-	handlers map[string]map[string]http.HandlerFunc
+	Handlers map[string]map[string]http.HandlerFunc
 }
 
 // 라우터에 핸들러를 등록하기 위한 메서드 정의
-func (r *router) HandleFunc(method, pattern string, h http.HandlerFunc) {
+func (r *Router) HandleFunc(method, pattern string, h http.HandlerFunc) {
 	// http 메서드로 등록된 맵이 있는지 확인
-	m, ok := r.handlers[method]
+	m, ok := r.Handlers[method]
 	if !ok {
 		// 등록된 맵이 없으면 새 맵을 생성
 		m = make(map[string]http.HandlerFunc)
-		r.handlers[method] = m
+		r.Handlers[method] = m
 	}
 
 	// http 메서드로 등록된 맵에 URL 패턴과 핸들러 함수 등록
@@ -27,15 +27,14 @@ func (r *router) HandleFunc(method, pattern string, h http.HandlerFunc) {
 
 // http.Handler 인터페이스로 사용하기 위한 ServeHTTP(http.ResponseWriter, *http.Request) 메서드 정의
 // ServeHTTP 메서드는 웹 요청의 http 메서드와 URL 경로를 분석해서 그에 맞는 핸들러를 찾아 동작시킨다.
-func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func (r Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	// http 메서드에 맞는 모든 handlers를 반복하여 요청 URL에 해당하는 handler를 찾음
-	for patterns, handler := range r.handlers[req.Method] {
+	for patterns, handler := range r.Handlers[req.Method] {
 		if ok, _ := match(patterns, req.URL.Path); ok {
 			// 요청 URL에 해당하는 handler 실행
 			handler(w, req)
 			return
 		}
-
 	}
 	http.NotFound(w, req)
 	return
@@ -52,7 +51,7 @@ func match(pattern, path string) (bool, map[string]string) {
 	paths := strings.Split(path, "/")
 
 	// 패턴과 패스를 "/"로 구분한 후 부분 문자열 집합의 개수가 다르면 false를 반환
-	if len(pattern) != len(path) {
+	if len(patterns) != len(paths) {
 		return false, nil
 	}
 
@@ -60,13 +59,12 @@ func match(pattern, path string) (bool, map[string]string) {
 	params := make(map[string]string)
 
 	// "/"로 구분된 패턴/패스의 각 문자열을 하나씩 비교
-
-	for i:=0; i<len(pattern); i++ {
+	for i:=0; i<len(patterns); i++ {
 		switch {
 			case patterns[i] == paths[i]:
 				// 패턴과 패스의 부분 문자열이 일치하면 바로 다음 루프 실행
 			case len(patterns[i]) > 0 && patterns[i][0] == ':':
-				// 패턴이 ':' 문자로 시작하면 params에 URL params를 담은 후 다음 루프 실행
+				// 패턴이 ':' 문자로 시작하면 params에 URL params를 담은 후 다음 루프 실행≈
 				params[patterns[i][1:]] = paths[i]
 			default:
 				// 일치하는 경우가 없다면 false를 반환
