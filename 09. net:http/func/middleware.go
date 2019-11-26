4d434d3bd5ce7d79b92b5e708a4c8ff09c9edcfc
parkjinhong03 <jinhong0719@naver.com>
2019-11-26 13:13:06 +0900

    에러 처리 미들웨어 RecoverMiddleware 생성

--- 09. net:http/func/middleware.go
+++ 09. net:http/func/middleware.go
@@ -2,6 +2,7 @@ package net
 
 import (
 	"log"
+	"net/http"
 	"time"
 )
 
@@ -20,5 +21,19 @@ func LogHandler(next HandlerFunc) HandlerFunc {
 			c.Request.Method,
 			c.Request.URL.String(),
 			time.Now().Sub(t))
+	}
+}
+
+func RecoverHandler(next HandlerFunc) HandlerFunc {
+	return func(c *Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("panic: %+v", err)
+				http.Error(c.ResponseWriter,
+					http.StatusText(http.StatusInternalServerError),
+					http.StatusInternalServerError)
+			}
+		}()
+		next(c)
 	}
 }
--- 09. net:http/main.go
+++ 09. net:http/main.go
@@ -9,17 +9,20 @@ import (
 func main() {
 	r := net.Router{Handlers: make(map[string]map[string]net.HandlerFunc)}
 
-	r.HandleFunc("GET", "/", net.LogHandler(func(c *net.Context) {
-		fmt.Fprintln(c.ResponseWriter, "welcome!")
-	}))
+	r.HandleFunc("GET", "/", net.LogHandler(net.RecoverHandler(func(c *net.Context) {
+			fmt.Fprintln(c.ResponseWriter, "welcome!")
+	})))
 
 	r.HandleFunc("GET", "/about", func(c *net.Context) {
 		fmt.Fprintln(c.ResponseWriter, "about")
 	})
 
-	r.HandleFunc("GET", "/users/:user_id", func(c *net.Context) {
-		fmt.Fprintf(c.ResponseWriter, "retrieve user %v\n", c.Params["user_id"])
-	})
+	r.HandleFunc("GET", "/users/:user_id", net.LogHandler(net.RecoverHandler(func(c *net.Context) {
+		if c.Params["user_id"] == "0" {
+			panic("id is zero")
+		}
+		fmt.Fprintf(c.ResponseWriter, "recieve user %v\n", c.Params["user_id"])
+	})))
 
 	r.HandleFunc("GET", "/users/:user_id/addresses/:address_id", func(c *net.Context) {
 		fmt.Fprintf(c.ResponseWriter, "retrieve user %v's address %v\n", c.Params["user_id"], c.Params["address_id"])
