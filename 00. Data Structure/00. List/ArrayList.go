9f561d64ca70cbaa82a7f3d8750f99943740b9ae
지농 <jinhong0719@naver.com>
2019-12-13 16:04:35 +0900

    배열 기반 리스트 메서드 정의

--- 00. Data Structure/00. List/ArrayList.go
+++ 00. Data Structure/00. List/ArrayList.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 const ListLen = 100
 type LData interface{}
 
@@ -10,26 +12,54 @@ type List struct {
 	curPosition int		// 데이터 참조위치를 기록
 }
 
-func (plist *List) ListInit() {
-
+func (plist *List) NewList() *List {
+	return &List{
+		numOfData:	 0,
+		curPosition: -1,
+	}
 }
 
 func (plist *List) LInsert(data LData) {
+	if plist.numOfData >= ListLen {
+		fmt.Println("저장이 불가능합니다.")
+	}
 
+	plist.arr[plist.numOfData] = data
+	plist.curPosition ++
 }
 
-func (plist *List) LFirst(pdata *LData) int {
+func (plist *List) LFirst(pdata *LData) bool {
+	if plist.numOfData == 0 {
+		return false
+	}
 
+	plist.curPosition = 0
+	*pdata = plist.arr[plist.curPosition]
+	return true
 }
 
-func (plist *List) LNext(pdata *LData) int {
+func (plist *List) LNext(pdata *LData) bool {
+	if plist.curPosition >= plist.numOfData-1 {
+		return false
+	}
 
+	plist.curPosition ++
+	*pdata = plist.arr[plist.curPosition]
+	return true
 }
 
 func (plist *List) LRemove() LData {
+	var rdata LData = plist.arr[plist.curPosition]
 
+	for i:=plist.curPosition; i<plist.numOfData; i++ {
+		plist.arr[i] = plist.arr[i+1]
+	}
+
+	plist.numOfData--
+	plist.curPosition--
+	return rdata
 }
 
 func (plist *List) LCount() int {
-
+	return plist.numOfData
 }
