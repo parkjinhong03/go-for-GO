962e7d58460a49d5c32c282faacb59776eb90e38
지농 <jinhong0719@naver.com>
2019-12-13 19:19:48 +0900

    <배열 기반 리스트> main 함수 생성 및 주석 추가

--- 00. Data Structure/00. List/ArrayList.go
+++ 00. Data Structure/00. List/ArrayList.go
@@ -15,6 +15,7 @@ type List struct {
 	curPosition int		// 데이터 참조위치를 기록
 }
 
+// 배열 기반 리스트인 List 객체 생성 및 초기화 후 반환
 func NewList() *List {
 	return &List{
 		numOfData:	 0,
@@ -22,6 +23,7 @@ func NewList() *List {
 	}
 }
 
+// List 객체의 배열 arr 필드의 맨 뒤에 새로운 값 추가
 func (plist *List) LInsert(data LData) {
 	if plist.numOfData >= ListLen {
 		fmt.Println("저장이 불가능합니다.")
@@ -32,6 +34,7 @@ func (plist *List) LInsert(data LData) {
 	plist.numOfData++
 }
 
+// List 객체의 현재 참조 위치를 맨 앞으로 이동시킨 후 메개변수로 받은 pdata에 참조된 값 저장
 func (plist *List) LFirst(pdata *LData) bool {
 	if plist.numOfData == 0 {
 		return false
@@ -42,6 +45,7 @@ func (plist *List) LFirst(pdata *LData) bool {
 	return true
 }
 
+// List 객체의 현재 참조 위치를 다음 요소로 변경시킨 후 매개변수로 받은 pdata에 참조된 값 저장
 func (plist *List) LNext(pdata *LData) bool {
 	if plist.curPosition >= plist.numOfData-1 {
 		return false
@@ -52,6 +56,7 @@ func (plist *List) LNext(pdata *LData) bool {
 	return true
 }
 
+// List 객체의 현재 참조된 값 삭제 및 재정렬 후 삭제한 값 반환
 func (plist *List) LRemove() (LData, error) {
 	if plist.curPosition < 0 {
 		return nil, errors.New("삭제가 불가능합니다")
@@ -69,6 +74,7 @@ func (plist *List) LRemove() (LData, error) {
 	return rdata, nil
 }
 
+// List 객체의 현재 참조 위치, 데이터 갯수, 데이터를 출력
 func (plist List) LPrint() {
 	var data LData
 
@@ -87,4 +93,31 @@ func (plist List) LPrint() {
 		fmt.Println()
 	}
 
+}
+
+func main() {
+	list := NewList()
+	data := new(LData)
+
+	list.LInsert(1)
+	list.LInsert(2)
+	list.LInsert(3)
+
+	list.LPrint()
+	// 현재 참조 위치: -1 | 현재 데이터의 수: 3
+	// 1 2 3
+
+	if _, err:=list.LRemove(); err != nil {
+		fmt.Println(err)
+		// 삭제가 불가능합니다
+	}
+
+	list.LFirst(data)
+	if _, err:=list.LRemove(); err != nil {
+		fmt.Println(err)
+	}
+
+	list.LPrint()
+	// 현재 참조 위치: 0 | 현재 데이터의 수: 2
+	// 2 3
 }
