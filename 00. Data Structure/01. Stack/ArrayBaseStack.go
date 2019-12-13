1b9dc31270d71fd87a0e01325e9e3f6ca008dd23
지농 <jinhong0719@naver.com>
2019-12-13 15:15:51 +0900

    구조체 및 메서드에 주석 추가

--- 00. Data Structure/01. Stack/ArrayBaseStack.go
+++ 00. Data Structure/01. Stack/ArrayBaseStack.go
@@ -7,11 +7,13 @@ import (
 const StackLen = 5
 type Data interface {}
 
+// stack 구조체 정의
 type Stack struct {
 	stackArr [StackLen]Data
 	topIndex int
 }
 
+// 새로운 Stack 객체를 생성 및 초기화하고 그 객체의 주소 값을 반환해줌
 func New() *Stack {
 	return &Stack{
 		stackArr: [StackLen]Data{},
@@ -19,6 +21,7 @@ func New() *Stack {
 	}
 }
 
+// 전달받은 리시버 변수인 Stack이 빈 값인지 확인해주는 메서드
 func (ps *Stack) SIsEmpty() bool {
 	if ps.topIndex == -1 {
 		return true
@@ -27,6 +30,7 @@ func (ps *Stack) SIsEmpty() bool {
 	}
 }
 
+// 잔딜 빋은 리시버 변수인 Stack이 꽉 찼는지 확인해주는 메서드
 func (ps *Stack) SIsFull() bool {
 	if ps.topIndex == StackLen-1 {
 		return true
@@ -35,6 +39,7 @@ func (ps *Stack) SIsFull() bool {
 	}
 }
 
+// 전달 받은 리시버 변수인 Stack에 새 값을 넣어주는 메서드
 func (ps *Stack) SPush(data Data) {
 	pushErrorHandler(func(ps *Stack, data Data) {
 		if ps.SIsFull() {
@@ -46,6 +51,7 @@ func (ps *Stack) SPush(data Data) {
 	})(ps, data)
 }
 
+// 전달 받은 리시버 변수인 Stack에서 가장 최근에 넣은 값을 삭제 후 반환하는 메서드
 func (ps *Stack) SPop() Data {
 	return popErrorHandler(func(ps *Stack) Data {
 		if ps.SIsEmpty() {
@@ -59,6 +65,7 @@ func (ps *Stack) SPop() Data {
 	})(ps)
 }
 
+// SPush 함수에 에러 처리 코드를 추가한 함수 반환
 func pushErrorHandler(handler func(ps *Stack, data Data)) func(ps *Stack, data Data) {
 	return func(ps *Stack, data Data) {
 		defer func() {
@@ -71,6 +78,7 @@ func pushErrorHandler(handler func(ps *Stack, data Data)) func(ps *Stack, data D
 	}
 }
 
+// SPop 함수에 에러 처리 코드를 추가한 함수 반환
 func popErrorHandler(handler func(ps *Stack) Data) func (ps *Stack) Data {
 	return func(ps *Stack) Data {
 		defer func() {
@@ -83,6 +91,7 @@ func popErrorHandler(handler func(ps *Stack) Data) func (ps *Stack) Data {
 	}
 }
 
+// 절달 받은 리시버 변수인 Stack의 값들을 최근에 넣은 순서대로 출력해주는 메서드
 func (ps *Stack) SPrint() {
 	for i:=ps.topIndex; i>=0; i-- {
 		fmt.Print(ps.stackArr[i], " ")
@@ -92,6 +101,7 @@ func (ps *Stack) SPrint() {
 
 func main() {
 	stack := New()
+
 	stack.SPush("a")
 	stack.SPush("ab")
 	stack.SPush("abc")
