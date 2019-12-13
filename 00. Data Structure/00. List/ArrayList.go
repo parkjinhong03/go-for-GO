6b02b448f5947bcbad99cab14b0612ed01be183b
지농 <jinhong0719@naver.com>
2019-12-13 15:17:02 +0900

    배열 기반 리스트 구조체 정의

--- 00. Data Structure/00. List/ArrayList.go
+++ 00. Data Structure/00. List/ArrayList.go
@@ -1,1 +1,11 @@
-package _0__List
+package main
+
+const ListLen = 100
+type LData interface{}
+
+// 배열기반 리스트를 정의한 구조체
+type List struct {
+	arr [ListLen]LData	// 리스트의 저장소인 배열
+	numOfData int		// 저장된 데이터의 수
+	curPosition int		// 데이터 참조위치를 기록
+}
