d4c8f14901e3603f54e1a18faa5e5ed1f7a594fc
지농 <jinhong0719@naver.com>
2019-12-14 10:35:30 +0900

    연결 기반 리스트 메서드 정의

--- 00. Data Structure/01. List/02. LinkedList.go
+++ 00. Data Structure/01. List/02. LinkedList.go
@@ -10,6 +10,8 @@ type Data interface {}
 type LinkedList struct {
 	head *Node
 	tail *Node
+	cur *Node
+	before *Node
 }
 
 // value와 다음 Node의 주솟값을 가지는 각 노드를 표현할 구조체
@@ -19,25 +21,62 @@ type Node struct {
 }
 
 func NewLinkedList() *LinkedList {
-
+	return &LinkedList{
+		head:   nil,
+		tail:   nil,
+		cur:    nil,
+		before: nil,
+	}
 }
 
 func newNode(data Data) *Node {
-
+	return &Node{
+		data: data,
+		next: nil,
+	}
 }
 
 func (plist *LinkedList) LInsert(data Data) {
+	newNode := newNode(data)
 
+	if plist.head == nil {
+		plist.head = newNode
+	} else {
+		plist.tail.next = newNode
+	}
+
+	plist.tail = newNode
 }
 
-func (plist *LinkedList) LFirst(data *Data) {
+func (plist *LinkedList) LFirst(data *Data) bool {
+	if plist.head == nil {
+		return false
+	}
 
+	plist.cur = plist.head
+	plist.before = nil
+	*data = plist.cur.data
+	return true
 }
 
-func (plist *LinkedList) LNext(data *Data) {
+func (plist *LinkedList) LNext(data *Data) bool {
+	if plist.cur.next == nil {
+		return false
+	}
 
+	plist.before = plist.cur
+	plist.cur = plist.cur.next
+	*data = plist.cur.data
+	return true
 }
 
 func (plist *LinkedList) LRemove() Data {
-
+	if plist.cur == nil {
+		fmt.Println("참조된 노드가 없습니다")
+		return nil
+	}
+	rpos := plist.cur.data
+	plist.before.next = plist.cur.next
+	plist.cur = plist.before
+	return rpos
 }
