9ff44330893d3a9d0cfbf08097c895b81b351c0f
지농 <jinhong0719@naver.com>
2019-12-14 11:53:58 +0900

    더미 노드 기반 연결 리스트 ADT 정의

--- 00. Data Structure/01. List/03. DLinkedList.go
+++ 00. Data Structure/01. List/03. DLinkedList.go
@@ -1,16 +1,42 @@
 package main
 
-type LData interface {}
+type DData interface {}
+
+type SortingFunc func(Data, Data) int
 
 type DLinkedList struct {
 	head *DNode // 더미 노드를 가리키는 필드
 	cur *DNode // 참조 및 삭제를 돕는 필드
 	before *DNode // 삭제를 돕는 필드
 	numOfData int // 저장된 데이터의 수를 기록하기 위한 필드
-	comp *func (d1 LData, d2 LData) int // 정렬의 기준을 등록하기 위한 필드
+	comp SortingFunc // 정렬의 기준을 등록하기 위한 필드
 }
 
 type DNode struct {
-	data LData // 해당 노드의 값을 저장하기 위한 필드
+	data DData // 해당 노드의 값을 저장하기 위한 필드
 	next *DNode // 다음 노드의 주소값을 저장하기 위한 필드
+}
+
+func NewDLinkedList() *DLinkedList {
+
+}
+
+func (plist *DLinkedList) LInset(data DData) {
+
+}
+
+func (plist *DLinkedList) LFirst(data *DData) bool {
+
+}
+
+func (plist *DLinkedList) LSecond(data *DData) bool {
+
+}
+
+func (plist *DLinkedList) LRemove() DData {
+
+}
+
+func (plist *DLinkedList) SetSortRule(sf SortingFunc) {
+	
 }
