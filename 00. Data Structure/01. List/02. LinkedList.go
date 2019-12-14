37c1d0ac90cc9cae9d333dea660452f188487e04
지농 <jinhong0719@naver.com>
2019-12-14 09:47:22 +0900

    연결 기반 리스트 ADT 정의

--- 00. Data Structure/01. List/02. LinkedList.go
+++ 00. Data Structure/01. List/02. LinkedList.go
@@ -4,14 +4,40 @@ import (
 	"fmt"
 )
 
+type Data interface {}
+
 // 시작 Node와 끝 Node를 가지는 List 구조체
-type List struct {
+type LinkedList struct {
 	head *Node
 	tail *Node
 }
 
 // value와 다음 Node의 주솟값을 가지는 각 노드를 표현할 구조체
 type Node struct {
-	data int
+	data Data
 	next *Node
+}
+
+func NewLinkedList() *LinkedList {
+
+}
+
+func newNode(data Data) *Node {
+
+}
+
+func (plist *LinkedList) LInsert(data Data) {
+
+}
+
+func (plist *LinkedList) LFirst(data *Data) {
+
+}
+
+func (plist *LinkedList) LNext(data *Data) {
+
+}
+
+func (plist *LinkedList) LRemove() Data {
+
 }
