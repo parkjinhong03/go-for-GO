14117615b1ad0317a5f60c98f1f0e11051be874e
지농 <jinhong0719@naver.com>
2019-12-14 15:40:42 +0900

    원형 연결 리스트 초기화 및 노드 삽입

--- 00. Data Structure/01. List/04. CLinkedList.go
+++ 00. Data Structure/01. List/04. CLinkedList.go
@@ -19,15 +19,48 @@ type CNode struct {
 }
 
 func NewCLinkedList() *CLinkedList {
+	return &CLinkedList{
+		tail:      nil,
+		cur:       nil,
+		before:    nil,
+		numOfData: 0,
+	}
+}
 
+func NewCNode(data CData) *CNode {
+	return &CNode{
+		data: data,
+		next: nil,
+	}
 }
 
 func (plist *CLinkedList) LInsert(data CData) {
+	newNode := NewCNode(data)
 
+	if plist.tail == nil {
+		plist.tail = newNode
+		newNode.next = newNode
+	} else {
+		newNode.next = plist.tail.next
+		plist.tail.next = newNode
+		plist.tail = newNode
+	}
+
+	plist.numOfData++
 }
 
 func (plist *CLinkedList) LInsertFront(data CData) {
+	newNode := NewCNode(data)
 
+	if plist.tail == nil {
+		plist.tail = newNode
+		newNode.next = newNode
+	} else {
+		newNode.next = plist.tail.next
+		plist.tail.next = newNode
+	}
+
+	plist.numOfData++
 }
 
 func (plist *CLinkedList) LFirst(data *CData) bool {
