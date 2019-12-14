e30fa6eb2d6b9b3c8c1a597b9eed374219fe3573
지농 <jinhong0719@naver.com>
2019-12-14 16:22:07 +0900

    원형 연결 리스트 각각의 메서드에 설명 추가

--- 00. Data Structure/01. List/04. CLinkedList.go
+++ 00. Data Structure/01. List/04. CLinkedList.go
@@ -6,6 +6,7 @@ import (
 
 type CData interface {}
 
+// 원형 연결 리스트를 구현하기 위한 구조체 정의
 type CLinkedList struct {
 	tail *CNode
 	cur *CNode
@@ -13,11 +14,13 @@ type CLinkedList struct {
 	numOfData int
 }
 
+// 연결 리스트에 각각 연결한 노드 구조체 정의
 type CNode struct {
 	data CData
 	next *CNode
 }
 
+// 새 ClinkedList 객체 생성 및 초기화 후 주소 반환
 func NewCLinkedList() *CLinkedList {
 	return &CLinkedList{
 		tail:      nil,
@@ -27,6 +30,7 @@ func NewCLinkedList() *CLinkedList {
 	}
 }
 
+// 새 노드 생성 및 초기화 후 주소 반환
 func NewCNode(data CData) *CNode {
 	return &CNode{
 		data: data,
@@ -34,6 +38,7 @@ func NewCNode(data CData) *CNode {
 	}
 }
 
+// 새 노드 성성 후 ClinkedList의 tail에 해당 노드 연결
 func (plist *CLinkedList) LInsert(data CData) {
 	newNode := NewCNode(data)
 
@@ -49,6 +54,7 @@ func (plist *CLinkedList) LInsert(data CData) {
 	plist.numOfData++
 }
 
+// 새 노드 생성 후 CLinkedList의 head(tail Node의 next 필드 값)에 해당 노드 연결
 func (plist *CLinkedList) LInsertFront(data CData) {
 	newNode := NewCNode(data)
 
@@ -63,6 +69,7 @@ func (plist *CLinkedList) LInsertFront(data CData) {
 	plist.numOfData++
 }
 
+// CLinkedList의 before은 tail로, cur은 head로 참조 값을 설정해준다.
 func (plist *CLinkedList) LFirst(data *CData) bool {
 	if plist.tail == nil {
 		return false
@@ -74,6 +81,7 @@ func (plist *CLinkedList) LFirst(data *CData) bool {
 	return true
 }
 
+// CLickedList의 before과 cur의 참조 값을 각각 다음 노드로 이동시킨다.
 func (plist *CLinkedList) LNext(data *CData) bool {
 	if plist.cur == plist.tail {
 		return false
@@ -85,6 +93,7 @@ func (plist *CLinkedList) LNext(data *CData) bool {
 	return true
 }
 
+// 현재 참조 중인 노드를 삭제하고, 만약 현재 참조 값이 tail 이라면 tail을 before의 참조 값으로 바꾼다.
 func (plist *CLinkedList) LRemove() CData {
 	rData := plist.cur.data
 
@@ -103,6 +112,7 @@ func (plist *CLinkedList) LRemove() CData {
 	return rData
 }
 
+// 리시버 번수로 받은 CLinkedList의 현재 데이터 수와 그 값들을 head에서 tail 순서로 출력해준다.
 func (plist *CLinkedList) LPrint() {
 	data := new(CData)
 
@@ -130,7 +140,7 @@ func main() {
 	list.LInsert(2)
 	list.LInsertFront(3)
 	list.LInsertFront(4)
-
+	
 	list.LPrint()
 	// 현재 데이터의 수: 4
 	// 4 3 1 2
