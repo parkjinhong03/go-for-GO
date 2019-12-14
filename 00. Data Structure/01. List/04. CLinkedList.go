eacba1b4659b5adbc964c9c4d9aae3b7fa0bea72
지농 <jinhong0719@naver.com>
2019-12-14 15:23:31 +0900

    원형 연결 리스트 ADT 정의

--- 00. Data Structure/01. List/04. CLinkedList.go
+++ 00. Data Structure/01. List/04. CLinkedList.go
@@ -17,3 +17,27 @@ type CNode struct {
 	data CData
 	next *CNode
 }
+
+func NewCLinkedList() *CLinkedList {
+
+}
+
+func (plist *CLinkedList) LInsert(data CData) {
+
+}
+
+func (plist *CLinkedList) LInsertFront(data CData) {
+
+}
+
+func (plist *CLinkedList) LFirst(data *CData) bool {
+
+}
+
+func (plist *CLinkedList) LNext(data *CData) bool {
+
+}
+
+func (plist *CLinkedList) LRemove() CData {
+
+}
