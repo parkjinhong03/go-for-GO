0d191574480fb8d08a198fa6a389a3285cc2d546
지농 <jinhong0719@naver.com>
2019-12-15 13:49:41 +0900

    양방향 연결 리스트 초기화 및 노드 삽입

--- 00. Data Structure/01. List/05. DBLinkedList.go
+++ 00. Data Structure/01. List/05. DBLinkedList.go
@@ -15,11 +15,35 @@ type DBLinkedList struct {
 type DBNode struct {
 	data DBData
 	next *DBNode
-	before *DBNode
+	prev *DBNode
+}
+
+func NewDBLinkedList() *DBLinkedList {
+	return &DBLinkedList{
+		head:      nil,
+		cur:       nil,
+		numOfData: 0,
+	}
+}
+
+func NewDBNode(data DBData) *DBNode {
+	return &DBNode{
+		data:   data,
+		next:   nil,
+		prev: nil,
+	}
 }
 
 func (plist *DBLinkedList) LInsert(data DBData) {
+	newNode := NewDBNode(data)
 
+	newNode.next = plist.head
+	if plist.head != nil {
+		plist.head.prev = newNode
+	}
+	plist.head = newNode
+
+	plist.numOfData++
 }
 
 func (plist *DBLinkedList) LFirst(data *DBData) bool {
@@ -37,3 +61,4 @@ func (plist *DBLinkedList) LPrevious(data *DBData) bool {
 func (plist *DBLinkedList) LRemove() DBData {
 
 }
+-
