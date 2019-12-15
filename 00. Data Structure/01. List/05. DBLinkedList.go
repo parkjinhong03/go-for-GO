75afcc2f4d29a6e0b688bb0e37f86fc4443a6dc0
지농 <jinhong0719@naver.com>
2019-12-15 13:54:29 +0900

    양방향 연결 리스트 데이터 조회

--- 00. Data Structure/01. List/05. DBLinkedList.go
+++ 00. Data Structure/01. List/05. DBLinkedList.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 type DBData interface {}
@@ -47,15 +48,33 @@ func (plist *DBLinkedList) LInsert(data DBData) {
 }
 
 func (plist *DBLinkedList) LFirst(data *DBData) bool {
+	if plist.head == nil {
+		return false
+	}
 
+	plist.cur = plist.head
+	*data = plist.cur.data
+	return true
 }
 
 func (plist *DBLinkedList) LNext(data *DBData) bool {
+	if plist.cur.next == nil {
+		return false
+	}
 
+	plist.cur = plist.cur.next
+	*data = plist.cur.data
+	return true
 }
 
 func (plist *DBLinkedList) LPrevious(data *DBData) bool {
+	if plist.cur.prev == nil {
+		return false
+	}
 
+	plist.cur = plist.cur.prev
+	*data = plist.cur.data
+	return true
 }
 
 func (plist *DBLinkedList) LRemove() DBData {
