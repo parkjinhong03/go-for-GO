3deecb8448c8d8c9a163b0647477346154528042
지농 <jinhong0719@naver.com>
2019-12-16 22:22:33 +0900

    이진 트리 ADT 정의

--- 00. Data Structure/04. Tree/01. BinaryTree.go
+++ 00. Data Structure/04. Tree/01. BinaryTree.go
@@ -7,3 +7,31 @@ type BTreeNode struct {
 	left *BTreeNode
 	right *BTreeNode
 }
+
+func MakeBTreeNode() *BTreeNode {
+
+}
+
+func GetData(bt *BTreeNode) {
+
+}
+
+func SetData(bt *BTreeNode, data BTData) {
+
+}
+
+func GetLeftSubTree(bt *BTreeNode) *BTreeNode {
+
+}
+
+func GetRightSubTree(bt *BTreeNode) *BTreeNode {
+
+}
+
+func MakeLeftSubTree(main *BTreeNode, sub *BTreeNode) {
+
+}
+
+func MakeRightSubTree(main *BTreeNode, sub *BTreeNode) {
+
+}
