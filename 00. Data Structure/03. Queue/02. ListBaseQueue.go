317e51efaa1218fd966f680f3335034fd52d5c47
지농 <jinhong0719@naver.com>
2019-12-16 19:50:27 +0900

    연결 리스트 기반 큐 객체 생성 및 노드 삽입

--- 00. Data Structure/03. Queue/02. ListBaseQueue.go
+++ 00. Data Structure/03. Queue/02. ListBaseQueue.go
@@ -14,15 +14,39 @@ type LNode struct {
 }
 
 func NewLQueue() *LQueue {
-
+	return &LQueue{
+		front:     nil,
+		rear:      nil,
+		numOfData: 0,
+	}
 }
 
 func NewLNode(data LData) *LNode {
-	
+	return &LNode{
+		data: data,
+		next: nil,
+	}
+}
+
+func (pq *LQueue) QIsEmpty() bool {
+	if pq.front == nil {
+		return true
+	}
+	return false
 }
 
 func (pq *LQueue) Enqueue(data LData) {
+	newNode := NewLNode(data)
 
+	if pq.QIsEmpty() {
+		pq.rear = newNode
+		pq.front = newNode
+	} else {
+		pq.rear.next = newNode
+		pq.rear = newNode
+	}
+
+	pq.numOfData++
 }
 
 func (pq *LQueue) Dequeue() LData {
