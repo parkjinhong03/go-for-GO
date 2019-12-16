dfe46ab8aca58082f9b4767fda02a9b7a43ebc23
지농 <jinhong0719@naver.com>
2019-12-16 22:28:35 +0900

    이진 트리 메인 함수 테스트

--- 00. Data Structure/04. Tree/01. BinaryTree.go
+++ 00. Data Structure/04. Tree/01. BinaryTree.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type BTData interface {}
 
 type BTreeNode struct {
@@ -38,4 +40,25 @@ func MakeLeftSubTree(main *BTreeNode, sub *BTreeNode) {
 
 func MakeRightSubTree(main *BTreeNode, sub *BTreeNode) {
 	main.right = sub
+}
+
+func main() {
+	bt1 := MakeBTreeNode()
+	bt2 := MakeBTreeNode()
+	bt3 := MakeBTreeNode()
+	bt4 := MakeBTreeNode()
+
+	SetData(bt1, 1)
+	SetData(bt2, 2)
+	SetData(bt3, 3)
+	SetData(bt4, 4)
+
+	MakeLeftSubTree(bt1, bt2)
+	MakeRightSubTree(bt1, bt3)
+	MakeLeftSubTree(bt2, bt4)
+
+	fmt.Printf("%d\n", GetData(GetLeftSubTree(bt1)))
+	// 2
+	fmt.Printf("%d\n", GetData(GetLeftSubTree(GetLeftSubTree(bt1))))
+	// 4
 }
