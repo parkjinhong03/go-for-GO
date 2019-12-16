338d45711ef5f6754f7c13f55621ec11ad752b0f
지농 <jinhong0719@naver.com>
2019-12-16 20:56:08 +0900

    연결 리스트 기반 큐 ADT 정의

--- 00. Data Structure/03. Queue/03. ListBaseDeque.go
+++ 00. Data Structure/03. Queue/03. ListBaseDeque.go
@@ -13,3 +13,39 @@ type DNode struct {
 	next *DNode
 	prev *DNode
 }
+
+func NewDequeue() *DQueue {
+
+}
+
+func NewDNode(data DData) *DNode {
+
+}
+
+func (pq *DQueue) DQIsEmpty() bool {
+
+}
+
+func (pq *DQueue) DQAddFirst(data DData) {
+
+}
+
+func (pq *DQueue) DQAddLast(data DData) {
+
+}
+
+func (pq *DQueue) DQRemoveFirst() DData {
+
+}
+
+func (pq *DQueue) DQRemoveLast() DData {
+
+}
+
+func (pq *DQueue) DQGetFirst() DData {
+
+}
+
+func (pq *DQueue) DQGetLast() DData {
+
+}
