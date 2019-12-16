ce94a51ded1567067a23589e5223ab260c930593
지농 <jinhong0719@naver.com>
2019-12-16 19:42:53 +0900

    연결 리스트 기반 큐 ADT 정의

--- 00. Data Structure/03. Queue/02. ListBaseQueue.go
+++ 00. Data Structure/03. Queue/02. ListBaseQueue.go
@@ -12,3 +12,23 @@ type LNode struct {
 	data LData
 	next *LNode
 }
+
+func NewLQueue() *LQueue {
+
+}
+
+func NewLNode(data LData) *LNode {
+	
+}
+
+func (pq *LQueue) Enqueue(data LData) {
+
+}
+
+func (pq *LQueue) Dequeue() LData {
+
+}
+
+func (pq *LQueue) QPrint() {
+
+}
