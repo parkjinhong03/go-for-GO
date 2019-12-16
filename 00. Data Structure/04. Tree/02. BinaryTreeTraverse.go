d05303024c7a11abbdfcc5e5819cdd59afba4175
지농 <jinhong0719@naver.com>
2019-12-16 23:37:59 +0900

    동일 패키지에 의한 중복 메서드 오류 수정

--- 00. Data Structure/04. Tree/02. BinaryTreeTraverse.go
+++ 00. Data Structure/04. Tree/02. BinaryTreeTraverse.go
@@ -8,14 +8,14 @@ type VisitFunc func(BTTData)
 
 // 데이터와 왼쪽 자식 노드, 오른쪽 자식 노드의 주소값을 가지고 있는 구조체
 type BTTreeNode struct {
-	data BTData
-	left *BTreeNode
-	right *BTreeNode
+	data BTTData
+	left *BTTreeNode
+	right *BTTreeNode
 }
 
 // 모든 값이 null인 이진 트리 노드 생성 및 주소값 반환
-func MakeBTTreeNode() *BTreeNode {
-	return &BTreeNode{
+func MakeBTTreeNode() *BTTreeNode {
+	return &BTTreeNode{
 		data:  nil,
 		left:  nil,
 		right: nil,
@@ -23,37 +23,37 @@ func MakeBTTreeNode() *BTreeNode {
 }
 
 // 인자 값으로 받은 이진 트리 노드의 데이터 반환
-func GetTData(bt *BTreeNode) BTData {
+func GetTData(bt *BTTreeNode) BTTData {
 	return bt.data
 }
 
 // 인자 값으로 받은 이진 트리 노드에 데이터를 저장시킴
-func SetTData(bt *BTreeNode, data BTData) {
+func SetTData(bt *BTTreeNode, data BTTData) {
 	bt.data = data
 }
 
 // 인자 값으로 받은 이진 트리 노드의 왼쪽 자식 노드의 주소값을 반환
-func TGetLeftSubTree(bt *BTreeNode) *BTreeNode {
+func TGetLeftSubTree(bt *BTTreeNode) *BTTreeNode {
 	return bt.left
 }
 
 // 인자 값으로 받은 이진 트리 노드의 오른쪽 자식 노드의 주소값을 반환
-func TGetRightSubTree(bt *BTreeNode) *BTreeNode {
+func TGetRightSubTree(bt *BTTreeNode) *BTTreeNode {
 	return bt.right
 }
 
 // 두 번째의 인자 값으로 받은 이진 트리 노드를 첫 번째의 인자 값으로 받은 이진 트리 노드의 왼쪽 서브 트리로 연결시킴
-func TMakeLeftSubTree(main *BTreeNode, sub *BTreeNode) {
+func TMakeLeftSubTree(main *BTTreeNode, sub *BTTreeNode) {
 	main.left = sub
 }
 
 // 두 번째의 인자 값으로 받은 이진 트리 노드를 첫 번째의 인자 값으로 받은 이진 트리 노드의 오른쪽 서브 트리로 연결시킴
-func TMakeRightSubTree(main *BTreeNode, sub *BTreeNode) {
+func TMakeRightSubTree(main *BTTreeNode, sub *BTTreeNode) {
 	main.right = sub
 }
 
 // 이진 트리 전체를 중위 순회한 결과를 출력하는 함수
-func InorderTraverse(bt *BTreeNode, action VisitFunc) {
+func InorderTraverse(bt *BTTreeNode, action VisitFunc) {
 	if bt == nil {
 		return
 	}
@@ -64,7 +64,7 @@ func InorderTraverse(bt *BTreeNode, action VisitFunc) {
 }
 
 // 이진 트리 전체를 전위 순회한 결과를 출력하는 함수
-func PreorderTraverse(bt *BTreeNode, action VisitFunc) {
+func PreorderTraverse(bt *BTTreeNode, action VisitFunc) {
 	if bt == nil {
 		return
 	}
@@ -75,7 +75,7 @@ func PreorderTraverse(bt *BTreeNode, action VisitFunc) {
 }
 
 // 이진 트리 전체를 후위 순회한 결과를 출력하는 함수
-func PostorderTraverse(bt *BTreeNode, action VisitFunc) {
+func PostorderTraverse(bt *BTTreeNode, action VisitFunc) {
 	if bt == nil {
 		return
 	}
@@ -86,25 +86,25 @@ func PostorderTraverse(bt *BTreeNode, action VisitFunc) {
 }
 
 func main() {
-	bt1 := MakeBTreeNode()
-	bt2 := MakeBTreeNode()
-	bt3 := MakeBTreeNode()
-	bt4 := MakeBTreeNode()
-	bt5 := MakeBTreeNode()
-	bt6 := MakeBTreeNode()
+	bt1 := MakeBTTreeNode()
+	bt2 := MakeBTTreeNode()
+	bt3 := MakeBTTreeNode()
+	bt4 := MakeBTTreeNode()
+	bt5 := MakeBTTreeNode()
+	bt6 := MakeBTTreeNode()
 
-	SetData(bt1, 1)
-	SetData(bt2, 2)
-	SetData(bt3, 3)
-	SetData(bt4, 4)
-	SetData(bt5, 5)
-	SetData(bt6, 6)
+	SetTData(bt1, 1)
+	SetTData(bt2, 2)
+	SetTData(bt3, 3)
+	SetTData(bt4, 4)
+	SetTData(bt5, 5)
+	SetTData(bt6, 6)
 
-	MakeLeftSubTree(bt1, bt2)
-	MakeRightSubTree(bt1, bt3)
-	MakeLeftSubTree(bt2, bt4)
-	MakeRightSubTree(bt2, bt5)
-	MakeRightSubTree(bt3, bt6)
+	TMakeLeftSubTree(bt1, bt2)
+	TMakeRightSubTree(bt1, bt3)
+	TMakeLeftSubTree(bt2, bt4)
+	TMakeRightSubTree(bt2, bt5)
+	TMakeRightSubTree(bt3, bt6)
 
 	PreorderTraverse(bt1, ShowIntData); fmt.Println()
 	// 1 2 4 5 3 6
