804505fd399a79754058d4d695bf9975b55a88ac
지농 <jinhong0719@naver.com>
2019-12-16 23:04:08 +0900

    이진 트리 메서드 단위로 설명 추가

--- 00. Data Structure/04. Tree/01. BinaryTree.go
+++ 00. Data Structure/04. Tree/01. BinaryTree.go
@@ -4,12 +4,14 @@ import "fmt"
 
 type BTData interface {}
 
+// 데이터와 왼쪽 자식 노드, 오른쪽 자식 노드의 주소값을 가지고 있는 구조체
 type BTreeNode struct {
 	data BTData
 	left *BTreeNode
 	right *BTreeNode
 }
 
+// 모든 값이 null인 이진 트리 노드 생성 및 주소값 반환
 func MakeBTreeNode() *BTreeNode {
 	return &BTreeNode{
 		data:  nil,
@@ -18,26 +20,32 @@ func MakeBTreeNode() *BTreeNode {
 	}
 }
 
+// 인자 값으로 받은 이진 트리 노드의 데이터 반환
 func GetData(bt *BTreeNode) BTData {
 	return bt.data
 }
 
+// 인자 값으로 받은 이진 트리 노드에 데이터를 저장시킴
 func SetData(bt *BTreeNode, data BTData) {
 	bt.data = data
 }
 
+// 인자 값으로 받은 이진 트리 노드의 왼쪽 자식 노드의 주소값을 반환
 func GetLeftSubTree(bt *BTreeNode) *BTreeNode {
 	return bt.left
 }
 
+// 인자 값으로 받은 이진 트리 노드의 오른쪽 자식 노드의 주소값을 반환
 func GetRightSubTree(bt *BTreeNode) *BTreeNode {
 	return bt.right
 }
 
+// 두 번째의 인자 값으로 받은 이진 트리 노드를 첫 번째의 인자 값으로 받은 이진 트리 노드의 왼쪽 서브 트리로 연결시킴
 func MakeLeftSubTree(main *BTreeNode, sub *BTreeNode) {
 	main.left = sub
 }
 
+// 두 번째의 인자 값으로 받은 이진 트리 노드를 첫 번째의 인자 값으로 받은 이진 트리 노드의 오른쪽 서브 트리로 연결시킴
 func MakeRightSubTree(main *BTreeNode, sub *BTreeNode) {
 	main.right = sub
 }
