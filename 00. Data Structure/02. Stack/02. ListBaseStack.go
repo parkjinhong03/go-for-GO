30c030b88a16eadf3f8e2ffaef389f8ef35d9b5b
지농 <jinhong0719@naver.com>
2019-12-16 15:19:09 +0900

    연결 리스트 기반 스택 객체 생성 및 노드 삽입

--- 00. Data Structure/02. Stack/02. ListBaseStack.go
+++ 00. Data Structure/02. Stack/02. ListBaseStack.go
@@ -4,6 +4,7 @@ type LData interface {}
 
 type ListStack struct {
 	head *ListNode
+	numOfData int
 }
 
 type ListNode struct {
@@ -12,19 +13,37 @@ type ListNode struct {
 }
 
 func NewListStack() *ListStack {
-
+	return &ListStack{
+		head:      nil,
+		numOfData: 0,
+	}
 }
 
-func NewListNode() *ListNode {
-
+func NewListNode(data LData) *ListNode {
+	return &ListNode{
+		data: data,
+		next: nil,
+	}
 }
 
 func (ps *ListStack) SIsEmpty() bool {
-
+	if ps.head == nil {
+		return true
+	}
+	return false
 }
 
 func (ps *ListStack) SPush(data LData) {
+	if ps.SIsEmpty() {
+		return
+	}
 
+	newNode := NewListNode(data)
+
+	newNode.next = ps.head
+	ps.head = newNode
+	
+	ps.numOfData++
 }
 
 func (ps *ListStack) SPop() LData {
