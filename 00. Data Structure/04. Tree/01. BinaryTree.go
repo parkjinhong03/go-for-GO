fc87fde9db6cbbd61410e19905e8bdb9f583266b
지농 <jinhong0719@naver.com>
2019-12-16 22:24:46 +0900

    이진 트리 메서드 정의

--- 00. Data Structure/04. Tree/01. BinaryTree.go
+++ 00. Data Structure/04. Tree/01. BinaryTree.go
@@ -9,29 +9,33 @@ type BTreeNode struct {
 }
 
 func MakeBTreeNode() *BTreeNode {
-
+	return &BTreeNode{
+		data:  nil,
+		left:  nil,
+		right: nil,
+	}
 }
 
-func GetData(bt *BTreeNode) {
-
+func GetData(bt *BTreeNode) BTData {
+	return bt.data
 }
 
 func SetData(bt *BTreeNode, data BTData) {
-
+	bt.data = data
 }
 
 func GetLeftSubTree(bt *BTreeNode) *BTreeNode {
-
+	return bt.left
 }
 
 func GetRightSubTree(bt *BTreeNode) *BTreeNode {
-
+	return bt.right
 }
 
 func MakeLeftSubTree(main *BTreeNode, sub *BTreeNode) {
-
+	main.left = sub
 }
 
 func MakeRightSubTree(main *BTreeNode, sub *BTreeNode) {
-
+	main.right = sub
 }
