9b40ce1dd98dc38a876d8d4f1ab986d6a1801817
지농 <jinhong0719@naver.com>
2019-12-16 15:11:54 +0900

    연결 리스트 기반 스택 ADT 정의

--- 00. Data Structure/02. Stack/02. ListBaseStack.go
+++ 00. Data Structure/02. Stack/02. ListBaseStack.go
@@ -10,3 +10,27 @@ type ListNode struct {
 	data LData
 	next *ListNode
 }
+
+func NewListStack() *ListStack {
+
+}
+
+func NewListNode() *ListNode {
+
+}
+
+func (ps *ListStack) SIsEmpty() bool {
+
+}
+
+func (ps *ListStack) SPush(data LData) {
+
+}
+
+func (ps *ListStack) SPop() LData {
+
+}
+
+func (ps *ListStack) SPrint() {
+
+}
