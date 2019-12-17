75480ef2184669022cd668f97954060aa35bfa4a
지농 <jinhong0719@naver.com>
2019-12-17 21:46:05 +0900

    힙 기반 우선순위 큐 구조체 및 ADT 정의

---
+++ 00. Data Structure/05. Heap/02. PriorityQueue.go
@@ -0,0 +1,21 @@
+// 02. ArrayBaseHeap을 import 했다는 조건으로 시작, min heap을 이용한 우선순위 큐 구현 코드
+package main
+
+type PQueue Heap
+type PQData HData
+
+func NewPQueue() *PQueue {
+
+}
+
+func (pqq *PQueue) PQIsEmpty() bool {
+
+}
+
+func (ppq *PQueue) PEnqueue(data PQData) {
+
+}
+
+func (ppq *PQueue) PDequeue() PQData {
+
+}
