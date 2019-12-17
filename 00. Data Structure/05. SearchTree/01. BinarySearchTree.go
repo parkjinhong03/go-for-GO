23795f703f9f8163f5a538544888384a478bcef4
지농 <jinhong0719@naver.com>
2019-12-17 14:42:48 +0900

    이진 탐색 트리 구조체 및 ADT 정의

---
+++ 00. Data Structure/05. SearchTree/01. BinarySearchTree.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	BTree "../04. Tree"
+	"fmt"
+)
+
+type BSTData BTree.BTData
+
+func BSTMakeAndInit(pRoot **BTree.BTreeNode) {
+
+}
+
+func BSTGetNodeData(pRoot **BTree.BTreeNode) BSTData {
+
+}
+
+func BSTInsert(pRoot **BTree.BTreeNode, data BSTData) {
+
+}
+
+func BSTSearch(bst *BTree.BTreeNode, target BSTData) *BTree.BTTreeNode {
+
+}
