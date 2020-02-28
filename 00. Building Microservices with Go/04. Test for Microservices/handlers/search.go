63f52e235d9eb795aacf51ec1c1097aaa307bf9b
지농 <jinhong0719@naver.com>
2020-02-28 15:23:47 +0900

    데이터 저장소의 모의 객체를 search 핸들러 객체의 필드에 넣음으로써 의존성 주입

--- 00. Building Microservices with Go/04. Test for Microservices/handlers/search.go
+++ 00. Building Microservices with Go/04. Test for Microservices/handlers/search.go
@@ -1,14 +1,23 @@
 package handlers
 
 import (
+	"../data"
 	"encoding/json"
 	"net/http"
 )
 
-type SearchHandler struct {}
+// 새끼 고양이 정보를 반환하는 기능의 핸들러이다.
+// 필드에 검색 기능을 가지고 있는 데이터 저장소 객체를 가지고 있다.
+type SearchHandler struct {
+	DataStore data.Store
+}
 
 type searchRequest struct {
 	Query string `json:"query"`
+}
+
+type searchResponse struct {
+	Kittens []data.Kitten `json:"kittens"`
 }
 
 func (h SearchHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
@@ -22,4 +31,9 @@ func (h SearchHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		http.Error(rw, "Bad Request", http.StatusBadRequest)
 		return
 	}
+
+	kittens := h.DataStore.Search(request.Query)
+
+	encoder := json.NewEncoder(rw)
+	encoder.Encode(searchResponse{Kittens: kittens})
 }
