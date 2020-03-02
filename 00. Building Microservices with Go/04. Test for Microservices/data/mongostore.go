a0f68da884fdfd9e491cf4b32c3cd990055405a3
지농 <jinhong0719@naver.com>
2020-03-02 12:06:48 +0900

    원하는 IP의 mongoDB 세션에 연결시켜 구조체와 같이 해당 연결을 반환하는 함수 정의

--- 00. Building Microservices with Go/04. Test for Microservices/data/mongostore.go
+++ 00. Building Microservices with Go/04. Test for Microservices/data/mongostore.go
@@ -6,3 +6,14 @@ import "gopkg.in/mgo.v2"
 type MongoStore struct {
 	session *mgo.Session
 }
+
+// 매개변수로 받은 IP로 MongoDB 세션을 연결시키고, 그 연결시킨 세션을 필드에 넣어 MongoStore 구조체를 반환하는 메서드.
+func NewMongoStore(connection string) (*MongoStore, error) {
+	// mgo.Dial 함수를 이용하여 MongoDB 서버에 연결시킬 수 있다.
+	session, err := mgo.Dial(connection)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MongoStore{session: session}, nil
+}
