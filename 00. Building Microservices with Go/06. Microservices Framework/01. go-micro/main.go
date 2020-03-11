25fc369dd0a04ffca23ce684963245e3bd60f54b
지농 <jinhong0719@naver.com>
2020-03-11 18:47:14 +0900

    protoc을 사용하여 생성한 API 인터페이스를 구현하여 RPC 서비스에 등록 성공

--- 00. Building Microservices with Go/06. Microservices Framework/01. go-micro/main.go
+++ 00. Building Microservices with Go/06. Microservices Framework/01. go-micro/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/micro/cli"
 	"github.com/micro/go-micro"
+	"go-micro.com/examples/1/proto"
 	"log"
 )
+
+type Greeter struct {}
+
+func (g *Greeter) Hello(ctx context.Context, req *proto.HelloRequest, rsp *proto.HelloResponse) error {
+	rsp.Message = "hello " + req.Name
+	return nil
+}
 
 func main() {
 	service := micro.NewService(
@@ -27,6 +36,11 @@ func main() {
 			}
 		}),
 	)
+
+	err := proto.RegisterGreeterHandler(service.Server(), new(Greeter))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	log.Fatal(service.Run())
 }
