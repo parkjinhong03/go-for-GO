794e6182eaeabe45130affb7ff214d07752905c7
지농 <jinhong0719@naver.com>
2020-03-12 12:11:39 +0900

    client 패키지의 RunClient메서드를 이용하여 rpc 클라이언트 테스트 완료

--- 00. Building Microservices with Go/06. Microservices Framework/01. go-micro/main.go
+++ 00. Building Microservices with Go/06. Microservices Framework/01. go-micro/main.go
@@ -5,14 +5,17 @@ import (
 	"fmt"
 	"github.com/micro/cli"
 	"github.com/micro/go-micro"
+	"go-micro.com/examples/1/client"
 	"go-micro.com/examples/1/proto"
 	"log"
+	"os"
+	"time"
 )
 
 type Greeter struct {}
 
 func (g *Greeter) Hello(ctx context.Context, req *proto.HelloRequest, rsp *proto.HelloResponse) error {
-	rsp.Message = "hello " + req.Name
+	rsp.Message = "Hello " + req.Name
 	return nil
 }
 
@@ -21,19 +24,34 @@ func main() {
 		micro.Name("greeter"),
 		micro.Version("latest"),
 		micro.Flags(
+			cli.BoolFlag{
+				Name:  "run_client",
+				Usage: "Launch the client",
+			},
 			cli.StringFlag{
-				Name:  "env",
-				Usage: "The Environment",
+				Name:  "name",
+				Usage: "Search Service's Name",
 			},
 		),
 	)
 
 	service.Init(
 		micro.Action(func(ctx *cli.Context) {
-			env := ctx.String("env")
-			if len(env) > 0 {
-				fmt.Println("Environment set to", env)
+			if !ctx.Bool("run_client") {
+				return
 			}
+			if ctx.String("name") == "" {
+				fmt.Println("Please specify service's name which is not blank if you want to run client.")
+				return
+			}
+			go func() {
+				time.Sleep(time.Second)
+				resp := client.RunClient(service.Client(), ctx.String("name"), &proto.HelloRequest{
+					Name: "Example",
+				})
+				fmt.Println(resp.Message)
+				os.Exit(0)
+			}()
 		}),
 	)
 
