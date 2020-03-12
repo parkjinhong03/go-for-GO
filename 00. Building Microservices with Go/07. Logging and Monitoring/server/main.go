68f8a8b2faf2dc695ac3615e421b48f40a589c0b
지농 <jinhong0719@naver.com>
2020-03-12 17:07:54 +0900

    statud 서버에 연결하여 새 클라이언트를 반환해주는 함수 생성

--- 00. Building Microservices with Go/07. Logging and Monitoring/server/main.go
+++ 00. Building Microservices with Go/07. Logging and Monitoring/server/main.go
@@ -3,8 +3,30 @@
 
 package main
 
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/alexcesaro/statsd"
+)
+
 const port = 8080
 
 func main() {
-
+	statsd, err := createStatsDClient(os.Getenv("STATSD"))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
+
+// statsD 서버의 주소를 매개변수로 받아 해당 서버의 클라이언트를 생성하여 반환하는 함수이다.
+func createStatsDClient(address string) (*statsd.Client, error) {
+	if address == "" {
+		return &statsd.Client{}, errors.New("please set environment variable 'address'")
+	}
+
+	// statsd.New 함수를 이용하여 서버 주소를 전달해 클라이언트를 생성할 수 있다.
+	return statsd.New(statsd.Address(address))
+}
+
