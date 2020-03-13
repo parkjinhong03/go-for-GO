518cc979d31ba6b5632cf116fc2dfc71c2524bef
지농 <jinhong0719@naver.com>
2020-03-13 11:34:46 +0900

    statsD에 로그를 저장할 때 사용할 버킷 이름에 대한 상수

---
+++ 00. Building Microservices with Go/07. Logging and Monitoring/server/middlewares/const.go
@@ -0,0 +1,6 @@
+package middlewares
+
+const (
+	validationSuccess string = "helloWorldServer.middleware.validation.success"
+	validationFailed  string = "helloWorldServer.middleware.validation.failed"
+)
