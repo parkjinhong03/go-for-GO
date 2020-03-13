8bb8f0edb98b0f6a3f16bb771e3bf0c8827ca8e9
지농 <jinhong0719@naver.com>
2020-03-13 17:23:25 +0900

    helloWorldHandler에서 사용할 버킷의 이름에 대한 상수 정의

---
+++ 00. Building Microservices with Go/07. Logging and Monitoring/server/handlers/const.go
@@ -0,0 +1,7 @@
+package handlers
+
+const (
+	helloWorldSuccess = "helloWorldServer.handler.helloWorld.success"
+	helloWorldFailed  = "helloWorldServer.handler.helloWorld.failed"
+	helloWorldTiming  = "helloWorldServer.handler.helloWorld.timing"
+)
