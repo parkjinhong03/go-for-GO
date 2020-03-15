8e2b4b04ccefc82ce067176c629cdcb868e19112
지농 <jinhong0719@naver.com>
2020-03-15 17:19:32 +0900

    요청 관련 정보를 message에 전부 저장하지 않고, 인덱스당 나눠서 로그에 기록

--- 00. Building Microservices with Go/07. Logging and Monitoring/httputil/request.go
+++ 00. Building Microservices with Go/07. Logging and Monitoring/httputil/request.go
@@ -1,20 +1,24 @@
 package httputil
 
 import (
-	"encoding/json"
+	"github.com/sirupsen/logrus"
 	"net/http"
 	"strings"
 )
 
 // RequestSerializer는 http.Request를 로깅에 필요한 정보만 뽑아서 json으로 Serialize하는 객체이다.
-type RequestSerializer struct {
+type requestSerializer struct {
 	*http.Request
 }
 
 // rs.serialize 메서드의 반환 객체(serializedRequest)를 마샬링하여 문자열로 반환하는 함수이다.
-func (rs *RequestSerializer) ToJSON() string {
-	bytes, _ := json.Marshal(rs.serializeRequest())
-	return string(bytes)
+func (rs *requestSerializer) ToLogrusFields() logrus.Fields {
+	return logrus.Fields{
+		"method": rs.Method,
+		"path": rs.URL.Path,
+		"host": rs.Host,
+		"headers": rs.serializeRequestHeader(),
+	}
 }
 
 type serializedHeader struct {
@@ -22,25 +26,17 @@ type serializedHeader struct {
 	Value string
 }
 
-type serializedRequest struct {
-	Method string
-	Host string
-	Path string
-	Headers []serializedHeader
-}
-
 // http.Request 객체중에서 로깅에 필요한 필드만 골라서 serializedRequest 객체에 저장하여 반환하는 함수이다.
-func (rs *RequestSerializer) serializeRequest() serializedRequest {
+func (rs *requestSerializer) serializeRequestHeader() []serializedHeader {
 	var headers []serializedHeader
 	for k, v := range rs.Header {
 		// strings.Join 함수를 이용하여 슬라이스로 되어있는 Header의 Value를 ", "를 사이사이에 두어 하나의 문자열로 변환시킬 수 있다.
 		headers = append(headers, serializedHeader{Key: k, Value: strings.Join(v, ", ")})
 	}
 
-	return serializedRequest{
-		Method:  rs.Method,
-		Host:    rs.Host,
-		Path:    rs.URL.Path,
-		Headers: headers,
-	}
+	return headers
+}
+
+func NewRequestSerializer(r *http.Request) *requestSerializer {
+	return &requestSerializer{Request: r}
 }
