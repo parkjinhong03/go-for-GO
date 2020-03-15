310adc8d456adfb3112c466891c4e6187884105a
지농 <jinhong0719@naver.com>
2020-03-15 17:18:14 +0900

    statsD 관련 측정 지표 상수 값 모두 삭제

--- 00. Building Microservices with Go/07. Logging and Monitoring/handlers/const.go
+++
@@ -1,7 +0,0 @@
-package handlers
-
-const (
-	helloWorldSuccess = "helloWorldServer.handler.helloWorld.success"
-	helloWorldFailed  = "helloWorldServer.handler.helloWorld.failed"
-	helloWorldTiming  = "helloWorldServer.handler.helloWorld.timing"
-)
--- 00. Building Microservices with Go/07. Logging and Monitoring/middlewares/const.go
+++
@@ -1,6 +0,0 @@
-package middlewares
-
-const (
-	validationSuccess string = "helloWorldServer.middleware.validation.success"
-	validationFailed  string = "helloWorldServer.middleware.validation.failed"
-)
