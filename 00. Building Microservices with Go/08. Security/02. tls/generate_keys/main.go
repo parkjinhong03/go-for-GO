44cb4decad163da05b706fbf0243da50efea2e12
지농 <jinhong0719@naver.com>
2020-03-17 15:07:44 +0900

    main 패키지 생성 및 인증서 발급에 사용될 유효 기간에 대한 상수 선언

---
+++ 00. Building Microservices with Go/08. Security/02. tls/generate_keys/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"time"
+)
+
+// 루트, 어플리케이션, 인스턴스 인증서에 대한 각각의 유효 기간을 설정하기 위한 상수 선언
+const (
+	durationMonth = time.Hour * 24 * 30
+	durationYear = durationMonth * 12 + 5
+	durationDecade = durationYear * 10
+)
