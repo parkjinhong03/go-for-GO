5320e8086798cf210735782bb98ed14b38ce5d61
지농 <jinhong0719@naver.com>
2020-04-21 13:51:17 +0900

    [ADD] <socket.io/server/socket.go> NAME 이벤트에 대한 핸들러 정의

--- 05. Tcp/socket.io/server/socket.go
+++ 05. Tcp/socket.io/server/socket.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"github.com/googollee/go-socket.io"
 	"log"
 	"sort"
@@ -30,23 +31,35 @@ func NewSocketServer() (*socketServer, error) {
 		if err = s.remove(conn); err == nil {
 			log.Printf("Completed disconnecting with client(id: %s)! tatal client: %d", conn.ID(), len(s.clients))
 		}
+		return
+	})
+	s.Server.OnEvent("/", "NAME", func(conn socketio.Conn, name string) {
+		idx := s.searchIdx(conn.ID())
+		s.clients[idx].name = name
+		for _, i := range s.clients { fmt.Println(i.name) }
 	})
 	return s, nil
+}
+
+func (s *socketServer) searchIdx(id string) (idx int) {
+	idx = sort.Search(len(s.clients)-1, func(i int) bool {
+		return s.clients[i].id == id
+	})
+	return
 }
 
 func (s *socketServer) accept(conn socketio.Conn) error {
 	c := &client{
 		conn: conn,
 		id: conn.ID(),
+		name: "undefined",
 	}
 	s.clients = append(s.clients, c)
 	return nil
 }
 
 func (s *socketServer) remove(conn socketio.Conn) error {
-	idx := sort.Search(len(s.clients), func(i int) bool {
-		return s.clients[i].id == conn.ID()
-	})
+	idx := s.searchIdx(conn.ID())
 	s.clients = append(s.clients[:idx], s.clients[idx+1:]...)
 	return nil
 }
