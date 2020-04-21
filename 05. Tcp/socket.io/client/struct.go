6b894ac21f522c2804122401413f0606b1e696ca
지농 <jinhong0719@naver.com>
2020-04-21 14:20:03 +0900

    [MODIFY] <socket.io/client/struct.go> 클라이언트 타입 필드 임베디드 필드로 변경

--- 05. Tcp/socket.io/client/struct.go
+++ 05. Tcp/socket.io/client/struct.go
@@ -3,7 +3,7 @@ package client
 import  "github.com/zhouhui8915/go-socket.io-client"
 
 type socketClient struct {
-	client		*socketio_client.Client
+	*socketio_client.Client
 	incoming 	<- chan chat
 }
 
