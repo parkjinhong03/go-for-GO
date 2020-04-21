cc280951df776fa26b74b9c24f364e7ad6372716
지농 <jinhong0719@naver.com>
2020-04-21 13:32:25 +0900

    [ADD] <socket.io/server/socket.go> disconnect 이벤트 핸들러 추가

--- 05. Tcp/socket.io/server/socket.go
+++ 05. Tcp/socket.io/server/socket.go
@@ -3,6 +3,7 @@ package server
 import (
 	"github.com/googollee/go-socket.io"
 	"log"
+	"sort"
 	"sync"
 )
 
@@ -21,9 +22,14 @@ func NewSocketServer() (*socketServer, error) {
 
 	s.Server.OnConnect("/", func(conn socketio.Conn) (err error) {
 		if err = s.accept(conn); err == nil {
-			log.Printf("Completed connecting new client(id: %s)! tatal client: %d", conn.ID(), len(s.clients))
+			log.Printf("Completed connecting with new client(id: %s)! tatal client: %d", conn.ID(), len(s.clients))
 		}
 		return
+	})
+	s.Server.OnDisconnect("/", func(conn socketio.Conn, reason string) {
+		if err = s.remove(conn); err == nil {
+			log.Printf("Completed disconnecting with client(id: %s)! tatal client: %d", conn.ID(), len(s.clients))
+		}
 	})
 	return s, nil
 }
@@ -34,5 +40,13 @@ func (s *socketServer) accept(conn socketio.Conn) error {
 		id: conn.ID(),
 	}
 	s.clients = append(s.clients, c)
+	return nil
+}
+
+func (s *socketServer) remove(conn socketio.Conn) error {
+	idx := sort.Search(len(s.clients), func(i int) bool {
+		return s.clients[i].id == conn.ID()
+	})
+	s.clients = append(s.clients[:idx], s.clients[idx+1:]...)
 	return nil
 }
