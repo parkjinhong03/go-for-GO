0a418d2687a4a93233133cd3101edf2f7cf7525b
지농 <jinhong0719@naver.com>
2020-05-08 22:46:00 +0900

    [MODIFY] <tool/message/message.go> 메세지 인터페이스 이름 변경

--- 04. MSA Example/tool/message/message.go
+++ 04. MSA Example/tool/message/message.go
@@ -2,7 +2,7 @@ package message
 
 import "github.com/nats-io/nats.go"
 
-type Message interface {
+type NatsMessage interface {
 	Publish(subj string, data []byte) error
 	Subscribe(subj string, cb nats.MsgHandler) (*nats.Subscription, error)
 }
