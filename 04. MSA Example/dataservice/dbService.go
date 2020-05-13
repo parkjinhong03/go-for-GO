af5d6503abeec023525daf73ef83cf9ce15bd8a4
지농 <jinhong0719@naver.com>
2020-05-13 20:24:34 +0900

    [MODIFY] <dataservice/dbService.go> DB 비밀번호 매개변수 -> 환경 변수 파싱

--- 04. MSA Example/dataservice/dbService.go
+++ 04. MSA Example/dataservice/dbService.go
@@ -1,12 +1,18 @@
 package dataservice
 
 import (
+	"errors"
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"os"
 )
 
-func ConnectDB(pwd, db string) (*gorm.DB, error) {
+func ConnectDB(db string) (*gorm.DB, error) {
+	pwd := os.Getenv("DB_PASSWORD")
+	if pwd == "" {
+		return nil, errors.New("unable to parse DB_PASSWORD from environment variable")
+	}
 	conn, err := gorm.Open("mysql", fmt.Sprintf("root:%s@tcp(localhost)/%s?charset=utf8&parseTime=True&loc=Local", pwd, db))
 	if err != nil { return nil, err }
 
