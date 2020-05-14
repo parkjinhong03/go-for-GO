cedf5f580fb92b2ef09faa50b3c867a00958ca83
지농 <jinhong0719@naver.com>
2020-05-14 15:53:01 +0900

    [ADD] <tool/message/nats_message.go> 플래그를 이용한 NATS 서버 연결 메서드 추가

--- 04. MSA Example/tool/message/nats_message.go
+++ 04. MSA Example/tool/message/nats_message.go
@@ -2,31 +2,49 @@ package message
 
 import (
 	"errors"
+	"flag"
 	"github.com/nats-io/nats.go"
 	"os"
 )
 
+var address *string
+func init() {
+	address = flag.String("nats", "", "Nats server address")
+}
+
 type defaultNatsMessage struct {
-	conn *nats.Conn
+	*nats.Conn
 }
 
 func DefaultNatsMessageByEnv() (*defaultNatsMessage, error) {
-	url := os.Getenv("NATS")
-	if url == "" {
+	address := os.Getenv("NATS")
+	if address == "" {
 		return nil, errors.New("please set your NATS environment variable")
 	}
-	conn, err := nats.Connect("nats://" + url)
+	return DefaultNatsMessage(address)
+}
+
+func DefaultNatsMessageByFlag() (*defaultNatsMessage, error) {
+	flag.Parse()
+	if *address == "" {
+		return nil, errors.New("please set your nats command line flag")
+	}
+	return DefaultNatsMessage(*address)
+}
+
+func DefaultNatsMessage(address string) (*defaultNatsMessage, error) {
+	conn, err := nats.Connect("nats://" + address)
 	if err != nil {
 		return nil, err
 	}
 
-	return &defaultNatsMessage{conn: conn}, nil
+	return &defaultNatsMessage{Conn: conn}, nil
 }
 
 func (nm *defaultNatsMessage) Subscribe(subj string, cb nats.MsgHandler) (*nats.Subscription, error) {
-	return nm.conn.Subscribe(subj, cb)
+	return nm.Conn.Subscribe(subj, cb)
 }
 
 func (nm *defaultNatsMessage) Publish(subj string, data []byte) error {
-	return nm.conn.Publish(subj, data)
+	return nm.Conn.Publish(subj, data)
 }
