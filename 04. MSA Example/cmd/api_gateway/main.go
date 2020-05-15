10b7cb9184839414fdd4536bf0c5f0dec906bb33
지농 <jinhong0719@naver.com>
2020-05-15 19:41:58 +0900

    [MODIFY] <cmd/api_gateway/main.go> 핸들러 단위 축소 및 미들웨어 적용

--- 04. MSA Example/cmd/api_gateway/main.go
+++ 04. MSA Example/cmd/api_gateway/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"MSA.example.com/1/entities"
+	"MSA.example.com/1/middleware"
 	"MSA.example.com/1/tool/message"
+	"encoding/json"
 	"fmt"
+	"github.com/go-playground/validator/v10"
 	"log"
 	"net/http"
 )
@@ -12,19 +16,23 @@ func main () {
 	if err != nil {
 		log.Fatalf("unable to connect nats message server, err:%v\n", err)
 	}
-	handler := &apiGateWayHandler{natsM: natsM}
 
-	http.Handle("/api/", http.StripPrefix("/api/", handler))
+	authH := middleware.NewCorrelationMiddleware(&authServiceHandler{
+		natsM: natsM,
+		validate: validator.New(),
+	})
+
+	http.Handle("/api/auth/", http.StripPrefix("/api/auth/", authH))
+	http.Handle("/api/auth", http.StripPrefix("/api/auth", authH))
 	log.Println("Server is starting on port 8080...")
 	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
 
-type apiGateWayHandler struct {
+type authServiceHandler struct {
 	natsM message.NatsMessage
+	validate *validator.Validate
 }
 
-func (h *apiGateWayHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Path)
-	_, _ = fmt.Fprint(rw, 1)
-	return
+func (h *authServiceHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	
 }
