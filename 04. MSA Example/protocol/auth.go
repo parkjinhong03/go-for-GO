ea95480ac00898438524261551248275bc614235
지농 <jinhong0719@naver.com>
2020-05-15 20:49:31 +0900

    [CREATE] <protocol/auth.go> 메시지 포매팅 형식 정의를 위한 protocol 패키지 생성

---
+++ 04. MSA Example/protocol/auth.go
@@ -0,0 +1,12 @@
+package protocol
+
+type AuthSignUpProtocol struct {
+	RequestId string
+	UserId string
+	UserPwd string
+	Name string
+	PhoneNumber string
+	Introduction string
+	Email string
+	ReturnChannel string
+}
