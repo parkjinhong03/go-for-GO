491bc06421d705229986f9f23258ec8a536a36c4
지농 <jinhong0719@naver.com>
2020-05-17 18:32:26 +0900

    [ADD] <proxy/auth_proxy.go> 바이트 배열로부터 InputChannel 값 추출 후 메시징 코드 추가

--- 04. MSA Example/proxy/auth_proxy.go
+++ 04. MSA Example/proxy/auth_proxy.go
@@ -2,6 +2,7 @@ package proxy
 
 import (
 	"MSA.example.com/1/tool/message"
+	"encoding/json"
 )
 
 type AuthServiceProxy struct {
@@ -14,6 +15,13 @@ func NewAuthServiceProxy(natsM message.NatsMessage) *AuthServiceProxy {
 	}
 }
 
-func (ap *AuthServiceProxy) Write(b []byte) (int, error) {
+func (ap *AuthServiceProxy) Write(b []byte) (i int, err error) {
+	in := input{}
+	if err = json.Unmarshal(b, &in); err != nil { return }
+	if err = ap.natsM.Publish(in.InputChannel, b); err != nil { return }
+	return
+}
 
+type input struct {
+	InputChannel string
 }
