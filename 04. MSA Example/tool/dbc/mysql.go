16bf9f9d6f472cf20f6ea95b8ba9d38af6b383fb
지농 <jinhong0719@naver.com>
2020-05-17 21:06:41 +0900

    [CREATE] <tool/dbc/mysql.go> gorm을 이용하여 mysql 서버 연결 및 시그널 핸들링 함수 생성

---
+++ 04. MSA Example/tool/dbc/mysql.go
@@ -0,0 +1,30 @@
+package dbc
+
+import (
+	"errors"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func ConnectDB(db string) (*gorm.DB, error) {
+	pwd := os.Getenv("DB_PASSWORD")
+	if pwd == "" {
+		return nil, errors.New("unable to parse DB_PASSWORD from environment variable")
+	}
+	conn, err := gorm.Open("mysql", fmt.Sprintf("root:%s@tcp(localhost)/%s?charset=utf8&parseTime=True&loc=Local", pwd, db))
+	if err != nil { return nil, err }
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGKILL, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println(sig)
+		_ = conn.Close()
+		os.Exit(0)
+	}()
+	return conn, nil
+}
