dc02c364b50f6a9bc860e200ed00623b0f060f22
지농 <jinhong0719@naver.com>
2020-05-17 21:08:42 +0900

    [ADD] <dataservice/dbService.go> UserDataService에 대한 인터페이스 어댑터 생성

--- 04. MSA Example/dataservice/dbService.go
+++ 04. MSA Example/dataservice/dbService.go
@@ -1,31 +1,13 @@
 package dataservice
 
 import (
-	"errors"
-	"fmt"
-	"github.com/jinzhu/gorm"
+	"MSA.example.com/1/model"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
-func ConnectDB(db string) (*gorm.DB, error) {
-	pwd := os.Getenv("DB_PASSWORD")
-	if pwd == "" {
-		return nil, errors.New("unable to parse DB_PASSWORD from environment variable")
-	}
-	conn, err := gorm.Open("mysql", fmt.Sprintf("root:%s@tcp(localhost)/%s?charset=utf8&parseTime=True&loc=Local", pwd, db))
-	if err != nil { return nil, err }
-
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGKILL, syscall.SIGTERM)
-
-	go func() {
-		sig := <-sigs
-		fmt.Println(sig)
-		_ = conn.Close()
-		os.Exit(0)
-	}()
-	return conn, nil
+type UserDataService interface {
+	Find(id uint32) (user *model.Users, err error)
+	FindByUserId(userId string) (user *model.Users, err error)
+	Insert(user *model.Users) (result *model.Users, err error)
+	Remove(id uint32) (rowAffected int64, err error)
 }
