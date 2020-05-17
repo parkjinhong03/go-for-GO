fb48b4474709e85a4ddd7aa7a2585ad2fe0b584e
지농 <jinhong0719@naver.com>
2020-05-17 16:42:24 +0900

    [CREATE] <tool/encoder/nats/json_encoder.go> json.Encoder 클래스를 임베딩하는 객체 추가

--- 04. MSA Example/tool/encoder/nats/json_encoder.go
+++ 04. MSA Example/tool/encoder/nats/json_encoder.go
@@ -1,1 +1,18 @@
-package nats
+package natsEncoder
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type jsonEncoder struct {
+	*json.Encoder
+	proxy io.Writer
+}
+
+func NewJsonEncoder(proxy io.Writer) *jsonEncoder {
+	return &jsonEncoder{
+		Encoder: json.NewEncoder(proxy),
+		proxy:   proxy,
+	}
+}
