a758166ab9f40927cc4535f008b3949a24b5e80e
지농 <jinhong0719@naver.com>
2020-05-18 19:19:56 +0900

    [MODIFY] <cmd/auth_microservice/main.go> 핸들러 내장함수 -> 외부 객체 메서드 변경 및 로깅추가

--- 04. MSA Example/cmd/auth_microservice/main.go
+++ 04. MSA Example/cmd/auth_microservice/main.go
@@ -1,38 +1,35 @@
 package main
 
 import (
-	"MSA.example.com/1/dataservice"
 	"MSA.example.com/1/dataservice/userdata"
+	"MSA.example.com/1/tool/dbc"
 	"MSA.example.com/1/tool/message"
-	"fmt"
-	"github.com/nats-io/nats.go"
+	"MSA.example.com/1/usecase"
+	"github.com/go-playground/validator/v10"
 	"log"
 	"runtime"
 )
 
 func main() {
-	dbc, err := dataservice.ConnectDB("AuthMicroservice")
+	db, err := dbc.ConnectDB("AuthMicroservice")
 	if err != nil {
 		log.Fatalf("unable to connect DB server, err: %v\n", err)
 	}
 
-	_ = userdata.GetUserDAO(dbc)
+	userD := userdata.GetUserDAO(db)
 	natsM, err := message.GetDefaultNatsByEnv()
 	if err != nil {
 		log.Fatalf("unable to connect NATS server, err: %v\n", err)
 	}
+	validate := validator.New()
+	u := usecase.NewAuthDefaultUseCase(userD, natsM, validate)
 
-	_, err = natsM.Subscribe("auth.signup", signUpMsgHandler)
+	_, err = natsM.Subscribe("auth.signup", u.SignUpMsgHandler)
 	// handler들을 메서드로 가지고 있는 usecase 구조체 생성 추가 (db, nats 필드 소유)
 	if err != nil {
 		log.Fatalf("unable to subscribe auth.login from nats message broker, err: %v\n", err)
 	}
+	log.Println("Auth message pub/sub server is completely started.")
 	runtime.Goexit()
 }
 
-func signUpMsgHandler(msg *nats.Msg) {
-	fmt.Println(string(msg.Data))
-	msg.Reply = "auth.signup.reply"
-	err := msg.Respond([]byte("ok"))
-	fmt.Println(err)
-}
