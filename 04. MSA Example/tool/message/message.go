fb204742fbfe1638fb739a3daf3fceaa10d700e1
지농 <jinhong0719@naver.com>
2020-05-18 19:16:13 +0900

    [ADD] <tool/message/message.go> NatsMessage 인터페이스 Request 메서드 추가

--- 04. MSA Example/tool/message/message.go
+++ 04. MSA Example/tool/message/message.go
@@ -1,8 +1,12 @@
 package message
 
-import "github.com/nats-io/nats.go"
+import (
+	"github.com/nats-io/nats.go"
+	"time"
+)
 
 type NatsMessage interface {
 	Publish(subj string, data []byte) error
 	Subscribe(subj string, cb nats.MsgHandler) (*nats.Subscription, error)
+	Request(subj string, data []byte, timeout time.Duration) (*nats.Msg, error)
 }
