6f2d8242b98953f943fe851359c3638af5eeb559
지농 <jinhong0719@naver.com>
2020-05-20 22:04:28 +0900

    [ADD] <usecase/api_gateway_usecase.go> validator 에러에 대한 assertion 구문 추가

--- 04. MSA Example/usecase/api_gateway_usecase.go
+++ 04. MSA Example/usecase/api_gateway_usecase.go
@@ -14,9 +14,9 @@ import (
 )
 
 type authServiceHandler struct {
-	natsM message.NatsMessage
-	validate *validator.Validate
-	natsE natsEncoder.Encoder
+	natsM 		message.NatsMessage
+	validate 	*validator.Validate
+	natsE 		natsEncoder.Encoder
 }
 
 func NewAuthServiceHandler(nastM message.NatsMessage, validator *validator.Validate, natsE natsEncoder.Encoder) *authServiceHandler {
@@ -52,24 +52,33 @@ func (h *authServiceHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) 
 		// 타임아웃, 회로 차단기 구현
 		// auth_microservice 응답 코드 추가
 		// usecase 트랜직셔널, 트랜잭셔널 메시징 기능 추가
-		err = h.natsE.Encode(protocol.AuthSignUpProtocol{
-			RequestId:     r.Header.Get("X-Request-ID"),
-			UserId:        request.UserId,
-			UserPwd:       request.UserPwd,
-			Name:          request.Name,
-			PhoneNumber:   request.PhoneNumber,
-			Introduction:  request.Introduction,
-			Email:         request.Email,
-			ReturnChannel: "auth.signup.return",
-			InputChannel:  "auth.signup",
+		err = h.natsE.Encode(protocol.AuthSignUpRequestProtocol{
+			Required: protocol.RequiredProtocol{
+				Usage:        "AuthSignUpRequest",
+				InputChannel: "auth.signup",
+			},
+			RequestId:    r.Header.Get("X-Request-ID"),
+			UserId:       request.UserId,
+			UserPwd:      request.UserPwd,
+			Name:         request.Name,
+			PhoneNumber:  request.PhoneNumber,
+			Introduction: request.Introduction,
+			Email:        request.Email,
 		})
 
 		enErr := err.(*customError.ProxyWriteError)
 		if enErr.Err != nil {
 			log.Printf("some error occurs while encoding to message, err: %v\n", err)
-			rw.WriteHeader(http.StatusInternalServerError)
+			switch enErr.Err.(type) {
+			case validator.ValidationErrors:
+				rw.WriteHeader(http.StatusBadRequest)
+			default:
+				rw.WriteHeader(http.StatusInternalServerError)
+			}
 			return
 		}
+
+		// ApiGateSignUpResponseProtocol Unmarshal 및 처리 로직 추가
 		rw.WriteHeader(http.StatusOK)
 		return
 
