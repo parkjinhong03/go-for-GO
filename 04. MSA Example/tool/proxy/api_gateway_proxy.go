960f27e97525e24566686b42a4470bcc2253554a
지농 <jinhong0719@naver.com>
2020-05-20 21:59:38 +0900

    [ADD] <tool/proxy/api_gateway_proxy.go> ApiGatewayProxy객체 Write 메서드 구현

--- 04. MSA Example/tool/proxy/api_gateway_proxy.go
+++ 04. MSA Example/tool/proxy/api_gateway_proxy.go
@@ -1,18 +1,38 @@
 package proxy
 
 import (
+	"MSA.example.com/1/protocol"
 	"MSA.example.com/1/tool/message"
+	"encoding/json"
+	"errors"
 	"github.com/go-playground/validator/v10"
 )
 
-type apiGatewayProxy struct {
+type ApiGatewayProxy struct {
 	natsM message.NatsMessage
 	validate *validator.Validate
 }
 
-func NewApiGatewayProxy(nastM message.NatsMessage, validate *validator.Validate) *apiGatewayProxy {
-	return &apiGatewayProxy{
+func NewApiGatewayProxy(nastM message.NatsMessage, validate *validator.Validate) *ApiGatewayProxy {
+	return &ApiGatewayProxy{
 		natsM:    nastM,
 		validate: validate,
 	}
+}
+
+func (ap *ApiGatewayProxy) Write(b []byte) (i int, err error) {
+	r := struct {
+		Required protocol.RequiredProtocol
+	}{}
+	if err = json.Unmarshal(b, &r); err != nil { return }
+	if err = ap.validate.Struct(&r); err != nil { return }
+
+
+	switch r.Required.Usage {
+	case "AuthSignUpResponse":
+		err = ap.natsM.Publish(r.Required.InputChannel, b)
+	default:
+		err = errors.New("this Usage is undefined so cannot be processed")
+	}
+	return
 }
