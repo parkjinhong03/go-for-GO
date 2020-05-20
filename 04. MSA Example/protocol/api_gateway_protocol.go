3ad5dbe644ffaf63ab7bb30bac1dd90cd9aed3ec
지농 <jinhong0719@naver.com>
2020-05-20 22:00:45 +0900

    [MODIFY] <protocol/api_protocol/protocol.go> protocol 구조체 필드 정렬

--- 04. MSA Example/protocol/api_gateway_protocol.go
+++ 04. MSA Example/protocol/api_gateway_protocol.go
@@ -3,9 +3,9 @@ package protocol
 import "MSA.example.com/1/model"
 
 type ApiGatewaySignUpResponseProtocol struct {
-	Required   RequiredProtocol	`validate:"required"`
-	RequestId  string	`validate:"required"`
-	ResultUser *model.Users	`validate:"required"`
-	Success    bool	`validate:"required"`
-	ErrorCode  int	`validate:"required"`
+	Required   RequiredProtocol
+	RequestId  string       `validate:"required"`
+	ResultUser *model.Users `validate:"required"`
+	Success    bool         `validate:"required"`
+	ErrorCode  string       `validate:"required"`
 }
