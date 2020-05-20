8f1f4d5f6cc0e9e9e23e82a32789495c1e5d3cb2
지농 <jinhong0719@naver.com>
2020-05-20 22:06:20 +0900

    [ADD] <usecase/auth_usecase.go> protocol 객체 생성 및 초기화 & Encode 메서드 호출 구문 추가

--- 04. MSA Example/usecase/auth_usecase.go
+++ 04. MSA Example/usecase/auth_usecase.go
@@ -4,7 +4,7 @@ import (
 	"MSA.example.com/1/dataservice"
 	"MSA.example.com/1/model"
 	"MSA.example.com/1/protocol"
-	"MSA.example.com/1/tool/message"
+	natsEncoder "MSA.example.com/1/tool/encoder/nats"
 	"encoding/json"
 	"github.com/go-playground/validator/v10"
 	"github.com/jinzhu/gorm"
@@ -13,21 +13,22 @@ import (
 )
 
 type authDefaultUseCase struct {
-	userD 		dataservice.UserDataService
-	natsM 		message.NatsMessage
-	validate 	*validator.Validate
+	userD    dataservice.UserDataService
+	validate *validator.Validate
+	agNatsE  natsEncoder.Encoder
 }
 
-func NewAuthDefaultUseCase(userD dataservice.UserDataService, natsM message.NatsMessage, validate *validator.Validate) *authDefaultUseCase {
+func NewAuthDefaultUseCase(
+	userD dataservice.UserDataService, validate *validator.Validate, agNatsE natsEncoder.Encoder) *authDefaultUseCase {
 	return &authDefaultUseCase{
-		userD: 		userD,
-		natsM: 		natsM,
-		validate: 	validate,
+		userD:    userD,
+		validate: validate,
+		agNatsE:  agNatsE,
 	}
 }
 
 func (h *authDefaultUseCase) SignUpMsgHandler(msg *nats.Msg) {
-	data := protocol.AuthSignUpProtocol{}
+	data := protocol.AuthSignUpRequestProtocol{}
 	if err := json.Unmarshal(msg.Data, &data); err != nil  {
 		log.Printf("something occurs error while unmarshal json byte in struct, err: %v\n", err)
 		return
@@ -38,15 +39,34 @@ func (h *authDefaultUseCase) SignUpMsgHandler(msg *nats.Msg) {
 	}
 
 	user := model.Users{
-		Model:        gorm.Model{},
-		UserId:       data.UserId,
-		UserPwd:      data.UserPwd,
+		Model:   gorm.Model{},
+		UserId:  data.UserId,
+		UserPwd: data.UserPwd,
 	}
-	_, err := h.userD.Insert(&user)
+	result, err := h.userD.Insert(&user)
+
+	p := protocol.ApiGatewaySignUpResponseProtocol{
+		Required:   protocol.RequiredProtocol{
+			Usage:        "AuthSignUpResponse",
+			InputChannel: msg.Reply,
+		},
+		RequestId:  data.RequestId,
+		ResultUser: result,
+		Success:    true,
+		ErrorCode:  "",
+	}
 
 	if err != nil {
 		log.Printf("unable to insert new user in database, err: %v\n", err)
-		_ = h.natsM.Publish(msg.Reply, []byte("test"))
+		// 에러 코드 파싱 추가
+		p.ResultUser = nil
+		p.Success = false
+		p.ErrorCode = err.Error()
+	}
+
+	err = h.agNatsE.Encode(p)
+	if err != nil {
+		log.Printf("some error occurs while proccessing that send message from auth.signup, err: %v\n", err)
 	}
 	return
 }
