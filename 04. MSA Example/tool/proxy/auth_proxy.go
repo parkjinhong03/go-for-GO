a65431ccb2f8abd5f4e4dda7397ae701ed1d3521
지농 <jinhong0719@naver.com>
2020-05-20 20:58:48 +0900

    [MODIFY] <tool/proxy/auth_proxy.go> Required 정보 바인딩 객체 수정 및 유효성 검사 코드 추가

---
+++ 04. MSA Example/tool/proxy/auth_proxy.go
@@ -0,0 +1,56 @@
+package proxy
+
+import (
+	"MSA.example.com/1/protocol"
+	"MSA.example.com/1/tool/customError"
+	"MSA.example.com/1/tool/message"
+	"encoding/json"
+	"errors"
+	"github.com/go-playground/validator/v10"
+	"time"
+)
+
+type AuthServiceProxy struct {
+	natsM 	 message.NatsMessage
+	validate *validator.Validate
+}
+
+func NewAuthServiceProxy(natsM message.NatsMessage, validate *validator.Validate) *AuthServiceProxy {
+	return &AuthServiceProxy{
+		natsM: natsM,
+		validate: validate,
+	}
+}
+
+func (ap *AuthServiceProxy) Write(b []byte) (int, error) {
+	myErr := &customError.ProxyWriteError{}
+	
+	r := struct {
+		Required protocol.RequiredProtocol
+	}{}
+	if err := json.Unmarshal(b, &r); err != nil {
+		myErr.Err = err
+		return 0, myErr
+	}
+	// json 외의 포맷팅 방식을 이용한 Unmarshal 추가 필요
+	if err := ap.validate.Struct(&r); err != nil {
+		myErr.Err = err
+		return 0, myErr
+	}
+
+	switch r.Required.Usage {
+	case "AuthSignUp":
+		msg, err := ap.natsM.Request(r.Required.InputChannel, b, 1 * time.Second)
+		if err != nil {
+			myErr.Err = err
+			return 0, myErr
+		}
+		myErr.ReturnMsg = msg
+
+	default:
+		err := errors.New("this channel is currently unavailable")
+		myErr.Err = err
+	}
+
+	return 0, myErr
+}
