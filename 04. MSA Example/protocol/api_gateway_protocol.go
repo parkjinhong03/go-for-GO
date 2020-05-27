d0020cdc580205f883d9e7beb101abcd8ce8f463
지농 <jinhong0719@naver.com>
2020-05-27 22:22:26 +0900

    [MODIFY] <protocol/api_gateway_protocol.go> 일부 필드 validate:"required" 제거

--- 04. MSA Example/protocol/api_gateway_protocol.go
+++ 04. MSA Example/protocol/api_gateway_protocol.go
@@ -5,7 +5,7 @@ import "MSA.example.com/1/model"
 type ApiGatewaySignUpResponseProtocol struct {
 	Required   RequiredProtocol
 	RequestId  string       `validate:"required"`
-	ResultUser *model.Users `validate:"required"`
-	Success    bool         `validate:"required"`
-	ErrorCode  string       `validate:"required"`
+	ResultUser *model.Users
+	Success    bool
+	ErrorCode  int
 }
