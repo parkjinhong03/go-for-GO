4c9232d2d517e43df8c7e279a450b7361bf9b8a2
지농 <jinhong0719@naver.com>
2020-05-28 19:52:13 +0900

    [MODIFY] <usecase/apiGatewayUsecase/auth_service.go> signup 처리 로직 함수로 분리

--- 04. MSA Example/usecase/apiGatewayUsecase/auth_service.go
+++ 04. MSA Example/usecase/apiGatewayUsecase/auth_service.go
@@ -35,91 +35,95 @@ func (h *authServiceHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) 
 	}
 	paths := strings.Split(r.URL.Path, "/")
 
-	d := json.NewDecoder(r.Body)
 	switch paths[0] {
 	case "signup":
-		request := entities.SignUpRequestEntities{}
-		err := d.Decode(&request)
-		if err != nil {
-			rw.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		err = h.validate.Struct(&request)
-		if err != nil {
-			rw.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		// 타임아웃, 회로 차단기 구현
-		// auth_microservice 응답 코드 추가
-		// usecase 트랜직셔널, 트랜잭셔널 메시징 기능 추가
-		enErr := h.natsE.Encode(protocol.AuthSignUpRequestProtocol{
-			Required: protocol.RequiredProtocol{
-				Usage:        "AuthSignUpRequest",
-				InputChannel: "auth.signup",
-			},
-			RequestId:    r.Header.Get("X-Request-ID"),
-			UserId:       request.UserId,
-			UserPwd:      request.UserPwd,
-			Name:         request.Name,
-			PhoneNumber:  request.PhoneNumber,
-			Introduction: request.Introduction,
-			Email:        request.Email,
-		}).(*customError.ProxyWriteError)
-
-		if enErr.Err != nil {
-			switch enErr.Err.(type) {
-			case validator.ValidationErrors:
-				rw.WriteHeader(http.StatusBadRequest)
-			default:
-				log.Printf("some error occurs while encoding to message, err: %v\n", enErr.Err)
-				rw.WriteHeader(http.StatusInternalServerError)
-			}
-			return
-		}
-
-		p := protocol.ApiGatewaySignUpResponseProtocol{}
-		if err := json.Unmarshal(enErr.ReturnMsg.Data, &p); err != nil {
-			log.Printf("some error occurs while decoding message into struct, err: %v\n", err)
-			rw.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if err := h.validate.Struct(&p); err != nil {
-			log.Printf("the message recived from auth.signup is invalid, err: %v\n", err)
-			rw.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		if p.Success {
-			reply := entities.SignUpResponseEntities{
-				StatusCode: http.StatusOK,
-				ResultUser: p.ResultUser,
-			}
-			encoder := json.NewEncoder(rw)
-
-			if encoder.Encode(reply) != nil {
-				rw.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-		}
-
-		// 에러 코드 상수화 추가
-		switch p.ErrorCode {
-		case usecase.UserIdDuplicateErrorCode:
-			rw.WriteHeader(470)
-			return
-		case usecase.ParsingFailureErrorCode:
-			log.Println("undefined error code")
-			rw.WriteHeader(http.StatusInternalServerError)
-			return
-		default:
-			log.Println("undefined error code")
-			rw.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
+		h.SignUpHandler(rw, r)
 	default:
 		rw.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func (h *authServiceHandler) SignUpHandler(rw http.ResponseWriter, r *http.Request) {
+	d := json.NewDecoder(r.Body)
+	request := entities.SignUpRequestEntities{}
+	err := d.Decode(&request)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.validate.Struct(&request)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// 타임아웃, 회로 차단기 구현
+	// auth_microservice 응답 코드 추가
+	// usecase 트랜직셔널, 트랜잭셔널 메시징 기능 추가
+	enErr := h.natsE.Encode(protocol.AuthSignUpRequestProtocol{
+		Required: protocol.RequiredProtocol{
+			Usage:        "AuthSignUpRequest",
+			InputChannel: "auth.signup",
+		},
+		RequestId:    r.Header.Get("X-Request-ID"),
+		UserId:       request.UserId,
+		UserPwd:      request.UserPwd,
+		Name:         request.Name,
+		PhoneNumber:  request.PhoneNumber,
+		Introduction: request.Introduction,
+		Email:        request.Email,
+	}).(*customError.ProxyWriteError)
+
+	if enErr.Err != nil {
+		switch enErr.Err.(type) {
+		case validator.ValidationErrors:
+			rw.WriteHeader(http.StatusBadRequest)
+		default:
+			log.Printf("some error occurs while encoding to message, err: %v\n", enErr.Err)
+			rw.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	p := protocol.ApiGatewaySignUpResponseProtocol{}
+	if err := json.Unmarshal(enErr.ReturnMsg.Data, &p); err != nil {
+		log.Printf("some error occurs while decoding message into struct, err: %v\n", err)
+		rw.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err := h.validate.Struct(&p); err != nil {
+		log.Printf("the message recived from auth.signup is invalid, err: %v\n", err)
+		rw.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if p.Success {
+		rw.WriteHeader(http.StatusCreated)
+		reply := entities.SignUpResponseEntities{
+			StatusCode: http.StatusOK,
+			ResultUser: p.ResultUser,
+		}
+		encoder := json.NewEncoder(rw)
+
+		if encoder.Encode(reply) != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		return
+	}
+
+	// 에러 코드 상수화 추가
+	switch p.ErrorCode {
+	case usecase.UserIdDuplicateErrorCode:
+		rw.WriteHeader(470)
+		return
+	case usecase.ParsingFailureErrorCode:
+		log.Println("auth.signup fail to parse error code")
+		rw.WriteHeader(http.StatusInternalServerError)
+		return
+	default:
+		log.Println("undefined error code's come from auth.signup")
+		rw.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 }
