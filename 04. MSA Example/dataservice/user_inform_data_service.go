0e8b75eb656235ead0e4f5531a1a41e70f11d11a
지농 <jinhong0719@naver.com>
2020-06-01 19:28:29 +0900

    [ADD] <dataservice/user_inform_data_service.go> 인터페이스 구현을 위한 Insert 메서드 정의

--- 04. MSA Example/dataservice/user_inform_data_service.go
+++ 04. MSA Example/dataservice/user_inform_data_service.go
@@ -1,6 +1,9 @@
 package dataservice
 
-import "github.com/jinzhu/gorm"
+import (
+	"MSA.example.com/1/model"
+	"github.com/jinzhu/gorm"
+)
 
 type userInformDAO struct {
 	db *gorm.DB
@@ -10,4 +13,15 @@ func NewUserInformDAO(db *gorm.DB) *userInformDAO {
 	return &userInformDAO{
 		db: db,
 	}
+}
+
+func (ui *userInformDAO) Insert(userInform *model.UserInform) (*model.UserInform, error) {
+	var r *model.UserInform
+	txFunc := func(tx *gorm.DB) (err error) {
+		if tx = tx.Create(userInform); tx.Error == nil {
+			r = tx.Value.(*model.UserInform)
+		}
+		return tx.Error
+	}
+	return r, ui.db.Transaction(txFunc)
 }
