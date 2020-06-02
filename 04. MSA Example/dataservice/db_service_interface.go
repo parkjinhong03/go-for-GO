fa91b7884d1a31b86fcd133a0a9bed77a69f04e7
지농 <jinhong0719@naver.com>
2020-06-02 15:10:36 +0900

    [ADD] <dataservice/db_service_interface.go> UseDataService 인터페이스에 메서드 추가

--- 04. MSA Example/dataservice/db_service_interface.go
+++ 04. MSA Example/dataservice/db_service_interface.go
@@ -10,6 +10,7 @@ type UserDataService interface {
 	FindByUserId(userId string) (user *model.Users, err error)
 	Insert(user *model.Users) (result *model.Users, err error)
 	Remove(id uint32) (rowAffected int64, err error)
+	UpdateStatus(user *model.Users, status string) (result *model.Users, err error)
 }
 
 type UserInformDataService interface {
