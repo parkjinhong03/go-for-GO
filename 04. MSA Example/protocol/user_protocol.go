94e0d4f83c85f5782e538224190b553992481187
지농 <jinhong0719@naver.com>
2020-06-02 23:54:32 +0900

    [MODIFY] <protocol/user_protocol.go> 프로토콜에 ResultUserInform 필드 추가

--- 04. MSA Example/protocol/user_protocol.go
+++ 04. MSA Example/protocol/user_protocol.go
@@ -1,8 +1,11 @@
 package protocol
 
+import "MSA.example.com/1/model"
+
 type AuthRegistryResponseProtocol struct {
-	Required  RequiredProtocol
-	RequestId string `validate:"required"`
-	Success   bool
-	ErrorCode int
+	Required         RequiredProtocol
+	RequestId        string `validate:"required"`
+	ResultUserInform *model.UserInform
+	Success          bool
+	ErrorCode        int
 }
