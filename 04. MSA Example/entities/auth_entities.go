ee7a825552e52f3a505f9a9929217607be4a5d68
지농 <jinhong0719@naver.com>
2020-06-02 20:21:15 +0900

    [MODIFY] <entities/auth_entities.go> SignUpRequestEntities 필드 정렬

--- 04. MSA Example/entities/auth_entities.go
+++ 04. MSA Example/entities/auth_entities.go
@@ -3,12 +3,12 @@ package entities
 import "MSA.example.com/1/model"
 
 type SignUpRequestEntities struct {
-	UserId string `json:"user_id" validate:"required"`
-	UserPwd string `json:"user_pwd" validate:"required"`
-	Name string `json:"name" validate:"required"`
-	PhoneNumber string `json:"phone_number" validate:"required"`
+	UserId       string `json:"user_id" validate:"required"`
+	UserPwd      string `json:"user_pwd" validate:"required"`
+	Name         string `json:"name" validate:"required"`
+	PhoneNumber  string `json:"phone_number" validate:"required"`
 	Introduction string `json:"introduction"`
-	Email string `json:"email" validate:"email"`
+	Email        string `json:"email" validate:"email"`
 }
 
 type SignUpResponseEntities struct {
