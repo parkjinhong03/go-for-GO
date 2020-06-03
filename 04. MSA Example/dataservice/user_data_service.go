4b32dcf0fcb4f192bf4b8c40f57246c52db533db
지농 <jinhong0719@naver.com>
2020-06-03 19:50:49 +0900

    [MODIFY] <dataservice/user_data_service.go> Find, FindByUserId 로직 변경

--- 04. MSA Example/dataservice/user_data_service.go
+++ 04. MSA Example/dataservice/user_data_service.go
@@ -22,20 +22,24 @@ func GetUserDAO(db *gorm.DB) *userDAO {
 	return &userDAO{db: db}
 }
 
-func (u *userDAO) Find(id uint32) (*model.Users, error) {
-	user := model.Users{}
-	if u.db.Where("id = ?", id).Find(&user).RowsAffected != 0 {
-		return nil, errors.New("A user with that ID already exists")
+func (u *userDAO) Find(id uint) (user *model.Users, exist bool) {
+	user = &model.Users{}
+	if u.db.Where("id = ?", id).Find(user).RowsAffected == 0 {
+		exist = false
+	} else {
+		exist = true
 	}
-	return &user, nil
+	return
 }
 
-func (u *userDAO) FindByUserId(userId string) (*model.Users, error) {
-	user := model.Users{}
-	if u.db.Where("user_id = ?", userId).Find(&user).RowsAffected != 0 {
-		return nil, errors.New("A user with that ID already exists")
+func (u *userDAO) FindByUserId(userId string) (user *model.Users, exist bool) {
+	user = &model.Users{}
+	if u.db.Model(user).Where("user_id = ?", userId).First(user).RowsAffected == 0 {
+		exist = false
+	} else {
+		exist = true
 	}
-	return &user, nil
+	return
 }
 
 func (u *userDAO) Insert(user *model.Users) (*model.Users, error) {
@@ -50,7 +54,7 @@ func (u *userDAO) Insert(user *model.Users) (*model.Users, error) {
 }
 
 
-func (u *userDAO) Remove(id uint32) (int64, error) {
+func (u *userDAO) Remove(id uint) (int64, error) {
 	db := u.db.Where("id = ?", id).Delete(model.Users{})
 	if db.Error != nil {
 		return 0, db.Error
@@ -58,13 +62,13 @@ func (u *userDAO) Remove(id uint32) (int64, error) {
 	return db.RowsAffected, nil
 }
 
-func (u *userDAO) UpdateStatus(user *model.Users, status string) (*model.Users, error) {
-	var r *model.Users
-	txFunc := func(tx *gorm.DB) error {
-		if tx = tx.Model(user).Update("status", status); tx.Error == nil {
-			r = tx.Value.(*model.Users)
-		}
-		return tx.Error
+func (u *userDAO) UpdateStatus(id uint, status string) (*model.Users, error) {
+	user, exist := u.Find(id)
+	if !exist {
+		return nil, errors.New("A user with that ID is not exists")
 	}
-	return r, u.db.Transaction(txFunc)
+	if err := u.db.Model(user).Update("status", status).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
 }
