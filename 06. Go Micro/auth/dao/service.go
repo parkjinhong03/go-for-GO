38ca09031a8e8248e4819e70300ac2bde3f99da4
지농 <jinhong0719@naver.com>
2020-06-13 17:19:05 +0900

    [ADD] <auth/dao/service.go> UserDaoService 인터페이스 생성 및 UserDAO 반환 메서드 추가

--- 06. Go Micro/auth/dao/service.go
+++ 06. Go Micro/auth/dao/service.go
@@ -1,6 +1,8 @@
 package dao
 
 import (
+	"auth/dao/user"
+	"auth/model"
 	"github.com/jinzhu/gorm"
 )
 
@@ -11,5 +13,16 @@ type daoCreator struct {
 func NewDaoCreator(db *gorm.DB) *daoCreator {
 	return &daoCreator{
 		db: db,
+	}
+}
+
+type UserDAOService interface {
+	Insert(*model.User) (result *model.User, err error)
+}
+
+func (dc *daoCreator) GetUserDefaultDAO() UserDAOService {
+	tx := dc.db.New()
+	return &user.DefaultDAO{
+		DB: tx,
 	}
 }
