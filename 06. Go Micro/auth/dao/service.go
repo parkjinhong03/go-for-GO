d083f6c44fe69500c5163015906a15e90de893b8
지농 <jinhong0719@naver.com>
2020-06-14 13:14:18 +0900

    [ADD] <auth/dao/service.go> db Table 생성 및 컬럼 Migrate 기능 추가

--- 06. Go Micro/auth/dao/service.go
+++ 06. Go Micro/auth/dao/service.go
@@ -6,12 +6,16 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-type authDAOCreator struct {
+type AuthDAOCreator struct {
 	db *gorm.DB
 }
 
-func NewAuthDAOCreator(db *gorm.DB) *authDAOCreator {
-	return &authDAOCreator{
+func NewAuthDAOCreator(db *gorm.DB) *AuthDAOCreator {
+	if !db.HasTable(&model.Auth{}) {
+		db.CreateTable(&model.Auth{})
+	}
+	db.AutoMigrate(&model.Auth{})
+	return &AuthDAOCreator{
 		db: db,
 	}
 }
@@ -20,8 +24,8 @@ type UserDAOService interface {
 	Insert(*model.Auth) (result *model.Auth, err error)
 }
 
-func (dc *authDAOCreator) GetDefaultAuthDAO() UserDAOService {
-	tx := dc.db.New()
+func (dc *AuthDAOCreator) GetDefaultAuthDAO() UserDAOService {
+	tx := dc.db.Begin()
 	return &user.DefaultDAO{
 		DB: tx,
 	}
