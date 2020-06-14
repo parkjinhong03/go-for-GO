1f45df693b8ddad4743d255ca766c9a97dd2f76f
지농 <jinhong0719@naver.com>
2020-06-14 10:51:36 +0900

    [MODIFY] <auth/dao/service.go> DAO 생성 객체 이름 daoCreator -> authDAOCreator

--- 06. Go Micro/auth/dao/service.go
+++ 06. Go Micro/auth/dao/service.go
@@ -6,21 +6,21 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-type daoCreator struct {
+type authDAOCreator struct {
 	db *gorm.DB
 }
 
-func NewDaoCreator(db *gorm.DB) *daoCreator {
-	return &daoCreator{
+func NewAuthDAOCreator(db *gorm.DB) *authDAOCreator {
+	return &authDAOCreator{
 		db: db,
 	}
 }
 
 type UserDAOService interface {
-	Insert(*model.User) (result *model.User, err error)
+	Insert(*model.Auth) (result *model.Auth, err error)
 }
 
-func (dc *daoCreator) GetUserDefaultDAO() UserDAOService {
+func (dc *authDAOCreator) GetDefaultAuthDAO() UserDAOService {
 	tx := dc.db.New()
 	return &user.DefaultDAO{
 		DB: tx,
