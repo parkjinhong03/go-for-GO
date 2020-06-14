1f1f04872593de558b4833530b7df79107f1e739
지농 <jinhong0719@naver.com>
2020-06-14 16:30:36 +0900

    [ADD] <auth/dao/service.go> 서비스 interface 메서드 추가 및 TestAuthDAO 생성 함수 추가

--- 06. Go Micro/auth/dao/service.go
+++ 06. Go Micro/auth/dao/service.go
@@ -4,29 +4,42 @@ import (
 	"auth/dao/user"
 	"auth/model"
 	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/mock"
 )
 
 type AuthDAOCreator struct {
 	db *gorm.DB
 }
 
-func NewAuthDAOCreator(db *gorm.DB) *AuthDAOCreator {
+func NewAuthDAOCreator(db *gorm.DB) (adc *AuthDAOCreator) {
+	adc = &AuthDAOCreator{
+		db: db,
+	}
+	if db == nil {
+		return
+	}
 	if !db.HasTable(&model.Auth{}) {
 		db.CreateTable(&model.Auth{})
 	}
 	db.AutoMigrate(&model.Auth{})
-	return &AuthDAOCreator{
-		db: db,
-	}
+	return
 }
 
-type UserDAOService interface {
+type AuthDAOService interface {
 	Insert(*model.Auth) (result *model.Auth, err error)
+	Commit() *gorm.DB
+	Rollback() *gorm.DB
 }
 
-func (dc *AuthDAOCreator) GetDefaultAuthDAO() UserDAOService {
+func (dc *AuthDAOCreator) GetDefaultAuthDAO() AuthDAOService {
 	tx := dc.db.Begin()
 	return &user.DefaultDAO{
 		DB: tx,
+	}
+}
+
+func (dc *AuthDAOCreator) GetTestAuthDAO(mock mock.Mock) AuthDAOService {
+	return &user.TestDAO{
+		Mock: mock,
 	}
 }
