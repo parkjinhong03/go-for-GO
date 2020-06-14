a61d570389720b2a538d44c3d15a72e32d5b6857
지농 <jinhong0719@naver.com>
2020-06-14 10:52:47 +0900

    [ADD] <auth/main.go> db 연결 함수 호출 및 auth handler에 db 연결 의존성 주입

--- 06. Go Micro/auth/main.go
+++ 06. Go Micro/auth/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"auth/adapter/db"
 	"auth/handler"
 	auth "auth/proto/auth"
 	"auth/subscriber"
@@ -8,23 +9,25 @@ import (
 	log "github.com/micro/go-micro/v2/logger"
 )
 
+
 func main() {
-	// New Service
 	service := micro.NewService(
 		micro.Name("examples.blog.service.auth"),
 		micro.Version("latest"),
 	)
 
-	// Initialise service
-	service.Init()
+	conn, err := db.ConnMysql()
+	if err != nil {
+		log.Fatalf("unable to connect mysql server, err: %v\n", err)
+	}
+	h := handler.NewAuth(conn)
 
-	// Register Handler
-	auth.RegisterAuthHandler(service.Server(), new(handler.Auth))
-
-	// Register Struct as Subscriber
-	micro.RegisterSubscriber("examples.blog.service.auth", service.Server(), new(subscriber.Auth))
-
-	// Run service
+ 	if err := auth.RegisterAuthHandler(service.Server(), h); err != nil {
+ 		log.Fatal(err)
+	}
+	if err := micro.RegisterSubscriber("examples.blog.service.auth", service.Server(), new(subscriber.Auth)); err != nil {
+		log.Fatal(err)
+	}
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
 	}
