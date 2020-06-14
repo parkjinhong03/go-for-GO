588b06f86d144d368267b20f14090af5efaad56e
지농 <jinhong0719@naver.com>
2020-06-14 10:49:15 +0900

    [ADD] <auth/handler/auth.go> Call 메서드 삭제 및 CreateAuth 메서드 생성 & Auth 캡슐화

--- 06. Go Micro/auth/handler/auth.go
+++ 06. Go Micro/auth/handler/auth.go
@@ -2,28 +2,38 @@ package handler
 
 import (
 	"context"
+	"github.com/jinzhu/gorm"
+	"net/http"
 
 	log "github.com/micro/go-micro/v2/logger"
 
-	auth "auth/proto/auth"
+	proto "auth/proto/auth"
 )
 
-type Auth struct{}
+type auth struct{
+	db *gorm.DB
+}
 
-// Call is a single request handler called via client.Call or the generated client code
-func (e *Auth) Call(ctx context.Context, req *auth.Request, rsp *auth.Response) error {
-	log.Info("Received Auth.Call request")
-	rsp.Msg = "Hello " + req.Name
+func NewAuth(db *gorm.DB) *auth {
+	return &auth{
+		db: db,
+	}
+}
+
+func (e *auth) CreateAuth(ctx context.Context, req *proto.CreateAuthRequest, rsp *proto.CreateAuthResponse) error {
+	log.Info("Received Auth.CreateAuth request")
+	rsp.Status = http.StatusOK
+	rsp.Message = "test"
 	return nil
 }
 
 // Stream is a server side stream handler called via client.Stream or the generated client code
-func (e *Auth) Stream(ctx context.Context, req *auth.StreamingRequest, stream auth.Auth_StreamStream) error {
+func (e *auth) Stream(ctx context.Context, req *proto.StreamingRequest, stream proto.Auth_StreamStream) error {
 	log.Infof("Received Auth.Stream request with count: %d", req.Count)
 
 	for i := 0; i < int(req.Count); i++ {
 		log.Infof("Responding: %d", i)
-		if err := stream.Send(&auth.StreamingResponse{
+		if err := stream.Send(&proto.StreamingResponse{
 			Count: int64(i),
 		}); err != nil {
 			return err
@@ -34,14 +44,14 @@ func (e *Auth) Stream(ctx context.Context, req *auth.StreamingRequest, stream au
 }
 
 // PingPong is a bidirectional stream handler called via client.Stream or the generated client code
-func (e *Auth) PingPong(ctx context.Context, stream auth.Auth_PingPongStream) error {
+func (e *auth) PingPong(ctx context.Context, stream proto.Auth_PingPongStream) error {
 	for {
 		req, err := stream.Recv()
 		if err != nil {
 			return err
 		}
 		log.Infof("Got ping %v", req.Stroke)
-		if err := stream.Send(&auth.Pong{Stroke: req.Stroke}); err != nil {
+		if err := stream.Send(&proto.Pong{Stroke: req.Stroke}); err != nil {
 			return err
 		}
 	}
