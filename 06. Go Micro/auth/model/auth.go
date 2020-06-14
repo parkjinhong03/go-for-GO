b6589ae6657a65fe58ca703a185345e905ff132c
지농 <jinhong0719@naver.com>
2020-06-14 10:45:21 +0900

    [CREATE] <auth/model/auth.go> AuthDB의 Auth 릴레이션 속성 정의 구조체 생성

---
+++ 06. Go Micro/auth/model/auth.go
@@ -0,0 +1,10 @@
+package model
+
+import "github.com/jinzhu/gorm"
+
+type Auth struct {
+	gorm.Model
+	UserId string `gorm:"Type:varchar(20);NOT NULL;UNIQUE_INDEX"`
+	UserPw string `gorm:"Type:varchar(50);NOT NULL"`
+	Status string `gorm:"Type:varchar(20);NOT NULL"`
+}
