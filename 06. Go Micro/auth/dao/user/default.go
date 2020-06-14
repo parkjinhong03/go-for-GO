ad051e1e93128bb481a959e6558790a305f9bed9
지농 <jinhong0719@naver.com>
2020-06-14 13:12:52 +0900

    [ADD] <auth/dao/user/default.go> DefaultDAO 객체 Insert 메서드 생성

--- 06. Go Micro/auth/dao/user/default.go
+++ 06. Go Micro/auth/dao/user/default.go
@@ -1,7 +1,18 @@
 package user
 
-import "github.com/jinzhu/gorm"
+import (
+	"auth/model"
+	"github.com/jinzhu/gorm"
+)
 
 type DefaultDAO struct {
 	*gorm.DB
+}
+
+func (d *DefaultDAO) Insert(u *model.Auth) (*model.Auth, error) {
+	r := d.Create(u)
+	if r.Error != nil {
+		return nil, r.Error
+	}
+	return r.Value.(*model.Auth), nil
 }
