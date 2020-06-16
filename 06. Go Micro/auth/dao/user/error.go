e7a01208f2fdc0ae47fada56105aeadb3820b7f6
지농 <jinhong0719@naver.com>
2020-06-16 10:27:04 +0900

    [ADD] <auth/dao/user/error.go> BcryptGenerateError&DataLengthOverError 추가

--- 06. Go Micro/auth/dao/user/error.go
+++ 06. Go Micro/auth/dao/user/error.go
@@ -3,6 +3,8 @@ package user
 import "errors"
 
 var (
+	BcryptGenerateError = errors.New("bcrypt hash generate error")
 	IdDuplicateError = errors.New("user_id duplicate error")
+	DataLengthOverError = errors.New("data too long for column error")
 	UnknownError = errors.New("an unknown error")
 )
