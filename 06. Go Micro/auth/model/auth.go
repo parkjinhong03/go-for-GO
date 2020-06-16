76289f062ead75ce04b326d9f29a726d54f1a315
지농 <jinhong0719@naver.com>
2020-06-16 10:29:01 +0900

    [MODIFY] <auth/model/auth.go> UserPW 속성 도메인 varchar(50 ) -> varchar(100)

--- 06. Go Micro/auth/model/auth.go
+++ 06. Go Micro/auth/model/auth.go
@@ -5,6 +5,6 @@ import "github.com/jinzhu/gorm"
 type Auth struct {
 	gorm.Model
 	UserId string `gorm:"Type:varchar(20);NOT NULL;UNIQUE_INDEX"`
-	UserPw string `gorm:"Type:varchar(50);NOT NULL"`
+	UserPw string `gorm:"Type:varchar(100);NOT NULL"`
 	Status string `gorm:"Type:varchar(20);NOT NULL"`
 }
