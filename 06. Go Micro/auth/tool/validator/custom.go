466a1502b805c8a39fbcddcd0a7e0923f88a5a72
지농 <jinhong0719@naver.com>
2020-06-17 13:29:50 +0900

    [ADD] <auth/tool/validator/custom.go> (min, max)Length, length 태그 핸들러 함수 추가

--- 06. Go Micro/auth/tool/validator/custom.go
+++ 06. Go Micro/auth/tool/validator/custom.go
@@ -2,10 +2,45 @@ package validator
 
 import (
 	"github.com/go-playground/validator/v10"
+	"strconv"
+	"unicode/utf8"
 )
 
 func New() (validate *validator.Validate, err error) {
 	validate = validator.New()
+	if err = validate.RegisterValidation("minLength", minLengthValidator); err != nil { return }
+	if err = validate.RegisterValidation("maxLength", maxLengthValidator); err != nil { return }
+	if err = validate.RegisterValidation("length", lengthValidator); err != nil { return }
 
 	return
+}
+
+func minLengthValidator(fl validator.FieldLevel) bool {
+	paramLen, err := strconv.Atoi(fl.Param())
+	fieldLen := utf8.RuneCountInString(fl.Field().String())
+	if err != nil {
+		return false
+	}
+
+	return fieldLen > paramLen
+}
+
+func maxLengthValidator(fl validator.FieldLevel) bool {
+	paramLen, err := strconv.Atoi(fl.Param())
+	fieldLen := utf8.RuneCountInString(fl.Field().String())
+	if err != nil {
+		return false
+	}
+
+	return fieldLen < paramLen
+}
+
+func lengthValidator(fl validator.FieldLevel) bool {
+	fieldLen := len(strconv.Itoa(int(fl.Field().Int())))
+	paramLen, err := strconv.Atoi(fl.Param())
+	if err != nil {
+		return false
+	}
+
+	return fieldLen == paramLen
 }
