8edb30022bdd5245674fa406295f1bbba7fa8bdc
지농 <jinhong0719@naver.com>
2020-06-18 19:49:33 +0900

    [ADD] <auth/dao/service.go> AuthDAOService에 CheckIfUserIdExists 메서드 추가

--- 06. Go Micro/auth/dao/service.go
+++ 06. Go Micro/auth/dao/service.go
@@ -27,6 +27,7 @@ func NewAuthDAOCreator(db *gorm.DB) (adc *AuthDAOCreator) {
 
 type AuthDAOService interface {
 	Insert(*model.Auth) (result *model.Auth, err error)
+	CheckIfUserIdExists(id string) (exist bool)
 	Commit() *gorm.DB
 	Rollback() *gorm.DB
 }
