0739f9b3c394c8d67168c51d69f76667d5d878f7
지농 <jinhong0719@naver.com>
2020-06-20 23:26:50 +0900

    [ADD] <auth/handler/auth.go> proto 변경에 따른 CheckIfUserIdExist 메서드 추가 선언

--- 06. Go Micro/auth/handler/auth.go
+++ 06. Go Micro/auth/handler/auth.go
@@ -4,13 +4,15 @@ import (
 	"auth/dao"
 	"auth/dao/user"
 	"auth/model"
+	proto "auth/proto/auth"
+	"auth/tool/jwt"
 	"context"
 	"github.com/go-playground/validator/v10"
+	"github.com/micro/go-micro/v2/broker"
 	log "github.com/micro/go-micro/v2/logger"
 	"github.com/stretchr/testify/mock"
 	"net/http"
-
-	proto "auth/proto/auth"
+	"time"
 )
 
 const (
@@ -19,23 +21,24 @@ const (
 	StatusBcryptNotHashed = 571
 )
 
-type auth struct{
-	adc *dao.AuthDAOCreator
+type auth struct {
+	mq       broker.Broker
+	adc      *dao.AuthDAOCreator
 	validate *validator.Validate
 }
 
-func NewAuth(adc *dao.AuthDAOCreator, validate *validator.Validate) *auth {
+func NewAuth(mq broker.Broker, adc *dao.AuthDAOCreator, validate *validator.Validate) *auth {
 	return &auth{
+		mq:       mq,
 		adc:      adc,
 		validate: validate,
 	}
 }
 
-func (e *auth) CreateAuth(ctx context.Context, req *proto.CreateAuthRequest, rsp *proto.CreateAuthResponse) error {
+func (e *auth) CheckIfUserIdExist(ctx context.Context, req *proto.UserIdExistRequest, rsp *proto.UserIdExistResponse) (_ error) {
 	if err := e.validate.Struct(req); err != nil {
-		rsp.Status = http.StatusBadRequest
-		rsp.Message = err.Error()
-		return nil
+		rsp.SetResponse(http.StatusBadRequest, err.Error())
+		return
 	}
 
 	var ad dao.AuthDAOService
@@ -47,7 +50,63 @@ func (e *auth) CreateAuth(ctx context.Context, req *proto.CreateAuthRequest, rsp
 		ad = e.adc.GetDefaultAuthDAO()
 	}
 
-	_, err := ad.Insert(&model.Auth{
+	exist, err := ad.CheckIfUserIdExists(req.UserId)
+	if err != nil {
+		rsp.SetResponse(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if exist {
+		rsp.SetResponse(StatusUserIdDuplicate, user.IdDuplicateError.Error())
+		return
+	}
+
+	ss, err := jwt.GenerateDuplicateCertJWT(req.UserId, time.Hour)
+	if err != nil {
+		rsp.SetResponse(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	rsp.SetResponse(http.StatusOK, "this user ID can be used")
+	rsp.Authorization = ss
+	return
+}
+
+func (e *auth) CreateAuth(ctx context.Context, req *proto.CreateAuthRequest, rsp *proto.CreateAuthResponse) (_ error) {
+	if err := e.validate.Struct(req); err != nil {
+		rsp.Status = http.StatusBadRequest
+		rsp.Message = err.Error()
+		return
+	}
+
+	var ad dao.AuthDAOService
+	switch ctx.Value("env") {
+	case "test":
+		mockStore := ctx.Value("mockStore").(*mock.Mock)
+		ad = e.adc.GetTestAuthDAO(mockStore)
+	default:
+		ad = e.adc.GetDefaultAuthDAO()
+	}
+
+	exist, err := ad.CheckIfUserIdExists(req.UserId)
+	if err != nil {
+		rsp.Status = http.StatusInternalServerError
+		rsp.Message = err.Error()
+		return
+	}
+
+	switch exist {
+	case true:
+		rsp.Status = StatusUserIdDuplicate
+		rsp.Message = user.IdDuplicateError.Error()
+	case false:
+		rsp.Status = http.StatusCreated
+		rsp.Message = "succeed in creating new auth"
+	}
+	return
+
+	// 고루틴으로 실행
+	_, err = ad.Insert(&model.Auth{
 		UserId: req.UserId,
 		UserPw: req.UserPw,
 	})
@@ -75,3 +134,4 @@ func (e *auth) CreateAuth(ctx context.Context, req *proto.CreateAuthRequest, rsp
 	log.Info("Received Auth.CreateAuth request")
 	return nil
 }
+
