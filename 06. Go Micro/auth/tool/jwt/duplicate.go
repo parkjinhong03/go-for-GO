ff2225721e75169af188a861342347d05a4b46aa
지농 <jinhong0719@naver.com>
2020-06-25 22:48:22 +0900

    [ADD] <auth/tool/jwt/duplicate.go> generate 함수에 email 매개변수 추가 및 에러 리턴X 함수 추가

--- 06. Go Micro/auth/tool/jwt/duplicate.go
+++ 06. Go Micro/auth/tool/jwt/duplicate.go
@@ -3,17 +3,19 @@ package jwt
 import (
 	"github.com/dgrijalva/jwt-go"
 	"io/ioutil"
+	"log"
 	"time"
 )
 
-func GenerateDuplicateCertJWT(userId string, d time.Duration) (ss string, err error) {
-	priv, err := ioutil.ReadFile("./jwt_key.priv")
+func GenerateDuplicateCertJWT(userId, email string, d time.Duration) (ss string, err error) {
+	priv, err := ioutil.ReadFile("/Users/parkjinhong/Desktop/go-for-GO/06. Go Micro/auth/tool/jwt/jwt_key.priv")
 	if err != nil {
 		return
 	}
 
 	claims := duplicateCertClaim{
-		UserId:         userId,
+		UserId: userId,
+		Email:  email,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: time.Now().Add(d).Unix(),
 		},
@@ -21,5 +23,27 @@ func GenerateDuplicateCertJWT(userId string, d time.Duration) (ss string, err er
 
 	t := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	ss, err = t.SignedString(priv)
+	return
+}
+
+func GenerateDuplicateCertJWTNoReturnErr(userId, email string, d time.Duration) (ss string) {
+	priv, err := ioutil.ReadFile("/Users/parkjinhong/Desktop/go-for-GO/06. Go Micro/auth/tool/jwt/jwt_key.priv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	claims := duplicateCertClaim{
+		UserId: userId,
+		Email:  email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(d).Unix(),
+		},
+	}
+
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err = t.SignedString(priv)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return
 }
