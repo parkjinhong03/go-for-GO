92d7ddb6ccd2f805698e06c57a5fc83cc0a4c5b4
지농 <jinhong0719@naver.com>
2020-06-27 15:56:02 +0900

    [CREATE] <auth/handler/const.go> Response의 Message에 대한 상수 모음 파일 및 상수 생성

--- 06. Go Micro/auth/handler/const.go
+++ 06. Go Micro/auth/handler/const.go
@@ -4,3 +4,19 @@ const (
 	StatusUserIdDuplicate = 470
 	StatusEmailDuplicate = 471
 )
+
+// BeforeCreateAuth 메서드에서만 사용하는 Message
+const (
+	MessageAuthCreated = "user created reservation has been successfully processed"
+	MessageBadRequest = "the request is not valid. please check the document"
+)
+
+// UserIdDuplicated 메서드에서만 사용하는 Message
+const (
+	MessageUserIdNotDuplicated = "you can use that user id"
+)
+
+const (
+	MessageUserIdDuplicate = "this user id is already in use"
+	MessageEmailDuplicate = "this email is already in use"
+)
