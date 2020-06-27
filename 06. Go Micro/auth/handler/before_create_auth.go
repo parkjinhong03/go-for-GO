bff6dbab9f7516ee8c8f5d57f5ef4e7cd076ee7e
지농 <jinhong0719@naver.com>
2020-06-27 17:21:48 +0900

    [CREATE] <auth/handler/before_create_auth.go> BeforeCreateAuth 메서드 분리

---
+++ 06. Go Micro/auth/handler/before_create_auth.go
@@ -0,0 +1,57 @@
+package handler
+
+import (
+	proto "auth/proto/auth"
+	"auth/subscriber"
+	"auth/tool/jwt"
+	"auth/tool/random"
+	"context"
+	"github.com/micro/go-micro/v2/broker"
+	"net/http"
+)
+
+func (e *auth) BeforeCreateAuth(ctx context.Context, req *proto.BeforeCreateAuthRequest, rsp *proto.BeforeCreateAuthResponse) (_ error) {
+	if err := e.validate.Struct(req); err != nil {
+		rsp.SetStatusAndMsg(http.StatusBadRequest, MessageBadRequest)
+		return
+	}
+
+	// test 환경 시 context에서 MessageId 추출, 아닐 시 새로 생성
+	var mId string
+	switch ctx.Value("env") {
+	case "test": 	mId = ctx.Value("MessageId").(string)
+	default:		mId = random.GenerateString(32)
+	}
+
+	claim, err := jwt.ParseDuplicateCertClaimFromJWT(req.Authorization)
+	if err != nil {
+		rsp.SetStatus(http.StatusForbidden)
+		return
+	}
+
+	if claim.UserId != req.UserId {
+		rsp.SetStatusAndMsg(StatusUserIdDuplicate, MessageUserIdDuplicate)
+		return
+	}
+
+	if claim.Email != req.Email {
+		rsp.SetStatusAndMsg(StatusEmailDuplicate, MessageEmailDuplicate)
+		return
+	}
+
+	header := make(map[string]string)
+	header["XRequestId"] = req.XRequestID
+	header["MessageId"] = mId
+
+	// body Marshaling 추가
+	if err = e.mq.Publish(subscriber.CreateAuthEventTopic, &broker.Message{
+		Header: header,
+		Body:   nil,
+	}); err != nil {
+		rsp.SetStatus(http.StatusInternalServerError)
+		return
+	}
+
+	rsp.SetStatusAndMsg(http.StatusCreated, MessageAuthCreated)
+	return
+}
