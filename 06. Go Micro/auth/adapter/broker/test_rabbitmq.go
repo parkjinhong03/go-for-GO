e184b8188e529d9b0d56dacc82a8945c16c3c98f
지농 <jinhong0719@naver.com>
2020-06-27 11:16:37 +0900

    [ADD] <auth/adapter/broker/test_rebbitmq.go> 구조체 이름 변경 및 Publish 메서드 오버라이딩

--- 06. Go Micro/auth/adapter/broker/test_rabbitmq.go
+++ 06. Go Micro/auth/adapter/broker/test_rabbitmq.go
@@ -5,13 +5,18 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-type testRabbitMq struct {
+type testRabbitMQ struct {
 	mock *mock.Mock
 	broker.Broker
 }
 
 func NewRabbitMQForTest(mock *mock.Mock) broker.Broker {
-	return testRabbitMq{
-		mock:   mock,
+	return &testRabbitMQ{
+		mock: mock,
 	}
+}
+
+func (r *testRabbitMQ) Publish(topic string, m *broker.Message, opts ...broker.PublishOption) error {
+	args := r.mock.Called(topic, m)
+	return args.Error(0)
 }
