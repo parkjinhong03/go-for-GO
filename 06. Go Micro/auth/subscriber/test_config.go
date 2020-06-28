952128b02dec3ee2e308bf6d70482cebafdda1c6
지농 <jinhong0719@naver.com>
2020-06-28 18:53:43 +0900

    [ADD] <auth/subscriber/test_config.go> broker.Event 구현체 생성 (CustomEvent)

--- 06. Go Micro/auth/subscriber/test_config.go
+++ 06. Go Micro/auth/subscriber/test_config.go
@@ -2,10 +2,13 @@ package subscriber
 
 import (
 	"auth/dao"
+	"auth/tool/validator"
+	"github.com/micro/go-micro/v2/broker"
 	"github.com/stretchr/testify/mock"
+	"log"
 )
 
-var mockStore mock.Mock
+var mockStore *mock.Mock
 var authId uint
 var h *msgHandler
 
@@ -19,12 +22,48 @@ const (
 )
 
 func init() {
-	mockStore = mock.Mock{}
+	mockStore = new(mock.Mock)
 	adc := dao.NewAuthDAOCreator(nil)
-	h = NewMsgHandler(adc)
+	validate, err := validator.New()
+	if err != nil { log.Fatal(err) }
+	h = NewMsgHandler(adc, validate)
 }
 
 func setUp() {
-	mockStore = mock.Mock{}
+	mockStore = new(mock.Mock)
 	authId = 0
+}
+
+type CustomEvent struct {
+	mock *mock.Mock
+	msg *broker.Message
+}
+
+func NewCustomEvent(mock *mock.Mock, header map[string]string, body []byte) *CustomEvent {
+	return &CustomEvent{
+		mock: mock,
+		msg: &broker.Message{
+			Header: header,
+			Body:   body,
+		},
+	}
+}
+
+func (e *CustomEvent) Ack() error {
+	args := e.mock.Called()
+	return args.Error(0)
+}
+
+func (e *CustomEvent) Error() error {
+	args := e.mock.Called()
+	return args.Error(0)
+}
+
+func (e *CustomEvent) Topic() string {
+	args := e.mock.Called()
+	return args.String(0)
+}
+
+func (e *CustomEvent) Message() *broker.Message {
+	return e.msg
 }
