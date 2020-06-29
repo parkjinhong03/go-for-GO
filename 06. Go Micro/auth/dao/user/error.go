a492d9ea40090f0aa4df99f7e450b6191f6a3ae6
지농 <jinhong0719@naver.com>
2020-06-29 21:42:03 +0900

    [ADD] <auth/dao/user/error.go> MsgIdDuplicateError 에러 추가

--- 06. Go Micro/auth/dao/user/error.go
+++ 06. Go Micro/auth/dao/user/error.go
@@ -5,6 +5,7 @@ import "errors"
 var (
 	BcryptGenerateError = errors.New("bcrypt hash generate error")
 	IdDuplicateError = errors.New("user_id duplicate error")
+	MsgIdDuplicateError = errors.New("msg_id duplicate error")
 	DataLengthOverError = errors.New("data too long for column error")
 	UnknownError = errors.New("an unknown error")
 )
