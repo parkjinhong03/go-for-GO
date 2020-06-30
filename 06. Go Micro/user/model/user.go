c074369cade52edc2ef8bdd9f1245741d4305231
지농 <jinhong0719@naver.com>
2020-06-30 10:19:59 +0900

    [CREATE] <user/model/user.go> Users 테이블 속성 정의 및 Users 테이블의 튜플로써 사용될 구조체 정의

---
+++ 06. Go Micro/user/model/user.go
@@ -0,0 +1,12 @@
+package model
+
+import "github.com/jinzhu/gorm"
+
+type User struct {
+	gorm.Model
+	AuthId uint `gorm:"Type:mediumint(10) UNSIGNED;NOT NULL;UNIQUE_INDEX"`
+	Name string `gorm:"Type:char(4);NOT NULL"`
+	PhoneNumber string `gorm:"char(11);NOT NULL"`
+	Email string `gorm:"varchar(30);NOT NULL"`
+	Introduction string `gorm:"varchar(100);NOT NULL"`
+}
