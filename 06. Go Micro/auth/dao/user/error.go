9115eeec04776b7bc76741f7b3de6450ee254942
지농 <jinhong0719@naver.com>
2020-07-01 19:27:38 +0900

    [REMOVE] <auth/dao/user/error.go> 사용하지 않는 에러인 UnknownError 전역 변수 삭제

--- 06. Go Micro/auth/dao/user/error.go
+++ 06. Go Micro/auth/dao/user/error.go
@@ -7,5 +7,4 @@ var (
 	IdDuplicateError = errors.New("user_id duplicate error")
 	MsgIdDuplicateError = errors.New("msg_id duplicate error")
 	DataLengthOverError = errors.New("data too long for column error")
-	UnknownError = errors.New("an unknown error")
 )
