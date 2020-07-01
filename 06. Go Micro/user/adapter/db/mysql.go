8f76ae886ba5e7f6f4249235f5a99e7eb8ad9322
지농 <jinhong0719@naver.com>
2020-07-01 19:21:09 +0900

    [CREATE] <user/adapter/db/mysql.go> MySQL 연결 객체 생성 및 반환 함수 생성

---
+++ 06. Go Micro/user/adapter/db/mysql.go
@@ -0,0 +1,15 @@
+package db
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"os"
+)
+
+func ConnMysql() (*gorm.DB, error) {
+	dbUser := os.Getenv("DB_USER")
+	dbPwd := os.Getenv("DB_PASSWORD")
+	dialect := fmt.Sprintf("%s:%s@/AuthDB?charset=utf8&parseTime=True&loc=Local", dbUser, dbPwd)
+
+	return gorm.Open("mysql", dialect)
+}
