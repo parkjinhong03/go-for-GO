4541a6768746c9814dadbdd3f6bd4a93af9f237c
지농 <jinhong0719@naver.com>
2020-07-01 19:25:13 +0900

    [ADD] <user/model/user.go> User 구조체의 Email 필드 gorm 태그에 UNIQUE_INDEX 추가

--- 06. Go Micro/user/model/user.go
+++ 06. Go Micro/user/model/user.go
@@ -7,6 +7,6 @@ type User struct {
 	AuthId uint `gorm:"Type:mediumint(10) UNSIGNED;NOT NULL;UNIQUE_INDEX"`
 	Name string `gorm:"Type:char(4);NOT NULL"`
 	PhoneNumber string `gorm:"char(11);NOT NULL"`
-	Email string `gorm:"varchar(30);NOT NULL"`
+	Email string `gorm:"varchar(30);NOT NULL;UNIQUE_INDEX"`
 	Introduction string `gorm:"varchar(100);NOT NULL"`
 }
