342681b86020b08be9c0ef87e7744aacde169c12
지농 <jinhong0719@naver.com>
2020-07-01 19:24:18 +0900

    [ADD] <user/dao/service.go> UserDAOService 추가 및 UserDAO 객체 생성 및 반환 메서드 추가

--- 06. Go Micro/user/dao/service.go
+++ 06. Go Micro/user/dao/service.go
@@ -2,8 +2,18 @@ package dao
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/mock"
+	"user/dao/user"
 	"user/model"
 )
+
+type UserDAOService interface {
+	InsertUser(*model.User) (result *model.User, err error)
+	InsertMessage(*model.ProcessedMessage) (result *model.ProcessedMessage, err error)
+	CheckIfEmailExist(email string) (exist bool, err error)
+	Commit() (db *gorm.DB)
+	Rollback() (db *gorm.DB)
+}
 
 type UserDAOCreator struct {
 	db *gorm.DB
@@ -25,3 +35,11 @@ func NewUserDAOCreator(db *gorm.DB) (udc *UserDAOCreator) {
 	return
 }
 
+func (udc *UserDAOCreator) GetDefaultUserDAO() UserDAOService {
+	tx := udc.db.Begin()
+	return user.NewDefaultDAO(tx)
+}
+
+func (udc *UserDAOCreator) GetTestUserDAO(mock *mock.Mock) UserDAOService {
+	return user.NewTestDAO(mock)
+}
