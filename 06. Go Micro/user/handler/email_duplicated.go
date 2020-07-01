54340c00c4223f04ed63aee1eb3f57bb0e07ec5f
지농 <jinhong0719@naver.com>
2020-07-01 19:26:48 +0900

    [CREATE] <user/handler/email_duplicated.go> EmailDuplicated 메서드 해당 파일로 분리

---
+++ 06. Go Micro/user/handler/email_duplicated.go
@@ -0,0 +1,10 @@
+package handler
+
+import (
+	"context"
+	proto "user/proto/user"
+)
+
+func (u *user) EmailDuplicated(ctx context.Context, req *proto.EmailDuplicatedRequest, resp *proto.EmailDuplicatedResponse) (_ error) {
+	return
+}
