ba0635a9c823902f9f8a4869667f86f01db1e353
지농 <jinhong0719@naver.com>
2020-07-02 20:11:03 +0900

    [ADD] <user/handler/email_duplicated_test.go> 테스트 환경 설정을 위한 메서드 생성

--- 06. Go Micro/user/handler/email_duplicated_test.go
+++ 06. Go Micro/user/handler/email_duplicated_test.go
@@ -15,3 +15,37 @@ type emailDuplicatedTest struct {
 	ExpectMethods       map[method]returns
 	ExpectAuthorization string
 }
+
+func (e emailDuplicatedTest) createTestFromForm() (test emailDuplicatedTest) {
+	test = e
+
+	if e.Email == none 			{ test.Email = "" } 	 	else if e.Email == "" 		  { test.Email = defaultEmail }
+	if e.Authorization == none  { test.Authorization = "" } else if e.Authorization == "" { test.Authorization = "" }
+	if e.XRequestId == none 	{ test.XRequestId = "" } 	else if e.XRequestId == "" 	  { test.XRequestId = uuid.New().String() }
+
+	return
+}
+
+func (e emailDuplicatedTest) setRequestContext(req *proto.EmailDuplicatedRequest) {
+	req.Email = e.Email
+	req.Authorization = e.Authorization
+	req.XRequestId = e.XRequestId
+	return
+}
+
+func (e emailDuplicatedTest) onExpectMethods() {
+	for method, returns := range e.ExpectMethods {
+		e.onMethod(method, returns)
+	}
+	return
+}
+
+func (e emailDuplicatedTest) onMethod(method method, returns returns) {
+	switch method {
+	case "CheckIfEmailExist":
+		mockStore.On("CheckIfEmailExist", e.Email).Return(returns...)
+	default:
+		log.Fatalf("%s method cannot be on booked\n", method)
+	}
+	return
+}
