1460e4a057395c8def190154f1d73b873b37ed1b
지농 <jinhong0719@naver.com>
2020-07-02 20:12:31 +0900

    [MODIFY] <user/adapter/db/mysql.go> DB 연결을 위한 dialect의 테이블 명시 부분의 오류 수정

--- 06. Go Micro/user/adapter/db/mysql.go
+++ 06. Go Micro/user/adapter/db/mysql.go
@@ -3,13 +3,14 @@ package db
 import (
 	"fmt"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"os"
 )
 
 func ConnMysql() (*gorm.DB, error) {
 	dbUser := os.Getenv("DB_USER")
 	dbPwd := os.Getenv("DB_PASSWORD")
-	dialect := fmt.Sprintf("%s:%s@/AuthDB?charset=utf8&parseTime=True&loc=Local", dbUser, dbPwd)
+	dialect := fmt.Sprintf("%s:%s@/UserDB?charset=utf8&parseTime=True&loc=Local", dbUser, dbPwd)
 
 	return gorm.Open("mysql", dialect)
 }
