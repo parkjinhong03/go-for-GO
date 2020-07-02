a2acb45a4cbf6421c61003204fdc96abe3f9b57a
지농 <jinhong0719@naver.com>
2020-07-02 22:42:31 +0900

    [ADD] <user/handler/email_duplicated.go> user 구조체의 EmailDuplicated 메서드 정의

--- 06. Go Micro/user/handler/email_duplicated.go
+++ 06. Go Micro/user/handler/email_duplicated.go
@@ -2,9 +2,54 @@ package handler
 
 import (
 	"context"
+	"github.com/stretchr/testify/mock"
+	"log"
+	"net/http"
+	"time"
+	"user/dao"
 	proto "user/proto/user"
+	"user/tool/jwt"
 )
 
-func (u *user) EmailDuplicated(ctx context.Context, req *proto.EmailDuplicatedRequest, resp *proto.EmailDuplicatedResponse) (_ error) {
+func (u *user) EmailDuplicated(ctx context.Context, req *proto.EmailDuplicatedRequest, rsp *proto.EmailDuplicatedResponse) (_ error) {
+	if err := u.validate.Struct(req); err != nil {
+		rsp.SetStatusAndMsg(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	var userId string
+	if req.Authorization != "" {
+		claim, err := jwt.ParseDuplicateCertClaimFromJWT(req.Authorization)
+		if err != nil { log.Fatal(err) }
+		userId = claim.UserId
+	}
+
+	var ud dao.UserDAOService
+	switch ctx.Value("env") {
+	case "test":
+		mockStore := ctx.Value("mockStore").(*mock.Mock)
+		ud = u.udc.GetTestUserDAO(mockStore)
+	default:
+		ud = u.udc.GetDefaultUserDAO()
+	}
+
+	exist, err := ud.CheckIfEmailExist(req.Email)
+	if err != nil {
+		rsp.SetStatus(http.StatusInternalServerError)
+		return
+	}
+
+	if exist {
+		rsp.SetStatusAndMsg(StatusEmailDuplicated, MessageEmailDuplicated)
+		return
+	}
+
+	ss, err := jwt.GenerateDuplicateCertJWT(userId, req.Email, time.Hour)
+	if err != nil {
+		rsp.SetStatus(http.StatusInternalServerError)
+	}
+
+	rsp.SetStatusAndMsg(http.StatusOK, MessageEmailNotDuplicated)
+	rsp.Authorization = ss
 	return
 }
