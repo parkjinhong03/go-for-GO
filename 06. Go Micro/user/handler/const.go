7053e4d9be34eaee1734d329cd1dc713b0c4a970
지농 <jinhong0719@naver.com>
2020-07-02 22:38:50 +0900

    [CREATE] <user/handler/const.go> 가독성을 위해 Message 및 StatusCode 상수로 선언

---
+++ 06. Go Micro/user/handler/const.go
@@ -0,0 +1,10 @@
+package handler
+
+const (
+	StatusEmailDuplicated = 471
+)
+
+const (
+	MessageEmailNotDuplicated = "you can use this email"
+	MessageEmailDuplicated = "this email is already in use"
+)
