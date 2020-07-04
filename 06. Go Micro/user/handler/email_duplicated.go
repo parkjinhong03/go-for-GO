f9f0c8ed67b7895fc941574a67bce3fd5c760127
지농 <jinhong0719@naver.com>
2020-07-04 14:38:45 +0900

    [ADD] <user/handler/email_duplicated.go> metadata를 이용해 Forbiden 처리 로직 추가

--- 06. Go Micro/user/handler/email_duplicated.go
+++ 06. Go Micro/user/handler/email_duplicated.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"context"
+	"github.com/google/uuid"
+	"github.com/micro/go-micro/v2/metadata"
 	"github.com/stretchr/testify/mock"
 	"net/http"
 	"time"
@@ -16,12 +18,31 @@ func (u *user) EmailDuplicated(ctx context.Context, req *proto.EmailDuplicatedRe
 		return
 	}
 
+	var md metadata.Metadata
+	var ok bool
+	if md, ok = metadata.FromContext(ctx); !ok || md == nil {
+		rsp.SetStatus(http.StatusForbidden)
+		return
+	}
+
+	var xReqId string
+	if xReqId, ok = md.Get("XRequestID"); !ok || xReqId == "" {
+		rsp.SetStatus(http.StatusForbidden)
+		return
+	}
+
+	if _, err := uuid.Parse(xReqId); err != nil {
+		rsp.SetStatus(http.StatusForbidden)
+		return
+	}
+
 	var userId string
-	if req.Authorization != "" {
-		claim, err := jwt.ParseDuplicateCertClaimFromJWT(req.Authorization)
+	if ss, ok := md.Get("Authorization"); ok && ss != "" {
+		claim, err := jwt.ParseDuplicateCertClaimFromJWT(ss)
 		if err != nil { rsp.SetStatus(http.StatusForbidden); return }
 		userId = claim.UserId
 	}
+
 
 	var ud dao.UserDAOService
 	switch ctx.Value("env") {
@@ -46,6 +67,7 @@ func (u *user) EmailDuplicated(ctx context.Context, req *proto.EmailDuplicatedRe
 	ss, err := jwt.GenerateDuplicateCertJWT(userId, req.Email, time.Hour)
 	if err != nil {
 		rsp.SetStatus(http.StatusInternalServerError)
+		return
 	}
 
 	rsp.SetStatusAndMsg(http.StatusOK, MessageEmailNotDuplicated)
