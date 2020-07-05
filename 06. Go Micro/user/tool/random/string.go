007bdd0ed8e9f6a743b4c5877813ab0437a16532
지농 <jinhong0719@naver.com>
2020-07-05 14:56:52 +0900

    [CREATE] <user/tool/random/string.go> 랜덤 문자열을 생성 및 반환해주는 함수 생성

---
+++ 06. Go Micro/user/tool/random/string.go
@@ -0,0 +1,14 @@
+package random
+
+import "math/rand"
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func GenerateString(length int) string {
+	s := make([]rune, length)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(s)
+}
