efb361cf6512f3872924f8d50caa619be25de435
지농 <jinhong0719@naver.com>
2020-07-05 14:59:46 +0900

    [CREATE] <user/subscriber/mock_event.go> broker.Event의 test용 mock 객체 생성

---
+++ 06. Go Micro/user/subscriber/mock_event.go
@@ -0,0 +1,40 @@
+package subscriber
+
+import (
+	"github.com/micro/go-micro/v2/broker"
+	"github.com/stretchr/testify/mock"
+)
+
+type CustomEvent struct {
+	mock *mock.Mock
+	msg *broker.Message
+}
+
+func (e *CustomEvent) Ack() error {
+	args := e.mock.Called()
+	return args.Error(0)
+}
+
+func (e *CustomEvent) Error() error {
+	args := e.mock.Called()
+	return args.Error(0)
+}
+
+func (e *CustomEvent) Topic() string {
+	args := e.mock.Called()
+	return args.String(0)
+}
+
+func (e *CustomEvent) Message() *broker.Message {
+	return e.msg
+}
+
+func (e *CustomEvent) setMessage(header map[string]string, body []byte) {
+	e.msg.Header = header
+	e.msg.Body = body
+}
+
+func (e *CustomEvent) clearMessage() {
+	e.msg.Header = map[string]string{}
+	e.msg.Body = []byte{}
+}
