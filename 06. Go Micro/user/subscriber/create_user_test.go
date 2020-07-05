fde1b8290dbecd5cc821eb935ae37f33e473ee0f
지농 <jinhong0719@naver.com>
2020-07-05 17:07:21 +0900

    [ADD] <user/subscriber/create_user_test.go> 생성&무효, 중복 메시지&권한X에 대한 테스트 함수 추가

--- 06. Go Micro/user/subscriber/create_user_test.go
+++ 06. Go Micro/user/subscriber/create_user_test.go
@@ -1,9 +1,17 @@
 package subscriber
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/google/uuid"
+	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"log"
+	"testing"
 	"time"
+	daoUser "user/dao/user"
 	"user/model"
 	proto "user/proto/user"
 	"user/tool/random"
@@ -37,6 +45,10 @@ func (c createUserTest) createTestFromForm() (test createUserTest) {
 		c.setUserContext(c.ExpectMethods["InsertUser"][0].(*model.User))
 	}
 
+	if _, ok := c.ExpectMethods["InsertMessage"]; ok {
+		c.setProcessedMessageContext(c.ExpectMethods["InsertMessage"][0].(*model.ProcessedMessage))
+	}
+
 	return
 }
 
@@ -51,6 +63,14 @@ func (c createUserTest) setUserContext(user *model.User) {
 	user.UpdatedAt = time.Now()
 
 	userId++
+}
+
+func (c createUserTest) setProcessedMessageContext(msg *model.ProcessedMessage) {
+	msg.ID = psMsgId
+	msg.MsgId = c.MessageID
+	msg.CreatedAt = time.Now()
+	msg.UpdatedAt = time.Now()
+	psMsgId++
 }
 
 func (c createUserTest) setMessageContext(msg *proto.CreateUserMessage) {
@@ -71,6 +91,7 @@ func (c createUserTest) onMethod(method method, returns returns) {
 	switch method {
 	case "InsertUser":
 		mockStore.On("InsertUser", &model.User{
+			AuthId:		  uint(c.AuthId),
 			Name:         c.Name,
 			PhoneNumber:  c.PhoneNumber,
 			Email:        c.Email,
@@ -114,5 +135,231 @@ func (c createUserTest) onMethod(method method, returns returns) {
 	// 분산 추적 관련 메서드 추가
 	default:
 		panic(fmt.Sprintf("%s method cannot be on booked\n", method))
+	}
+}
+
+func (c createUserTest) generateMsgHeader() (header map[string]string) {
+	header = make(map[string]string)
+	header["XRequestID"] = c.XRequestID
+	header["MessageID"] = c.MessageID
+	header["AfterMessageID"] = c.AfterMessageID
+	header["Env"] = "Test"
+
+	return
+}
+
+func TestCreateUserValidMessage(t *testing.T) {
+	setUpEnv()
+	msg := &proto.CreateUserMessage{}
+	var tests []createUserTest
+
+	forms := []createUserTest{
+		{
+			AuthId: 1,
+			ExpectMethods: map[method]returns{
+				"InsertMessage": {&model.ProcessedMessage{}, nil},
+				"InsertUser":    {&model.User{}, nil},
+				"Commit":        {&gorm.DB{}},
+				"Ack":           {nil},
+			},
+			ExpectError: nil,
+		}, {
+			AuthId: 2,
+			ExpectMethods: map[method]returns{
+				"InsertMessage": {&model.ProcessedMessage{}, nil},
+				"InsertUser":    {&model.User{}, nil},
+				"Commit":        {&gorm.DB{}},
+				"Ack":           {errors.New("unable to ack this message")},
+			},
+			ExpectError: nil,
+		}, {
+			AuthId: 3,
+			ExpectMethods: map[method]returns{
+				"InsertMessage": {&model.ProcessedMessage{}, nil},
+				"InsertUser":    {&model.User{}, errors.New("unable to insert user")},
+				"Rollback":      {&gorm.DB{}},
+			},
+			ExpectError: nil,
+		}, {
+			AuthId: 4,
+			ExpectMethods: map[method]returns{
+				"InsertMessage": {&model.ProcessedMessage{}, errors.New("can't read this table")},
+			},
+			ExpectError: nil,
+		},
+	}
+
+	for _, form := range forms {
+		tests = append(tests, form.createTestFromForm())
+	}
+
+	for _, test := range tests {
+		mockStore = mock.Mock{}
+
+		test.setMessageContext(msg)
+		test.onExpectMethods()
+
+		header := test.generateMsgHeader()
+		body, err := json.Marshal(msg)
+		if err != nil { log.Fatal(err) }
+		event.setMessage(header, body)
+
+		err = h.CreateUser(event)
+		assert.Equalf(t, test.ExpectError, err, "error assertion error (test case: %v)\n", test)
+
+		mockStore.AssertExpectations(t)
+	}
+}
+
+func TestCreateUserUnmarshalErrorMessage(t *testing.T) {
+	setUpEnv()
+	msg := &proto.CreateUserMessage{}
+	var tests []createUserTest
+
+	forms := []createUserTest{{ExpectError: ErrorBadRequest}}
+
+	for _, form := range forms {
+		tests = append(tests, form.createTestFromForm())
+	}
+
+	for _, test := range tests {
+		mockStore = mock.Mock{}
+
+		test.setMessageContext(msg)
+		test.onExpectMethods()
+
+		header := test.generateMsgHeader()
+		body := []byte("unableToUnmarshalThisByteArrToStruct")
+		event.setMessage(header, body)
+
+		err := h.CreateUser(event)
+		assert.Equalf(t, test.ExpectError, err, "error assert error (test case: %v)\n", test)
+		mockStore.AssertExpectations(t)
+	}
+}
+
+func TestCreateAuthDuplicatedMessage(t *testing.T) {
+	setUpEnv()
+	msg := &proto.CreateUserMessage{}
+	var tests []createUserTest
+
+	forms := []createUserTest{
+		{
+			ExpectMethods: map[method]returns{
+				"InsertMessage": {&model.ProcessedMessage{}, daoUser.EmailDuplicatedError},
+			},
+			ExpectError: ErrorMsgDuplicated,
+		},
+	}
+
+	for _, form := range forms {
+		tests = append(tests, form.createTestFromForm())
+	}
+
+	for _, test := range tests {
+		mockStore = mock.Mock{}
+
+		test.setMessageContext(msg)
+		test.onExpectMethods()
+
+		header := test.generateMsgHeader()
+		body, err := json.Marshal(msg)
+		if err != nil { log.Fatal(err) }
+		event.setMessage(header, body)
+
+		err = h.CreateUser(event)
+		assert.Equalf(t, test.ExpectError, err, "error assert error (test case: %v)\n", test)
+
+		mockStore.AssertExpectations(t)
+	}
+}
+
+func TestCreateAuthForbiddenMessage(t *testing.T) {
+	setUpEnv()
+	msg := &proto.CreateUserMessage{}
+	var tests []createUserTest
+
+	forms := []createUserTest{
+		{
+			XRequestID: none,
+		}, {
+			XRequestID: "ThisIsInvalidXRequestIDString",
+		}, {
+			MessageID: none,
+		}, {
+			AfterMessageID: none,
+		}, {
+			MessageID: "LengthOfThisMessageIDIsNotThirtyTwo",
+		}, {
+			AfterMessageID: "LengthOfThisAfterMessageIDIsNotThirtyTwo",
+		},
+	}
+
+	for _, form := range forms {
+		form.ExpectError = ErrorForbidden
+		tests = append(tests, form.createTestFromForm())
+	}
+
+	for _, test := range tests {
+		mockStore = mock.Mock{}
+
+		test.setMessageContext(msg)
+		test.onExpectMethods()
+
+		header := test.generateMsgHeader()
+		body, _ := json.Marshal(msg)
+		event.setMessage(header, body)
+
+		err := h.CreateUser(event)
+		assert.Equalf(t, test.ExpectError, err, "error assert error (test case: %v)\n", test)
+
+		mockStore.AssertExpectations(t)
+	}
+}
+
+func TestCreateAuthBadRequestMessage(t *testing.T) {
+	setUpEnv()
+	msg := &proto.CreateUserMessage{}
+	var tests []createUserTest
+
+	forms := []createUserTest{
+		{
+			Name: none,
+		}, {
+			Name: "박진홍입니다",
+		}, {
+			Name: "응",
+		}, {
+			PhoneNumber: none,
+		}, {
+			PhoneNumber: "0108837834701088378347",
+		}, {
+			Email: none,
+		}, {
+			Email: "itIsNotEmailFormat",
+		}, {
+			Email: "itIsSoVeryTooLongEmail@naver.com",
+		},
+	}
+
+	for _, form := range forms {
+		form.ExpectError = ErrorBadRequest
+		tests = append(tests, form.createTestFromForm())
+	}
+
+	for _, test := range tests {
+		mockStore = mock.Mock{}
+
+		test.setMessageContext(msg)
+		test.onExpectMethods()
+
+		header := test.generateMsgHeader()
+		body, _ := json.Marshal(msg)
+		event.setMessage(header, body)
+
+		err := h.CreateUser(event)
+		assert.Equalf(t, test.ExpectError, err, "error assert error (test case: %v)\n", test)
+
+		mockStore.AssertExpectations(t)
 	}
 }
