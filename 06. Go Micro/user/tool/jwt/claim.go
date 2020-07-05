8499082f2ecc9377b2e8fa8365dd88f4cad4b706
지농 <jinhong0719@naver.com>
2020-07-05 13:51:47 +0900

    [MODIFY] <user/tool/jwt/>DuplicateCertClaim 구조체 접근 제한자 변경 (priv -> pub)

--- 06. Go Micro/user/tool/jwt/claim.go
+++ 06. Go Micro/user/tool/jwt/claim.go
@@ -2,7 +2,7 @@ package jwt
 
 import "github.com/dgrijalva/jwt-go"
 
-type duplicateCertClaim struct {
+type DuplicateCertClaim struct {
 	UserId string `json:"user_id"`
 	Email string `json:"email"`
 	jwt.StandardClaims
--- 06. Go Micro/user/tool/jwt/duplicate.go
+++ 06. Go Micro/user/tool/jwt/duplicate.go
@@ -12,7 +12,7 @@ func GenerateDuplicateCertJWT(userId, email string, d time.Duration) (ss string,
 	priv, err := ioutil.ReadFile("/Users/parkjinhong/Desktop/go-for-GO/06. Go Micro/auth/tool/jwt/jwt_key.priv")
 	if err != nil { return }
 
-	claims := duplicateCertClaim{
+	claims := DuplicateCertClaim{
 		UserId: userId,
 		Email:  email,
 		StandardClaims: jwt.StandardClaims{
@@ -31,7 +31,7 @@ func GenerateDuplicateCertJWTNoReturnErr(userId, email string, d time.Duration) 
 		log.Fatal(err)
 	}
 
-	claims := duplicateCertClaim{
+	claims := DuplicateCertClaim{
 		UserId: userId,
 		Email:  email,
 		StandardClaims: jwt.StandardClaims{
@@ -47,15 +47,15 @@ func GenerateDuplicateCertJWTNoReturnErr(userId, email string, d time.Duration) 
 	return
 }
 
-func ParseDuplicateCertClaimFromJWT(ss string) (*duplicateCertClaim, error) {
-	token, err := jwt.ParseWithClaims(ss, &duplicateCertClaim{}, func(t *jwt.Token) (i interface{}, e error) {
+func ParseDuplicateCertClaimFromJWT(ss string) (*DuplicateCertClaim, error) {
+	token, err := jwt.ParseWithClaims(ss, &DuplicateCertClaim{}, func(t *jwt.Token) (i interface{}, e error) {
 		return ioutil.ReadFile("/Users/parkjinhong/Desktop/go-for-GO/06. Go Micro/auth/tool/jwt/jwt_key.priv")
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	if claim, ok := token.Claims.(*duplicateCertClaim); ok && token.Valid {
+	if claim, ok := token.Claims.(*DuplicateCertClaim); ok && token.Valid {
 		return claim, nil
 	}
 
