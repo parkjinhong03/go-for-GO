962c0491764e180886a6d63dd40b0143b5d6569a
지농 <jinhong0719@naver.com>
2020-07-05 15:06:44 +0900

    [CREATE] <user/subscriber/test_config.go> test에서 공용으로 사용되는 타입&상수&변수&함수 선언

---
+++ 06. Go Micro/user/subscriber/test_config.go
@@ -0,0 +1,42 @@
+package subscriber
+
+import (
+	"github.com/micro/go-micro/v2/broker"
+	"github.com/stretchr/testify/mock"
+	"log"
+	br "user/adapter/broker"
+	"user/dao"
+	"user/tool/validator"
+)
+
+type method string
+type returns []interface{}
+
+const (
+	none = "none"
+	defaultName = "박진홍"
+	defaultPN = "01088378347"
+	defaultEmail = "jinhong0719@naver.com"
+)
+
+var mockStore mock.Mock
+var h *user
+var userId uint
+var event = &CustomEvent{
+	mock: &mockStore,
+	msg:  &broker.Message{},
+}
+
+func init() {
+	validate, err := validator.New()
+	if err != nil { log.Fatal(err) }
+	rbMQ := br.NewRabbitMQForTest(&mockStore)
+	udc := dao.NewUserDAOCreator(nil)
+	h = NewUser(rbMQ, validate, udc)
+}
+
+func setUpEnv() {
+	mockStore = mock.Mock{}
+	event.clearMessage()
+	userId = 0
+}
