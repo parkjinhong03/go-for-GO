2d114cdc07fe54817255ff191765836938072c50
지농 <jinhong0719@naver.com>
2020-07-07 10:13:03 +0900

    [ADD] <auth/subscriber/change_auth_status_test.go> 테스트 주도 객체 선언

--- 06. Go Micro/auth/subscriber/change_auth_status_test.go
+++ 06. Go Micro/auth/subscriber/change_auth_status_test.go
@@ -1,1 +1,10 @@
 package subscriber
+
+type changeAuthStatusTest struct {
+	AuthId       uint32
+	Success      bool
+	ExpectError  error
+	ExpectMethod map[method]returns
+	XRequestID   string
+	MessageID	 string
+}
