f517c92db7083fcc5e713c589489128937fc8bbf
지농 <jinhong0719@naver.com>
2020-07-12 23:05:19 +0900

    [ADD] <user/dao/user/default_test.go> InsertUser 관련 테스트 주도 객체 및 테스트 함수 생성

--- 06. Go Micro/user/dao/user/default_test.go
+++ 06. Go Micro/user/dao/user/default_test.go
@@ -5,8 +5,10 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/stretchr/testify/assert"
 	"log"
 	"os"
+	"testing"
 	"user/model"
 )
 
@@ -25,4 +27,74 @@ func init() {
 	db.LogMode(false)
 
 	ud = NewDefaultDAO(db)
+}
+
+type insertUserTest struct {
+	AuthId       uint
+	Name         string
+	PhoneNumber  string
+	Email        string
+	Introduction string
+	ExpectError	 error
+}
+
+func (i insertUserTest) Exec() (*model.User, error) {
+	return ud.InsertUser(&model.User{
+		AuthId:       i.AuthId,
+		Name:         i.Name,
+		PhoneNumber:  i.PhoneNumber,
+		Email:        i.Email,
+		Introduction: i.Introduction,
+	})
+}
+
+func TestDefaultUserDAOInsertUser(t *testing.T) {
+	ud.db = ud.db.Begin()
+
+	tests := []insertUserTest{
+		{
+			AuthId:       1,
+			Name:         "박진홍",
+			PhoneNumber:  "01088378347",
+			Email:        "jinhong0719@naver.com",
+			Introduction: "안녕하세요 저의 이름은 박진홍입니다.",
+			ExpectError:  nil,
+		}, {
+			AuthId:       1,
+			Name:         "박진홍",
+			PhoneNumber:  "01088378347",
+			Email:        "jinhong0719@hanmail.net",
+			Introduction: "안녕하세요 저의 이름은 박진홍입니다.",
+			ExpectError:  AuthIdDuplicatedError,
+		}, {
+			AuthId:       2,
+			Name:         "박진홍",
+			PhoneNumber:  "01088378347",
+			Email:        "jinhong0719@naver.com",
+			Introduction: "안녕하세요 저의 이름은 박진홍입니다.",
+			ExpectError:  EmailDuplicatedError,
+		}, {
+			AuthId:       3,
+			Name:         "박진홍박진홍",
+			PhoneNumber:  "01088378347",
+			Email:        "jinhong0719@hanmail.net",
+			Introduction: "안녕하세요 저의 이름은 박진홍입니다.",
+			ExpectError:  DataTooLongError,
+		}, {
+			AuthId:       4,
+			Name:         "박진홍",
+			PhoneNumber:  "01088378347123",
+			Email:        "jinhong0719@hanmail.net",
+			Introduction: "안녕하세요 저의 이름은 박진홍입니다.",
+			ExpectError:  DataTooLongError,
+		},
+	}
+
+	for _, test := range tests {
+		result, err := test.Exec()
+		fmt.Println(result)
+		assert.Equalf(t, test.ExpectError, err, "error assertion error (test case: %v\n)", test)
+	}
+
+	ud.Rollback()
 }
