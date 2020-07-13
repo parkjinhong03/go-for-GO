ab9a665be16e8886cb3600e03d88abc0f1b35298
지농 <jinhong0719@naver.com>
2020-07-13 12:59:24 +0900

    [MODIFY] <user/dao/user/const.go> DataTooLongError 세부 속성 에러로 분류

--- 06. Go Micro/user/dao/user/error.go
+++ 06. Go Micro/user/dao/user/error.go
@@ -6,5 +6,10 @@ var (
 	EmailDuplicatedError = errors.New("email duplicated error")
 	AuthIdDuplicatedError = errors.New("auth id duplicated error")
 	MessageDuplicatedError = errors.New("message duplicated error")
-	DataTooLongError = errors.New("data too long error")
+)
+
+var (
+	NameTooLongError = errors.New("name too long error")
+	EmailTooLongError = errors.New("email too long error")
+	PhoneNumberTooLongError = errors.New("phone number too long error")
 )
