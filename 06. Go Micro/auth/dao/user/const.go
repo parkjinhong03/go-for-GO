cbb295d772413da8f6ed1183def7082f12f4228d
지농 <jinhong0719@naver.com>
2020-07-13 17:14:17 +0900

    [ADD] <auth/dao/user/(const.go, error.go)> Auth의 UserId 속성에 관련된 상수 및 에러 추가

--- 06. Go Micro/auth/dao/user/const.go
+++ 06. Go Micro/auth/dao/user/const.go
@@ -8,7 +8,14 @@ const (
 )
 
 const (
-	IdDuplicateErrorCode = 1062
-	MsgIdDuplicateErrorCode = 1062
+	DuplicateErrorCode = 1062
 	DataTooLongErrorCode = 1406
+)
+
+const (
+	KeyUserId = "uix_auths_user_id"
+)
+
+const (
+	ColumnUserId = "user_id"
 )
--- 06. Go Micro/auth/dao/user/error.go
+++ 06. Go Micro/auth/dao/user/error.go
@@ -4,8 +4,14 @@ import "errors"
 
 var (
 	BcryptGenerateError = errors.New("bcrypt hash generate error")
-	IdDuplicateError = errors.New("user_id duplicate error")
-	MsgIdDuplicateError = errors.New("msg_id duplicate error")
-	DataLengthOverError = errors.New("data too long for column error")
 	InvalidStatusError = errors.New("this status is invalid")
+)
+
+var (
+	UserIdDuplicatedError = errors.New("user id duplicate error")
+	MsgIdDuplicateError = errors.New("msg_id duplicate error")
+)
+
+var (
+	UserIdTooLongError = errors.New("user id too long error")
 )
