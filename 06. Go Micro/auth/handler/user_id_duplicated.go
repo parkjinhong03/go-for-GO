6ef10e9ee7d599642cf899dabbad4c2108183448
지농 <jinhong0719@naver.com>
2020-07-14 22:46:34 +0900

    [ADD] <auth/handler/user_id_duplicated.go> stdin에 출력하는 로깅 추가

--- 06. Go Micro/auth/handler/user_id_duplicated.go
+++ 06. Go Micro/auth/handler/user_id_duplicated.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"github.com/google/uuid"
 	"github.com/micro/go-micro/v2/metadata"
+	"github.com/micro/go-micro/v2/util/log"
 	"github.com/stretchr/testify/mock"
 	"net/http"
 	"time"
@@ -20,17 +21,20 @@ func (e *auth) UserIdDuplicated(ctx context.Context, req *authProto.UserIdDuplic
 	var md metadata.Metadata
 	var ok bool
 	if md, ok = metadata.FromContext(ctx); !ok || md == nil {
+		log.Info(http.StatusForbidden, MessageUnableGetMetadata)
 		rsp.SetStatus(http.StatusForbidden)
 		return
 	}
 
 	var xId string
 	if xId, ok = md.Get("X-Request-Id"); !ok {
+		log.Info(http.StatusForbidden, MessageThereIsNoXReqId)
 		rsp.SetStatus(http.StatusForbidden)
 		return
 	}
 
 	if _, err := uuid.Parse(xId); err != nil {
+		log.Info(http.StatusForbidden, MessageInvalidXReqId)
 		rsp.SetStatus(http.StatusForbidden)
 		return
 	}
@@ -38,7 +42,11 @@ func (e *auth) UserIdDuplicated(ctx context.Context, req *authProto.UserIdDuplic
 	var email string
 	if ss, ok := md.Get("Unique-Authorization"); ok && ss != "" {
 		claim, err := jwt.ParseDuplicateCertClaimFromJWT(ss)
-		if err != nil { rsp.SetStatus(http.StatusForbidden); return }
+		if err != nil {
+			log.Info(http.StatusForbidden, MessageUnableParseJwt)
+			rsp.SetStatus(http.StatusForbidden)
+			return
+		}
 		email = claim.Email
 	}
 
@@ -54,21 +62,25 @@ func (e *auth) UserIdDuplicated(ctx context.Context, req *authProto.UserIdDuplic
 	// 로깅 추가
 	exist, err := ad.CheckIfUserIdExist(req.UserId)
 	if err != nil {
+		log.Info(http.StatusInternalServerError, MessageUnableCheckUserId)
 		rsp.SetStatus(http.StatusInternalServerError)
 		return
 	}
 
 	if exist {
+		log.Info(StatusUserIdDuplicate, MessageUserIdDuplicate)
 		rsp.SetStatusAndMsg(StatusUserIdDuplicate, MessageUserIdDuplicate)
 		return
 	}
 
 	ss, err := jwt.GenerateDuplicateCertJWT(req.UserId, email, time.Hour)
 	if err != nil {
+		log.Info(http.StatusInternalServerError, MessageUnableGenerateJwt)
 		rsp.SetStatus(http.StatusInternalServerError)
 		return
 	}
 
+	log.Info(http.StatusOK, MessageUserIdNotDuplicated)
 	rsp.SetStatusAndMsg(http.StatusOK, MessageUserIdNotDuplicated)
 	rsp.Authorization = ss
 	return
