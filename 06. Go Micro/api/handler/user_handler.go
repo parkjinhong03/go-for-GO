8f76e68aedc99936281041d1bac007744bdeefdf
지농 <jinhong0719@naver.com>
2020-07-14 20:24:20 +0900

    [ADD] <api/handler/user_handler.go> UserHandler 객체 필드 추가 (유효성 검사기, 등록기)

--- 06. Go Micro/api/handler/user_handler.go
+++ 06. Go Micro/api/handler/user_handler.go
@@ -2,15 +2,25 @@ package handler
 
 import (
 	userProto "gateway/proto/golang/user"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/micro/go-micro/v2/registry"
 )
 
-type userHandler struct {
+type UserHandler struct {
 	uc userProto.UserService
+	validate *validator.Validate
+	registry registry.Registry
 }
 
-func NewUserHandler(uc userProto.UserService) *userHandler {
-	return &userHandler{
+func NewUserHandler(uc userProto.UserService, validate *validator.Validate, registry registry.Registry) UserHandler {
+	return UserHandler{
 		uc: uc,
+		validate: validate,
+		registry: registry,
 	}
 }
 
+func (u UserHandler) EmailDuplicateHandler(c *gin.Context) {
+
+}
