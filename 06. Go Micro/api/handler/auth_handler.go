3a9d4378cd0c63aa073f23ff842c2524d29765c2
지농 <jinhong0719@naver.com>
2020-07-14 20:25:03 +0900

    [ADD] <api/handler/auth_handler.go> 핸들러 객체 필드 추가 및 UserIdDuplicate 메서드 구현

--- 06. Go Micro/api/handler/auth_handler.go
+++ 06. Go Micro/api/handler/auth_handler.go
@@ -1,15 +1,60 @@
 package handler
 
 import (
+	"context"
+	"gateway/entity"
 	authProto "gateway/proto/golang/auth"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/micro/go-micro/v2/metadata"
+	"github.com/micro/go-micro/v2/registry"
+	"log"
+	"net/http"
 )
 
-type authHandler struct {
+type AuthHandler struct {
 	ac authProto.AuthService
+	validate *validator.Validate
+	registry registry.Registry
 }
 
-func NewAuthHandler(ac authProto.AuthService) *authHandler {
-	return &authHandler{
+func NewAuthHandler(ac authProto.AuthService, validate *validator.Validate, registry registry.Registry) AuthHandler {
+	return AuthHandler{
 		ac: ac,
+		validate: validate,
+		registry: registry,
 	}
+}
+
+func (ah AuthHandler) UserIdDuplicateHandler(c *gin.Context) {
+	var body entity.UserIdDuplicate
+	if err := c.BindJSON(&body); err != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+
+	if err := ah.validate.Struct(&body); err != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+
+	xReqId := c.GetHeader("X-Request-Id")
+	if xReqId == "" {
+		c.Status(http.StatusForbidden)
+	}
+
+	ctx := context.Background()
+	ctx = metadata.Set(ctx, "X-Request-Id", xReqId)
+	resp, err := ah.ac.UserIdDuplicated(ctx, &authProto.UserIdDuplicatedRequest{
+		UserId: body.UserId,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.Status(int(resp.Status))
+}
+
+func (ah AuthHandler) UserCreateHandler(c *gin.Context) {
+
 }
