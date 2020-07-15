4dba52ac58ca0b0303e03e93225b6a3004da58ff
지농 <jinhong0719@naver.com>
2020-07-15 16:50:52 +0900

    [ADD] <api/entity/auth.go> UserCreate 관련 API에 대한 entity 정의

--- 06. Go Micro/api/entity/auth.go
+++ 06. Go Micro/api/entity/auth.go
@@ -3,3 +3,12 @@ package entity
 type UserIdDuplicate struct {
 	UserId string `json:"user_id" validate:"required,minLength=4,maxLength=16"`
 }
+
+type UserCreate struct {
+	UserId       string `json:"user_id" validate:"required,minLength=4,maxLength=16"`
+	UserPw       string `json:"user_pw" validate:"required,minLength=4,maxLength=16"`
+	Name         string `json:"name" validate:"required,minLength=2,maxLength=4"`
+	PhoneNumber  string `json:"phone_number" validate:"required,strLength=11"`
+	Email        string `json:"email" validate:"required,email,maxLength=30"`
+	Introduction string `json:"introduction" validate:"maxLength=100"`
+}
