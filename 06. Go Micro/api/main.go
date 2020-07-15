46c9bc4a3a9d48e5bc5b4e63b6fcc858a8c38482
지농 <jinhong0719@naver.com>
2020-07-15 20:09:31 +0900

    [ADD] <api/main.go> 의존성 객체(breaker) 추가 생성 및 핸들러 객체 필드에 전달

--- 06. Go Micro/api/main.go
+++ 06. Go Micro/api/main.go
@@ -6,26 +6,34 @@ import (
 	authProto "gateway/proto/golang/auth"
 	userProto "gateway/proto/golang/user"
 	"gateway/tool/validator"
+	"github.com/eapache/go-resiliency/breaker"
 	"github.com/gin-gonic/gin"
 	"github.com/micro/go-micro/v2/client"
 	"github.com/micro/go-micro/v2/client/grpc"
 	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-plugins/registry/consul/v2"
 	"log"
+	"time"
+)
+
+const (
+	DefaultErrorThreshold = 3
+	DefaultSuccessThreshold = 3
+	DefaultTimeout = time.Minute
 )
 
 func main() {
 	cs := consul.NewRegistry(registry.Addrs("http://localhost:8500"))
-
-	opts := []client.Option{client.Registry(cs)}
-
-	ac := authProto.NewAuthService("examples.blog.service.auth", grpc.NewClient(opts...))
-	uc := userProto.NewUserService("examples.blog.service.user", grpc.NewClient(opts...))
 	v, err := validator.New()
 	if err != nil { log.Fatal(err) }
+	bk := breaker.New(DefaultErrorThreshold, DefaultSuccessThreshold, DefaultTimeout)
 
-	ah := handler.NewAuthHandler(ac, v, cs)
-	uh := handler.NewUserHandler(uc, v, cs)
+	opts := []client.Option{client.Registry(cs)}
+	ac := authProto.NewAuthService("examples.blog.service.auth", grpc.NewClient(opts...))
+	uc := userProto.NewUserService("examples.blog.service.user", grpc.NewClient(opts...))
+
+	ah := handler.NewAuthHandler(ac, v, cs, bk)
+	uh := handler.NewUserHandler(uc, v, cs, bk)
 
 	router := gin.Default()
 	v1 := router.Group("/v1")
