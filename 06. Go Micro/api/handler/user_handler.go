4a771e407ad8a237598ce136d6f951cfe9665a8b
지농 <jinhong0719@naver.com>
2020-07-15 20:08:21 +0900

    [ADD] <api/handler/user_handler.go> 핸들러 객체 breaker 필드 추가 및 회로 차단기 패턴 적용

--- 06. Go Micro/api/handler/user_handler.go
+++ 06. Go Micro/api/handler/user_handler.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"gateway/entity"
 	userProto "gateway/proto/golang/user"
+	"github.com/eapache/go-resiliency/breaker"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
+	"github.com/micro/go-micro/v2/client"
+	"github.com/micro/go-micro/v2/errors"
 	"github.com/micro/go-micro/v2/metadata"
 	"github.com/micro/go-micro/v2/registry"
 	"net/http"
@@ -16,13 +19,16 @@ type UserHandler struct {
 	cli userProto.UserService
 	validate *validator.Validate
 	registry registry.Registry
+	breaker *breaker.Breaker
 }
 
-func NewUserHandler(cli userProto.UserService, validate *validator.Validate, registry registry.Registry) UserHandler {
+func NewUserHandler(cli userProto.UserService, validate *validator.Validate,
+	registry registry.Registry, breaker *breaker.Breaker) UserHandler {
 	return UserHandler{
 		cli: cli,
 		validate: validate,
 		registry: registry,
+		breaker: breaker,
 	}
 }
 
@@ -50,9 +56,33 @@ func (u UserHandler) EmailDuplicateHandler(c *gin.Context) {
 		ctx = metadata.Set(ctx, "Unique-Authorization", ss)
 	}
 
-	resp, _ := u.cli.EmailDuplicated(ctx, &userProto.EmailDuplicatedRequest{
-		Email: body.Email,
-	})
+	resp := new(userProto.EmailDuplicatedResponse)
+	reqFunc := func() (err error) {
+		opts := []client.CallOption{client.WithDialTimeout(DefaultDialTimeout), client.WithRequestTimeout(DefaultRequestTimeout)}
+		if resp, err = u.cli.EmailDuplicated(ctx, &userProto.EmailDuplicatedRequest{
+			Email: body.Email,
+		}, opts...); err != nil {
+			return
+		}
+		if resp.Status == http.StatusInternalServerError {
+			err = errors.InternalServerError("go.micro.client", "internal server error")
+		}
+		return
+	}
 
-	c.JSON(int(resp.Status), resp)
+	var err error
+	switch err = u.breaker.Run(reqFunc); err {
+	case nil:
+		c.JSON(int(resp.Status), resp)
+	case breaker.ErrBreakerOpen:
+		c.Status(http.StatusServiceUnavailable)
+		// 처음으로 열린 차단기라면, 알림 서비스 실행
+	default:
+		err, ok := err.(*errors.Error)
+		if !ok {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+		c.Status(int(err.Code))
+	}
 }
