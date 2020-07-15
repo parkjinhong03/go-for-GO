42c3fc962d4647bdeef46863e51047b53e7d966c
지농 <jinhong0719@naver.com>
2020-07-15 16:52:26 +0900

    [ADD] <api/handler/auth_handler.go> UserCreateHandler 메서드 구현

--- 06. Go Micro/api/handler/auth_handler.go
+++ 06. Go Micro/api/handler/auth_handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"gateway/entity"
 	authProto "gateway/proto/golang/auth"
+	"gateway/tool/jwt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
@@ -53,10 +54,50 @@ func (ah AuthHandler) UserIdDuplicateHandler(c *gin.Context) {
 	resp, _ := ah.cli.UserIdDuplicated(ctx, &authProto.UserIdDuplicatedRequest{
 		UserId: body.UserId,
 	})
+	// timeout 처리
 
 	c.JSON(int(resp.Status), resp)
+	return
 }
 
 func (ah AuthHandler) UserCreateHandler(c *gin.Context) {
+	var body entity.UserCreate
+	if err := c.BindJSON(&body); err != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
 
+	if err := ah.validate.Struct(&body); err != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+
+	xReqId := c.GetHeader("X-Request-Id")
+	if _, err := uuid.Parse(xReqId); err != nil {
+		c.Status(http.StatusForbidden)
+		return
+	}
+
+	ss := c.GetHeader("Unique-Authorization")
+	if _, err := jwt.ParseDuplicateCertClaimFromJWT(ss); err != nil || ss == "" {
+		c.Status(http.StatusForbidden)
+		return
+	}
+
+	ctx := context.Background()
+	ctx = metadata.Set(ctx, "X-Request-Id", xReqId)
+	ctx = metadata.Set(ctx, "Unique-Authorization", ss)
+
+	resp, _ := ah.cli.BeforeCreateAuth(ctx, &authProto.BeforeCreateAuthRequest{
+		UserId:       body.UserId,
+		UserPw:       body.UserPw,
+		Name:         body.Name,
+		PhoneNumber:  body.PhoneNumber,
+		Email:        body.Email,
+		Introduction: body.Introduction,
+	})
+	// timeout 처리
+
+	c.JSON(int(resp.Status), resp)
+	return
 }
