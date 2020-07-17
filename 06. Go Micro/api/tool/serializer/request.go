c9479bd32045cd3abdb52ab511a851075e8084f5
지농 <jinhong0719@naver.com>
2020-07-17 13:06:14 +0900

    [ADD] <api/tool/serializer/request.go> 특정 로그 필드 추가 & 변경 및 priv 메서드 선언

--- 06. Go Micro/api/tool/serializer/request.go
+++ 06. Go Micro/api/tool/serializer/request.go
@@ -1,8 +1,11 @@
 package serializer
 
 import (
+	"encoding/json"
 	"github.com/sirupsen/logrus"
+	"log"
 	"net/http"
+	"strings"
 )
 
 type ToLogrusField struct {}
@@ -11,7 +14,33 @@ func (s ToLogrusField) Request(r *http.Request) logrus.Fields {
 	return logrus.Fields{
 		"method": r.Method,
 		"path": r.URL.Path,
-		"client_ip": r.getClientIP(),
-		"headers": r.serializeRequestHeader(),
+		"client_ip": s.getClientIP(r),
+		"X-Request-Id": r.Header.Get("X-Request-Id"),
+		"header": s.encodeHeaderToString(r.Header),
 	}
+}
+
+func (s ToLogrusField) encodeHeaderToString(h http.Header) string {
+	out, err := json.Marshal(h)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(out)
+}
+
+func (s ToLogrusField) getClientIP(r *http.Request) string {
+	var ip string
+
+	if forwarded := r.Header.Get("X-FORWARDED-FOR"); forwarded != "" {
+		ip = forwarded
+		return ip
+	}
+
+	ip = r.RemoteAddr
+	if strings.Contains(ip, "[::1]") {
+		ip = strings.Replace(ip, "[::1]", "localhost", 1)
+	}
+	ip = strings.Split(ip, ":")[0]
+	return ip
 }
