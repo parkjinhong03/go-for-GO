ba525cf1ee90393434d98706cb7425c917d2e835
지농 <jinhong0719@naver.com>
2020-07-19 11:43:15 +0900

    [ADD] <api/tool/logrusfield/get.go> Request 관련 logrus.Fields 생성 및 반환 함수 추가

--- 06. Go Micro/api/tool/logrusfield/get.go
+++ 06. Go Micro/api/tool/logrusfield/get.go
@@ -3,6 +3,7 @@ package logrusfield
 import (
 	"encoding/json"
 	"github.com/sirupsen/logrus"
+	"net/http"
 )
 
 func ForReturn(v interface{}, c int, err error) logrus.Fields {
@@ -21,4 +22,23 @@ func ForReturn(v interface{}, c int, err error) logrus.Fields {
 		"outcome": c,
 		"error":   es,
 	}
+}
+
+func ForHandleRequest(r *http.Request, cip string) logrus.Fields {
+	return logrus.Fields{
+		"method":       r.Method,
+		"path":         r.URL.Path,
+		"client_ip":    cip,
+		"X-Request-Id": r.Header.Get("X-Request-Id"),
+		"header":       encodeHeader(r.Header),
+	}
+}
+
+func encodeHeader(h http.Header) string {
+	b, err := json.Marshal(h)
+	if err != nil {
+		b = []byte{}
+	}
+
+	return string(b)
 }
