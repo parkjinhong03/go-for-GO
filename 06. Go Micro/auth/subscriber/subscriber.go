699fa74d13dfd34944c6064a590c846fbdb9ac1f
지농 <jinhong0719@naver.com>
2020-07-19 22:01:47 +0900

    [ADD] <auth/(handler, subscriber)/(handler, subscriber).go> Tracer 필드 추가

--- 06. Go Micro/auth/subscriber/subscriber.go
+++ 06. Go Micro/auth/subscriber/subscriber.go
@@ -4,19 +4,24 @@ import (
 	"auth/dao"
 	"github.com/go-playground/validator/v10"
 	"github.com/micro/go-micro/v2/broker"
+	"github.com/opentracing/opentracing-go"
 )
 
 type auth struct {
-	mq		 broker.Broker
+	mq       broker.Broker
 	adc      *dao.AuthDAOCreator
 	validate *validator.Validate
+	tr       opentracing.Tracer
 }
 
-func NewAuth(mq broker.Broker, adc *dao.AuthDAOCreator, validate *validator.Validate) *auth {
+func NewAuth(mq broker.Broker, adc *dao.AuthDAOCreator, validate *validator.Validate,
+	tr opentracing.Tracer) *auth {
+
 	return &auth{
 		mq:       mq,
 		adc:      adc,
 		validate: validate,
+		tr:       tr,
 	}
 }
 
