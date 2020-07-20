fdf4c9ba646090ab0746d54e2055d35f09c12be7
지농 <jinhong0719@naver.com>
2020-07-20 22:55:45 +0900

    [ADD] <api/entity/user.go> http 요청 entity -> rpc 요청 proto로 변경 메서드 생성

--- 06. Go Micro/api/entity/user.go
+++ 06. Go Micro/api/entity/user.go
@@ -1,5 +1,15 @@
 package entity
 
+import (
+	userproto "gateway/proto/golang/user"
+)
+
 type EmailDuplicate struct {
 	Email string `json:"email" validate:"required,email,maxLength=30"`
+}
+
+func (ep EmailDuplicate) ToRequestProto() *userproto.EmailDuplicatedRequest {
+	return &userproto.EmailDuplicatedRequest{
+		Email: ep.Email,
+	}
 }
