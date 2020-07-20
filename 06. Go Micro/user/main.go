c1454cd2ba244f5c9a37edbefaefcf63025210fe
지농 <jinhong0719@naver.com>
2020-07-20 23:06:03 +0900

    [ADD] <user/main.go> jaeger 설정 관련 객체, tracer 객체 생성 추가 및 핸들러 의존성 주입

--- 06. Go Micro/user/main.go
+++ 06. Go Micro/user/main.go
@@ -7,6 +7,12 @@ import (
 	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-plugins/broker/rabbitmq/v2"
 	"github.com/micro/go-plugins/registry/consul/v2"
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	jaegerlog "github.com/uber/jaeger-client-go/log"
+	. "net"
+	"os"
 	br "user/adapter/broker"
 	"user/adapter/db"
 	"user/dao"
@@ -25,7 +31,19 @@ func main() {
 	rbMQ := br.ConnRabbitMQ()
 	cs := consul.NewRegistry(registry.Addrs("http://localhost:8500"))
 
-	h := handler.NewUser(rbMQ, validate, udc)
+	sc := &jaegercfg.SamplerConfig{Type: jaeger.SamplerTypeConst, Param: 1}
+	rc := &jaegercfg.ReporterConfig{LogSpans: true, LocalAgentHostPort: "localhost:6831"}
+	ujc := jaegercfg.Configuration{ServiceName: "user-service", Sampler: sc, Reporter: rc, Tags: []opentracing.Tag{
+		{Key: "environment", Value: getEnvironment()},
+		{Key: "host_ip", Value: getLocalAddr().IP},
+		{Key: "service", Value: "userService"},
+	}}
+
+	utr, c, err := ujc.NewTracer(jaegercfg.Logger(jaegerlog.StdLogger))
+	if err != nil { log.Fatal(err) }
+	defer func() { _ = c.Close() }()
+
+	h := handler.NewUser(rbMQ, validate, udc, utr)
 	s := subscriber.NewUser(rbMQ, validate, udc)
 
 	service := micro.NewService(
@@ -52,4 +70,24 @@ func main() {
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getLocalAddr() *UDPAddr {
+	conn, err := Dial("udp", "8.8.8.8:80")
+	if err != nil { log.Fatal(err) }
+	defer func() { _ = conn.Close() } ()
+	return conn.LocalAddr().(*UDPAddr)
+}
+
+func getEnvironment() (env string) {
+	env = os.Getenv("ENV")
+	switch env {
+	case "DEV":
+		env = "development"
+	case "PROD":
+		env = "production"
+	default:
+		env = "development"
+	}
+	return
 }
