a137083ca9471cee677b2c0165fed516d3cef83b
지농 <jinhong0719@naver.com>
2020-07-23 20:01:46 +0900

    [MODIFY] <auth/main.go> service discovery 의존성 객체 패키지 -> 라이브러리 수준으로 변경

--- 06. Go Micro/auth/main.go
+++ 06. Go Micro/auth/main.go
@@ -9,25 +9,38 @@ import (
 	"auth/subscriber"
 	"auth/tool/validator"
 	topic "auth/topic/golang"
+	"fmt"
+	"github.com/hashicorp/consul/api"
 	"github.com/micro/go-micro/v2"
 	br "github.com/micro/go-micro/v2/broker"
 	log "github.com/micro/go-micro/v2/logger"
-	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-micro/v2/transport/grpc"
 	"github.com/micro/go-plugins/broker/rabbitmq/v2"
-	"github.com/micro/go-plugins/registry/consul/v2"
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 	jaegerlog "github.com/uber/jaeger-client-go/log"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	// 플러그인 객체 생성
+	env := getEnvironment()
+	ip := getLocalAddr().IP
+
+	// Message Broker 객체 생성
 	rbMQ := broker.ConnRabbitMQ()
-	cs := consul.NewRegistry(registry.Addrs("http:localhost:8500"))
+	//cs := consul.NewRegistry(registry.Addrs("http:localhost:8500"))
+
+	// Service Discovery 객체 생성
+	dc := api.DefaultConfig()
+	dc.Address = ip.String() + ":8500"
+	cs, err := api.NewClient(dc)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// 의존성 주입을 위한 객체 생성
 	conn, err := db.ConnMysql()
@@ -42,40 +55,40 @@ func main() {
 	rc := &jaegercfg.ReporterConfig{LogSpans: true, LocalAgentHostPort: "localhost:6831"}
 
 	ajc := jaegercfg.Configuration{ServiceName: "auth-service", Sampler: sc, Reporter: rc, Tags: []opentracing.Tag{
-		{Key: "environment", Value: getEnvironment()},
-		{Key: "host_ip", Value: getLocalAddr().IP},
+		{Key: "environment", Value: env},
+		{Key: "host_ip", Value: ip.String()},
 		{Key: "service", Value: "authService"},
 	}}
 	atr, c, err := ajc.NewTracer(jaegercfg.Logger(jaegerlog.StdLogger))
-	if err != nil {log.Fatal(err)}
+	if err != nil { log.Fatal(err) }
 	defer func() { _ = c.Close() }()
 
 	// 서비스 생성
-	service := micro.NewService(
+	s := micro.NewService(
 		micro.Name("examples.blog.service.auth"),
 		micro.Version("latest"),
 		micro.Broker(rbMQ),
-		micro.Registry(cs),
+		//micro.Registry(cs),
 		micro.Transport(grpc.NewTransport()),
 	)
 
 	// 이벤트 및 rpc 핸들러 객체 생성
-	s := subscriber.NewAuth(rbMQ, adc, validate, atr)
+	as := subscriber.NewAuth(rbMQ, adc, validate, atr)
 	// mq := service.Options().Broker
-	h := handler.NewAuth(rbMQ, adc, validate, atr)
+	ah := handler.NewAuth(rbMQ, adc, validate, atr)
 
 	// Broker 초기화 핸들러 함수 생성
-	brkHandleFunc := func() (err error) {
-		brk := service.Options().Broker
+	brf := func() (err error) {
+		brk := s.Options().Broker
 
 		if err = brk.Connect(); err != nil { return }
-		_, err = brk.Subscribe(topic.CreateAuthEventTopic, s.CreateAuth,
+		_, err = brk.Subscribe(topic.CreateAuthEventTopic, as.CreateAuth,
 			br.Queue(topic.CreateAuthEventTopic), // Queue 정적 이름 설정
 			br.DisableAutoAck(), // Ack를 수동으로 실행하게 설정
 			rabbitmq.DurableQueue()) // Queue 연결을 종료해도 삭제X 설정
 		if err != nil { return }
 
-		_, err = brk.Subscribe(topic.ChangeAuthStatusEventTopic, s.ChangeAuthStatus,
+		_, err = brk.Subscribe(topic.ChangeAuthStatusEventTopic, as.ChangeAuthStatus,
 			br.Queue(topic.ChangeAuthStatusEventTopic), // Queue 정적 이름 설정
 			br.DisableAutoAck(), // Ack를 수동으로 실행하게 설정
 			rabbitmq.DurableQueue()) // Queue 연결을 종료해도 삭제X 설정
@@ -85,16 +98,18 @@ func main() {
 		return
 	}
 
+
+
 	// 서비스 초기화 등록
-	service.Init(
-		micro.AfterStart(brkHandleFunc),
+	s.Init(
+		micro.AfterStart(brf),
 	)
 
 	// 핸들러 등록 및 서비스 실행
- 	if err := authProto.RegisterAuthHandler(service.Server(), h); err != nil {
+ 	if err := authProto.RegisterAuthHandler(s.Server(), ah); err != nil {
  		log.Fatal(err)
 	}
-	if err := service.Run(); err != nil {
+	if err := s.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
