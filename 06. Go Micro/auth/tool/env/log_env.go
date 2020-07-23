337054b455e6f80e3c5e4e517c61c08fdd24ffc4
지농 <jinhong0719@naver.com>
2020-07-23 20:16:29 +0900

    [CREATE] <auth/tool/env/log_env.go> Env Variable 관련 함수를 모와두는 패키지 생성

---
+++ 06. Go Micro/auth/tool/env/log_env.go
@@ -0,0 +1,16 @@
+package env
+
+import "os"
+
+func GetLoggingEnv() (env string) {
+	env = os.Getenv("ENV")
+	switch env {
+	case "DEV":
+		env = "development"
+	case "PROD":
+		env = "production"
+	default:
+		env = "development"
+	}
+	return
+}
