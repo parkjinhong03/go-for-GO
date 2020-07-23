1b14998dd25ab93f25df827c0406fe0aa904fa1b
지농 <jinhong0719@naver.com>
2020-07-23 20:15:45 +0900

    [CREATE] <auth/tool/addr/local.go> Local Addr 관련 함수를 모와두는 addr 패키지 생성

---
+++ 06. Go Micro/auth/tool/addr/local.go
@@ -0,0 +1,13 @@
+package addr
+
+import (
+	"log"
+	"net"
+)
+
+func GetLocalAddr() *net.UDPAddr {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil { log.Fatal(err) }
+	defer func() { _ = conn.Close() } ()
+	return conn.LocalAddr().(*net.UDPAddr)
+}
