94735b2f03b967fc54c4d0438c9f60b536026cbb
지농 <jinhong0719@naver.com>
2020-07-24 14:40:18 +0900

    [ADD] <api/handler/const.go> Auth, User Service 이름 상수 추가 및 필터링 관련 상수 추가

--- 06. Go Micro/api/handler/const.go
+++ 06. Go Micro/api/handler/const.go
@@ -14,6 +14,8 @@ const (
 )
 
 const (
+	AuthService = "examples.blog.service.auth"
+	UserService = "examples.blog.service.user"
 	apiGateway = "examples.blog.api.gateWay"
 	authClient = "examples.blog.client.auth"
 	userClient = "examples.blog.client.user"
@@ -23,4 +25,8 @@ const (
 	userIdDuplicate = "examples.blog.service.auth.UserIdDuplicate"
 	beforeCreateAuth = "examples.blog.service.auth.BeforeCreateAuth"
 	emailDuplicate = "examples.blog.service.user.EmailDuplicate"
+)
+
+const (
+	StatusMustBePassing = "Status==passing"
 )
