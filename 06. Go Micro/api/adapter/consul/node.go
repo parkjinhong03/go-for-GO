c8dc7a71d490333be405d5fb7b600f303afad5a1
지농 <jinhong0719@naver.com>
2020-07-25 12:13:28 +0900

    [CREATE] <api/adapter/consul/node.go> consul client를 받아 nodes를 반환하는 함수 생성

---
+++ 06. Go Micro/api/adapter/consul/node.go
@@ -0,0 +1,25 @@
+package consul
+
+import (
+	"fmt"
+	topic "gateway/topic/golang"
+	"github.com/hashicorp/consul/api"
+	"github.com/micro/go-micro/v2/registry"
+)
+
+const (
+	StatusMustBePassing = "Status==passing"
+)
+
+func GetServiceNodes(cs *api.Client) (nds []*registry.Node, err error) {
+	hcs, _, err := cs.Health().Checks(topic.AuthService, &api.QueryOptions{Filter: StatusMustBePassing})
+	if err != nil { return }
+
+	var as *api.AgentService
+	for _, hc := range hcs {
+		as, _, err = cs.Agent().Service(hc.ServiceID, nil)
+		if err != nil { return }
+		nds = append(nds, &registry.Node{Id: as.ID, Address: fmt.Sprintf("%s:%d", as.Address, as.Port)})
+	}
+	return
+}
