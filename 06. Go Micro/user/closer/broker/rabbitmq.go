70a826e9d7fb5f70c684412f153d86a65aebe08a
지농 <jinhong0719@naver.com>
2020-07-29 20:05:01 +0900

    [MODIFY] <user/closer/broker/rabbitmq.go> broker 연결 메서드 호출 명령어 main으로 이동

--- 06. Go Micro/user/closer/broker/rabbitmq.go
+++ 06. Go Micro/user/closer/broker/rabbitmq.go
@@ -11,7 +11,7 @@ import (
 func RabbitMQInitializer(s server.Server, us *subscriber.User) func() error {
 	return func() error {
 		brk := s.Options().Broker
-		if err := brk.Connect(); err != nil { log.Fatal(err) }
+
 		options := []broker.SubscribeOption{broker.Queue(subscriber.CreateUserEventTopic), broker.DisableAutoAck(), rabbitmq.DurableQueue()}
 		if _, err := brk.Subscribe(subscriber.CreateUserEventTopic, us.CreateUser, options...); err != nil { log.Fatal(err) }
 		log.Infof("succeed in connecting to broker!! (name: %s | addr: %s)\n",  brk.String(), brk.Address())
