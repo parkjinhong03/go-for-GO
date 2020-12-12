0e0e8fff77fbb9c40e8827abcb08d2313a98e66d
지농 <jinhong0719@naver.com>
2020-12-12 17:33:23 +0900

    [ADD] <00. Hash/베스트앨범.go> Add struct and solution function

--- 00. Algorithm/00. Hash/베스트앨범.go
+++ 00. Algorithm/00. Hash/베스트앨범.go
@@ -2,3 +2,58 @@
 
 package algorithm0
 
+import (
+	"sort"
+)
+
+type Count struct {
+	Genre string
+	Play int
+}
+
+type Count2 struct {
+	Num int
+	Play int
+}
+
+func solution2(genres []string, plays []int) (results []int) {
+	// map에 정리
+	countMap := map[string]int{}
+	playMap := map[string][]int{}
+	for index, play := range plays {
+		if _, ok := countMap[genres[index]]; !ok {
+			countMap[genres[index]] = 0
+		}
+		if _, ok := playMap[genres[index]]; !ok {
+			playMap[genres[index]] = []int{}
+		}
+		countMap[genres[index]] += play
+		playMap[genres[index]] = append(playMap[genres[index]], index)
+	}
+
+	// 정렬
+	counts := []Count{}
+	for key, value := range countMap {
+		counts = append(counts, Count{key, value})
+	}
+	sort.Slice(counts, func(i int, j int) bool {
+		return counts[i].Play >= counts[j].Play
+	})
+
+	// 추출
+	for _, count := range counts {
+		playCounts := []Count2{}
+		for _, num := range playMap[count.Genre] {
+			playCounts = append(playCounts, Count2{num, plays[num]})
+		}
+		sort.Slice(playCounts, func(i int, j int) bool {
+			return playCounts[i].Play > playCounts[j].Play
+		})
+		for index, playCount := range playCounts {
+			if index >= 2 { break }
+			results = append(results, playCount.Num)
+		}
+	}
+
+	return
+}
