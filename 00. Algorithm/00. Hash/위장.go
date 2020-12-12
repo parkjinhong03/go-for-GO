dd36ea0e972ef0844da9f8e019920a592b382af9
지농 <jinhong0719@naver.com>
2020-12-12 16:49:32 +0900

    [ADD] <00. Hash/위장.go> Add solution function

--- 00. Algorithm/00. Hash/위장.go
+++ 00. Algorithm/00. Hash/위장.go
@@ -2,3 +2,20 @@
 
 package algorithm0
 
+func solution(clothes [][]string) (cases int) {
+	myClothes := map[string][]string{}
+	for _, cloth := range clothes {
+		if _, ok := myClothes[cloth[1]]; !ok {
+			myClothes[cloth[1]] = []string{}
+		}
+		myClothes[cloth[1]] = append(myClothes[cloth[1]], cloth[0])
+	}
+
+	cases = 1
+	for _, clothes := range myClothes {
+		cases *= len(clothes) + 1
+	}
+	cases--
+
+	return
+}
