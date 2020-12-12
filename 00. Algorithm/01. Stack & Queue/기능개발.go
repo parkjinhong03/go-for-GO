31cdd301ec1ceb1795a3970a72f6148e8cf73487
지농 <jinhong0719@naver.com>
2020-12-12 14:14:09 +0900

    [ADD] <01. Stack & Queue/기능개발.go> Add LinkedBaseQueue struct

--- 00. Algorithm/01. Stack & Queue/기능개발.go
+++ 00. Algorithm/01. Stack & Queue/기능개발.go
@@ -2,3 +2,46 @@
 
 package algorithm1
 
+type PercentLinkedQueue struct {
+	start *PercentNode
+	current *PercentNode
+	end *PercentNode
+	count int
+}
+
+type PercentNode struct {
+	percent int
+	next *PercentNode
+}
+
+func (queue *PercentLinkedQueue) Enqueue(percent int) {
+	newNode := PercentNode{
+		percent: percent,
+	}
+	if queue.start == nil {
+		queue.start = &newNode
+	}
+	if queue.end != nil {
+		queue.end.next = &newNode
+	}
+	queue.end = &newNode
+	queue.count++
+}
+
+func (queue *PercentLinkedQueue) Dequeue() {
+	queue.start = queue.start.next;
+	queue.count--
+}
+
+func (queue *PercentLinkedQueue) InitCurrentNode() {
+	queue.current = nil
+}
+
+func (queue *PercentLinkedQueue) GetNextNode() (node *PercentNode) {
+	if queue.current == nil {
+		queue.current = queue.start
+	}
+	node = queue.current
+	queue.current = queue.current.next
+	return
+}
