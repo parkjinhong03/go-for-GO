37bc9375ea79bd64c526f2397ffd8512bb0955f6
지농 <jinhong0719@naver.com>
2020-12-12 15:41:13 +0900

    [ADD] <01. Stack & Queue/다리를 지나는 트럭.go> Add linked queue struct for across

--- 00. Algorithm/01. Stack & Queue/다리를 지나는 트럭.go
+++ 00. Algorithm/01. Stack & Queue/다리를 지나는 트럭.go
@@ -27,3 +27,47 @@ func (queue *TructLinkedQueue) Dequeue() (deletedNode *TructNode) {
 	return
 }
 
+// 다리 건너는 트럭용 큐
+type AcrossLinkedQueue struct {
+	start *AcrossNode
+	current *AcrossNode
+	end *AcrossNode
+	count int
+	acrossTime int
+	totalWeight int
+}
+
+type AcrossNode struct {
+	weight int
+	remainTime int
+	next *AcrossNode
+}
+
+func (queue *AcrossLinkedQueue) Enqueue(weight int) {
+	node := AcrossNode{
+		weight: weight,
+		remainTime: queue.acrossTime,
+	}
+	if queue.start == nil { queue.start = &node }
+	if queue.end != nil { queue.end.next = &node }
+	queue.end = &node
+	queue.count++
+	queue.totalWeight += weight
+}
+
+func (queue *AcrossLinkedQueue) Dequeue() (deletedNode *AcrossNode) {
+	deletedNode = queue.start
+	queue.totalWeight -= queue.start.weight
+	queue.start = queue.start.next
+	queue.count--
+	return
+}
+
+func (queue *AcrossLinkedQueue) GetNextNode() (node *AcrossNode) {
+	if queue.current == nil {
+		queue.current = queue.start
+	}
+	node = queue.current
+	queue.current = queue.current.next
+	return
+}
