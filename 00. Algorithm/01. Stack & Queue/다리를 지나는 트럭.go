ea0b1854fb18a2506c69163c8b021dbe30d21514
지농 <jinhong0719@naver.com>
2020-12-12 15:40:29 +0900

    [ADD] <01. Stack & Queue/다리를 지나는 트럭.go> Add linked queue queue for truck

--- 00. Algorithm/01. Stack & Queue/다리를 지나는 트럭.go
+++ 00. Algorithm/01. Stack & Queue/다리를 지나는 트럭.go
@@ -1,2 +1,29 @@
 package algorithm1
 
+// 대기 트럭용 큐
+type TructLinkedQueue struct {
+	start *TructNode
+	end *TructNode
+	count int
+}
+
+type TructNode struct {
+	weight int
+	next *TructNode
+}
+
+func (queue *TructLinkedQueue) Enqueue(weight int) {
+	node := TructNode{weight: weight}
+	if queue.start == nil { queue.start = &node }
+	if queue.end != nil { queue.end.next = &node }
+	queue.end = &node
+	queue.count++
+}
+
+func (queue *TructLinkedQueue) Dequeue() (deletedNode *TructNode) {
+	deletedNode = queue.start
+	queue.start = queue.start.next
+	queue.count--
+	return
+}
+
