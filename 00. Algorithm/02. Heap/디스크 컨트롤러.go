ded35ff1be1c379d28fe8a24c74d59a9ed787d29
지농 <jinhong0719@naver.com>
2020-12-14 22:46:08 +0900

    [ADD] <02. Heap/디스크 컨트롤러.go> Add struct to declare min heap

--- 00. Algorithm/02. Heap/디스크 컨트롤러.go
+++ 00. Algorithm/02. Heap/디스크 컨트롤러.go
@@ -1,1 +1,21 @@
 package algorithm2
+
+import (
+	"sort"
+)
+
+type JobMinHeap struct {
+	Nodes []JobNode
+	NodeNum int
+}
+
+type JobNode struct {
+	comeTime int // 요청 시점
+	workTime int // 작업 시간 (비교 기준)
+}
+
+func NewJobMinHeap(idxLen int) JobMinHeap {
+	return JobMinHeap{
+		Nodes: make([]JobNode, idxLen),
+	}
+}
