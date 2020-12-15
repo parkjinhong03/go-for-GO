26de32d35977bca9b0d9910917922603307aaa10
지농 <jinhong0719@naver.com>
2020-12-15 10:34:22 +0900

    <02. Heap/이중우선순위큐.go> Add MinHeap & MaxHeap struct

--- 00. Algorithm/02. Heap/이중우선순위큐.go
+++ 00. Algorithm/02. Heap/이중우선순위큐.go
@@ -1,1 +1,27 @@
 package algorithm2
+
+type HeapNode struct {
+	data int
+}
+
+type MinHeap struct {
+	count int
+	Nodes []HeapNode
+}
+
+func NewMinHeap(queueLen int) MinHeap {
+	return MinHeap{
+		Nodes: make([]HeapNode, queueLen),
+	}
+}
+
+type MaxHeap struct {
+	count int
+	Nodes []HeapNode
+}
+
+func NewMaxHeap(queueLen int) MaxHeap {
+	return MaxHeap{
+		Nodes: make([]HeapNode, queueLen),
+	}
+}
