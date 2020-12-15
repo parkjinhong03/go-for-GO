a1642eef3c8197d62cc8782739c73ca5649ff463
지농 <jinhong0719@naver.com>
2020-12-15 22:11:50 +0900

    [ADD] <03. Sorting/K번째수.go> Add solution function

--- 00. Algorithm/03. Sorting/K번째수.go
+++ 00. Algorithm/03. Sorting/K번째수.go
@@ -1,1 +1,12 @@
-package _3__Sorting
+package algorithm3
+
+import "sort"
+
+func solution(array []int, commands [][]int) (results []int) {
+	for _, command := range commands {
+		slicedArr := append([]int{}, array[command[0]-1:command[1]]...)
+		sort.Ints(slicedArr)
+		results = append(results, slicedArr[command[2]-1])
+	}
+	return
+}
