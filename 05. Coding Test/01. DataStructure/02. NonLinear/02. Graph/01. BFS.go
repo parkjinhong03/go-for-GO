59e97568d964583ec1d8a919c7ce9688c075c87b
지농 <jinhong0719@naver.com>
2021-03-18 11:47:29 +0900

    [ADD] <01. BFS.go> Add AddEdge & BFS method in graph struct

--- 05. Coding Test/01. DataStructure/02. NonLinear/02. Graph/01. BFS.go
+++ 05. Coding Test/01. DataStructure/02. NonLinear/02. Graph/01. BFS.go
@@ -2,7 +2,7 @@
 // 루트 노드 혹은 다른 임의의 노드에서 시작해서 인접한 노드들을 먼저 탐색함 (넓게 O, 깊게 X)
 // 두 노드 사이의 최단 경로 혹은 임의의 경로를 찾으려할 때 사용한다.
 
-// 방문할 노드들을 Stack이 아닌 Queue에 저장하는 이유는?
+// 방문할 노드들을 Stack이 아닌 Queue에 저장하는 이유는? 
 //  -> 먼저 들어온 노드를 먼저 방문해야 노드의 단계들을 순차적으로 방문할 수 있기 때문
 
 package main
@@ -31,6 +31,50 @@ func Graph(num int) (g *graph) {
 	for i:=0; i<num; i++ {
 		g.adj = append(g.adj, []int{})
 	}
+	return
+}
+
+// from 번째의 노드에서 to 번째 노드들 사이의 간선 추가
+func (g *graph) AddEdge(from int, to []int) {
+	g.adj[from] = append(g.adj[from], to...)
+}
+
+// start 번째 노드를 시작으로 BFS 방식으로 탐색하여 탐색 순서(seq) 반환
+func (g *graph) BFS(start int) (seq []int) {
+	// 특정 노드를 이미 탐색하였는지 저장 및 확인하기 위한 배열
+	// nodeNum: 3 -> visited 초깃값: [false false false]
+	visited := make([]bool, g.nodeNum)
+	
+	// 다음으로 탐색할 노드들을 저장해두기 위한 Queue 객체 선언
+	q := IntQueue()
+
+	// 시작 노드를 확인했다는 것을 등록하고, Queue에 등록하여 다음 탐색 순서에 추가
+	visited[start] = true
+	q.Push(start)
+
+	// Queue 요소가 비었을 때 까지 반복
+	for q.Size() != 0 {
+		// 이번 for문에서 인접 정점들을 탐색한 정점(노드)을 Queue에서 받음
+		// 첫 for문이라면 위에서 삽입한 start 값을 받음
+		current := q.Pop()
+
+		// 해당 노드와 연결된 모든 정점들을 다음 탐색 순서에 등록하는 for문
+		// 여기서 현재 정점과 연결된 같은 깊이의 정점들을 다음 탐색 순서에 등록함으로써 '너비 우선 탐색'이 되는거임
+		for _, linked := range g.adj[current] {
+			// 이미 처리한 인접 정점이라면 이번 for문 중단
+			if visited[linked] {
+				continue
+			}
+
+			// 방문하였다는 것을 기록하고 Queue를 통해 다음 탐색 순서에 등록
+			visited[linked] = true
+			q.Push(linked)
+		}
+
+		// 탐색 순서 리스트에 이번 for문에서 처리한 정점 추가
+		seq = append(seq, current)
+	}
+
 	return
 }
 
